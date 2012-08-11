98f2aa96c108f0f59ef0338c5a2e959c47227be7
jquast <dingo@1984.ws>
2012-08-10 21:51:49 -0400

    use a 'udb' from openudb() instead of a built-in.

--- default/viewlog.py
+++ default/viewlog.py
@@ -10,32 +10,27 @@ __copyright__ = ['Copyright (c) 2009 Jeffrey Quast']
 __license__ = 'ISC'
 __url__ = 'http://1984.ws'
 
-import os, time
+import datetime
 
 deps = ['bbs']
 
 def update():
   global lastupdate
-  lastupdate = timenow()
+  lastupdate = datetime.datetime.now()
   l=[]
   last_d=last_h=last_c=None
-  for t, (c, msg) in db.logfile.items():
-    d=time.strftime('%b %d', time.localtime(t))
-    h=time.strftime('%H:%M:%S', time.localtime(t))
+  records = sorted(openudb('eventlog').items ()) # so wrong.
+  for t, msg in records:
+    d=t.strftime('%b %d')
+    h=t.strftime('%H:%M:%S')
     s=color(*LIGHTBLUE)
     if d != last_d:
       s+= '%s%s ' % (d, color(*DARKGREY),)
       last_h=last_c=None
     if not last_h or ':'.join(h.split(':')[:2]) != ':'.join(last_h.split(':')[:2]):
       s+= '%s%s%s\n' % (h, color(*DARKGREY), color(*LIGHTBLUE),)
-      last_c=None
-    if c != last_c:
-      s+= '%s[%s%s%s] ' % \
-          (color(*WHITE), color(*DARKGREY), c, color(*WHITE))
-    else:
-      s+= ' '*(len(c)+3)
-    l.append('%s%s%s\n' % (s, color(), msg))
-    last_d,last_h,last_c=d,h,c
+    l.append ('%s\n' % (msg,))
+    last_d,last_h=d,h
   return l
 
 def init():
@@ -44,24 +39,24 @@ def init():
 
 def main():
   global printbuf, lastupdate
-  session = getsession()
+  eventlog = openudb('eventlog')
 
   def refresh(pager=None, pb=None):
     # transform the log buffer into a printable form
-    session.activity = 'Viewing system log'
-    x,y,w,h = 1, 1, session.width, session.height
+    getsession().activity = 'Viewing system log'
+    x,y,w,h = 1, 1, getsession().width, getsession().height
     echo (color() + cls() + pos())
     if h < 20:
       echo (color(*LIGHTRED) + 'screen height must be at least %i, ' \
-            'but is %i.' % (20, session.height))
+            'but is %i.' % (20, getsession().height))
       echo (color() + '\r\n\r\nPress any key...')
       readkey(); return False
     if w < 40:
       echo (color(*LIGHTRED) + 'screen width must be at least %i, ' \
-            'but is %i.' % (40, session.width))
+            'but is %i.' % (40, getsession().width))
       echo (color() + '\r\n\r\nPress any key...')
       readkey(); return False
-    if lastupdate < sorted(db.logfile.keys())[-1] or not pb:
+    if not pb or lastupdate < sorted(eventlog.keys())[-1]:
       # refresh the buffer content
       pb= update()
 
@@ -81,7 +76,8 @@ def main():
   while not pager.exit:
     event, data = readevent(['input','refresh'], timeout=1)
     # buffer is out of date, recalculate
-    if lastupdate < sorted(db.logfile.keys())[-1] or event == 'refresh':
+    if lastupdate < sorted(eventlog.keys())[-1] if len(eventlog) else 0 \
+    or event == 'refresh':
       pager, printbuf = refresh(pager)
       flushevents (['refresh','input'])
     if event == 'input':
