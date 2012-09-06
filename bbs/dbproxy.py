4845a0c136fa3d740a145a72c1f2c89eb9ea0851
jquast <dingo@1984.ws>
2012-09-06 15:53:14 -0400

    mending

--- bbs/dbproxy.py
+++ bbs/dbproxy.py
@@ -3,9 +3,12 @@ from session import getsession
 class DBProxy(object):
   def __init__(self, schema):
     self.schema = schema
-  def __proxy__(self, method, args):
-    getsession().send_event(('db-%s' % (self.schema,)), (method, args,))
-    event, data = getsession().read_event((('db-%s' % (self.schema,))))
+
+  def __proxy__(self, method, *args):
+    print 'proxy', repr(args)
+    event = 'db-%s' % (self.schema,)
+    getsession().send_event(event, (method, args,))
+    event, data = getsession().read_event((event,))
     return data
 
   def __cmp__(self, *args):
@@ -14,27 +17,25 @@ class DBProxy(object):
     return self.__proxy__ ('__contains__', args)
   def __getitem__(self, *args):
     return self.__proxy__ ('__getitem__', args)
-  def __iter__(self, *args):
-    return self.__proxy__ ('__iter__', args)
-  def __len__(self, *args):
-    return self.__proxy__ ('__len__', args)
+  def __len__(self):
+    return self.__proxy__ ('__len__')
   def __setitem__(self, *args):
     return self.__proxy__ ('__setitem__', args)
   def get(self, *args):
     return self.__proxy__ ('get', args)
   def has_key(self, *args):
-    return self.__proxy__ ('has_key', args)
-  def items(self, *args):
-    return self.__proxy__ ('items', args)
-  def keys(self, *args):
-    return self.__proxy__ ('keys', args)
-  def pop(self, *args):
-    return self.__proxy__ ('pop', args)
-  def popitem(self, *args):
-    return self.__proxy__ ('popitem', args)
+    return self.__proxy__ ('has_key', *args)
+  def items(self):
+    return self.__proxy__ ('items')
+  def keys(self):
+    return self.__proxy__ ('keys')
+  def pop(self):
+    return self.__proxy__ ('pop')
+  def popitem(self):
+    return self.__proxy__ ('popitem')
   def setdefault(self, *args):
     return self.__proxy__ ('setdefault', args)
   def update(self, *args):
     return self.__proxy__ ('update', args)
-  def values(self, *args):
-    return self.__proxy__ ('values', args)
+  def values(self):
+    return self.__proxy__ ('values')
