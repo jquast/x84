3c47c0966632e4b19bd62c67691c1fab8dad7cf8
jquast <dingo@1984.ws>
2012-09-06 16:13:27 -0400

    fix argument passing

--- bbs/dbproxy.py
+++ bbs/dbproxy.py
@@ -5,37 +5,51 @@ class DBProxy(object):
     self.schema = schema
 
   def __proxy__(self, method, *args):
-    print 'proxy', repr(args)
+    print '->proxy', method, repr(args)
     event = 'db-%s' % (self.schema,)
     getsession().send_event(event, (method, args,))
     event, data = getsession().read_event((event,))
     return data
 
   def __cmp__(self, *args):
-    return self.__proxy__ ('__cmp__', args)
+    return self.__proxy__ ('__cmp__', *args)
+
   def __contains__(self, *args):
-    return self.__proxy__ ('__contains__', args)
+    return self.__proxy__ ('__contains__', *args)
+
   def __getitem__(self, *args):
-    return self.__proxy__ ('__getitem__', args)
-  def __len__(self):
-    return self.__proxy__ ('__len__')
+    return self.__proxy__ ('__getitem__', *args)
+
   def __setitem__(self, *args):
-    return self.__proxy__ ('__setitem__', args)
+    return self.__proxy__ ('__setitem__', *args)
+
   def get(self, *args):
-    return self.__proxy__ ('get', args)
+    return self.__proxy__ ('get', *args)
+
   def has_key(self, *args):
     return self.__proxy__ ('has_key', *args)
-  def items(self):
-    return self.__proxy__ ('items')
-  def keys(self):
-    return self.__proxy__ ('keys')
-  def pop(self):
-    return self.__proxy__ ('pop')
-  def popitem(self):
-    return self.__proxy__ ('popitem')
+
   def setdefault(self, *args):
-    return self.__proxy__ ('setdefault', args)
+    return self.__proxy__ ('setdefault', *args)
+
   def update(self, *args):
-    return self.__proxy__ ('update', args)
+    return self.__proxy__ ('update', *args)
+
+  def __len__(self):
+    return self.__proxy__ ('__len__')
+
   def values(self):
     return self.__proxy__ ('values')
+
+  def items(self):
+    return self.__proxy__ ('items')
+
+  def keys(self):
+    return self.__proxy__ ('keys')
+
+  def pop(self):
+    return self.__proxy__ ('pop')
+
+  def popitem(self):
+    return self.__proxy__ ('popitem')
+
