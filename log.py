1912951c9457c673b0fea5821583b563bece6f57
jquast <dingo@1984.ws>
2012-09-06 10:44:54 -0400

    disable database records of log events

--- log.py
+++ log.py
@@ -2,11 +2,9 @@
 import copy
 import logging
 import datetime
-import db
+#import bbs.db
 
-# locals
-import ansi
-import session
+from bbs.session import getsession
 
 last_line1 = ('','','','','','')
 MAX_SIZE=10000 # N records to store (as openudb('eventlog'))
@@ -19,21 +17,20 @@ class ColoredConsoleHandler(logging.StreamHandler):
     '%(sep)s%(prefix)s%(message)s'
 
   def color_levelname (self, r):
-    r.levelname = '%s%s%s' % \
-      (ansi.color(ansi.LIGHTRED) if r.levelno >= 50 \
-      else ansi.color(*ansi.LIGHTRED) if r.levelno >= 40 \
-      else ansi.color(*ansi.YELLOW) if r.levelno >= 30 \
-      else ansi.color(*ansi.WHITE),
-        r.levelname.title(),
-      ansi.color())
     return r
+    #r.levelname = '%s%s%s' % \
+    #  (ansi.color(ansi.LIGHTRED) if r.levelno >= 50 \
+    #  else ansi.color(*ansi.LIGHTRED) if r.levelno >= 40 \
+    #  else ansi.color(*ansi.YELLOW) if r.levelno >= 30 \
+    #  else ansi.color(*ansi.WHITE),
+    #    r.levelname.title(),
+    #  ansi.color())
+    #return r
 
   def ins_handle(self, r):
     try:
-      r.handle = '%s' % \
-        (session.sessions.getsession().handle + ' ' \
-          if hasattr(session.sessions.getsession(), 'handle') \
-          and session.sessions.getsession().handle \
+      r.handle = '%s' % (getsession().handle + ' ' \
+          if hasattr(getsession(), 'handle') and getsession().handle \
           else '')
     except KeyError:
       r.handle = ''
@@ -70,29 +67,29 @@ class ColoredConsoleHandler(logging.StreamHandler):
 
   def emit(self, src_record):
     # XXX hook in an event log database ... dont like this
-    if self.el is None:
-      try:
-        self.el = db.openudb('eventlog')
-      except NameError, e:
-        pass
+    #if self.el is None:
+    #  try:
+    #    self.el = bbs.db.openudb('eventlog')
+    #  except NameError, e:
+    #    pass
 
     # trim database
-    if self.el is not None and len(self.el) > MAX_SIZE:
-      largedb = copy.copy(self.el)
-      for k in sorted(largedb.keys())[:-(MAX_SIZE-MAX_STEP)]:
-        del self.el[k]
+    #if self.el is not None and len(self.el) > MAX_SIZE:
+    #  largedb = copy.copy(self.el)
+    #  for k in sorted(largedb.keys())[:-(MAX_SIZE-MAX_STEP)]:
+    #    del self.el[k]
 
     dst_record = self.transform \
         (copy.copy(src_record))
 
     # write db record
-    if self.el is not None:
-      self.el.__setitem__ \
-          (datetime.datetime.now(), \
-          '%s %s %s:%s %s %s' % \
-            (dst_record.levelname, dst_record.handle,
-             dst_record.filename, dst_record.lineno,
-             dst_record.threadName, dst_record.getMessage(),))
+#    if self.el is not None:
+#      self.el.__setitem__ \
+#          (datetime.datetime.now(), \
+#          '%s %s %s:%s %s %s' % \
+#            (dst_record.levelname, dst_record.handle,
+#             dst_record.filename, dst_record.lineno,
+#             dst_record.threadName, dst_record.getMessage(),))
 
     # emit to console
     logging.StreamHandler.emit \
