b982cdf2188ff13bd2a45b00d5456c6d0db24816
jquast <dingo@1984.ws>
2012-09-06 22:23:41 -0400

    deprecate oflush()

--- bbs/output.py
+++ bbs/output.py
@@ -1,12 +1,11 @@
 from session import getsession
 
 def delay(n):
-    getsession().oflush()
     getsession().read_event([], seconds)
-
-def oflush():
-    return getsession().oflush()
 
 def echo(data):
     return getsession().write(data)
 
+def oflush():
+  import warnings
+  warnings.warn('oflush() is deprecated', DeprecationWarning)
