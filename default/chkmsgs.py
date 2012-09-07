0ad94f24153ea3e27ef8a72778447657201db277
jquast <dingo@1984.ws>
2012-09-07 12:28:09 -0400

    x

--- default/chkmsgs.py
+++ default/chkmsgs.py
@@ -1,63 +1,61 @@
-deps = ['bbs']
 
 def main():
-  getsession().activity = 'Checking for new messages'
-  user = getuser(getsession().handle)
-  term = getterminal()
+  session = getsession()
+  session.activity = 'Checking for new messages'
+  term = session.terminal
 
-  print repr(term.clear), repr(term.color)
   echo (term.clear + term.color)
   showfile ('art/msgs.asc')
 
   # check for new private messages
-  echo ('\r\n\r\n  Checking for private messages... '); oflush()
+  echo ('\r\n\r\n  Checking for private messages... ')
 
   # privmsgs returns message record numbers only
-  privmsgs = msgbase.listprivatemsgs(recipient=user.handle)
+  privmsgs = msgbase.listprivatemsgs(recipient=session.handle)
   newmsgs = [msg for msg in privmsgs if not getmsg(msg).read]
 
   echo ('%s messages, %s new\r\n' % (len(privmsgs), len(newmsgs)))
-  if len(newmsgs):
-    echo ((bel) + color(*LIGHTGREEN))
+  if 0 != len(newmsgs):
+    echo (term.bright_green)
     echo ('\r\n  --> Read new private messages? [yna]   <--' + '\b'*5)
-    echo (color())
+    echo (term.normal)
     while True:
       k = getch()
-      if k.lower() == 'y':
-        savescreen = getsession().buffer['resume'].getvalue()
+      if str(k).lower() == 'y':
+        #savescreen = getsession().buffer['resume'].getvalue()
         gosub('msgreader', [msg for msg in newmsgs])
-        echo (savescreen) # restore screen
+        #echo (savescreen) # restore screen
         break
-      elif k.lower() == 'a':
-        savescreen = getsession().buffer['resume'].getvalue()
+      elif str(k).lower() == 'a':
+        #savescreen = getsession().buffer['resume'].getvalue()
         gosub('msgreader', [msg for msg in privmsgs])
-        echo (savescreen) # restore screen
+        #echo (savescreen) # restore screen
         break
-      elif k.lower() == 'n':
+      elif str(k).lower() == 'n':
         break
 
   # check for new public messages
-  echo ('\r\n\r\n  Checking for public messages...'); oflush()
+  echo ('\r\n\r\n  Checking for public messages...')
 
   pubmsgs = msgbase.listpublicmsgs()
   newmsgs = [msg for msg in pubmsgs if not getsession().handle in getmsg(msg).read]
 
   echo ('%s messages, %s new\r\n' % (len(pubmsgs), len(newmsgs)))
   if len(newmsgs):
-    echo ((bel) + color(*LIGHTGREEN))
+    echo (term.bright_green)
     echo ('\r\nRead new public messages? [yna] ')
-    echo (color())
+    echo (term.normal ())
     while True:
       k = getch()
       if k.lower() == 'y':
-        savescreen = getsession().buffer['resume'].getvalue()
+        #savescreen = getsession().buffer['resume'].getvalue()
         gosub('msgreader', [msg for msg in newmsgs])
-        echo (savescreen) # restore screen
+        #echo (savescreen) # restore screen
         break
       elif k.lower() == 'a':
-        savescreen = self.buffer['resume'].getvalue()
+        #savescreen = self.buffer['resume'].getvalue()
         gosub('msgreader', [msg for msg in pubmsgs])
-        echo (savescreen) # restore screen
+        #echo (savescreen) # restore screen
         break
       elif k.lower() == 'n':
         break
