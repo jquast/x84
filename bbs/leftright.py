bd140a473a65fcbd35015cfb16b3b57f79a08b05
jquast <dingo@1984.ws>
2012-09-09 04:42:51 -0400

    various fixes

--- bbs/leftright.py
+++ bbs/leftright.py
@@ -5,14 +5,13 @@ __copyright__ = "Copyright (c) 2008 Jeffrey Quast"
 __version__ = "$Id: leftright.py,v 1.3 2009/05/31 16:12:05 dingo Exp $"
 __license__ = "ISC"
 
+from session import getsession
 from output import echo
 from input import getch
-from ansi import *
-import curses
 
-LEFT, RIGHT = 1, 2
 
 class LeftRightClass:
+  LEFT, RIGHT = 1, 2
   """
   A two-state horizontal lightbar.
 
@@ -36,80 +35,82 @@ class LeftRightClass:
   exit        = False
   laststate   = -1
   lastkey     = None
-  highlight   = color(INVERSE)
-  lowlight    = color()
   left_text   = ' Left '
   right_text  = ' Right '
 
-  def __init__(self, xypos, state=RIGHT):
+  def __init__(self, xypos, state=None):
     """
     @param xypos: screen xy position as tuple
     @param state: starting position
     """
     self.x, self.y = xypos
-    self.state = state
+    self.state = state if state is not None else self.RIGHT
+    self.terminal = getsession().terminal
+    self.highlight = self.terminal.reverse
+    self.lowlight = self.terminal.normal
 
-  def refresh(s, setState=-1):
+  def refresh(self, setState=-1):
     " redraw, optionaly set state (LEFT or RIGHT) "
     if setState == -1:
-      state = s.state
+      state = self.state
     else:
       state = setState
 
-    if state == LEFT:
-      leftattr,rightattr = s.highlight,s.lowlight
-    elif state == RIGHT:
-      leftattr,rightattr = s.lowlight,s.highlight
+    if state == self.LEFT:
+      leftattr,rightattr = self.highlight,self.lowlight
+    elif state == self.RIGHT:
+      leftattr,rightattr = self.lowlight,self.highlight
     else:
       raise StandardError, "illegal argument, %s, must be LEFT or RIGHT" % state
-    echo (pos(s.x, s.y) + color() \
-      + leftattr + s.left_text \
-      + rightattr + s.right_text \
-      + color())
-    s.state = state
+    echo (self.terminal.move(self.y, self.x) \
+        + self.terminal.normal \
+        + leftattr + self.left_text \
+        + rightattr + self.right_text \
+        + self.terminal.normal)
+    self.state = state
 
-  def clear(s):
+  def clear(self):
     " erase "
-    echo (pos(s.x, s.y) + color() \
-      + ' '*(len(s.left_text)+len(s.right_text)))
+    echo (self.terminal.move(self.y, self.x) + self.terminal.normal \
+      + ' '*(len(self.left_text)+len(self.right_text)))
 
-  def right(s):
+  def right(self):
     " set state to right "
-    if s.state != RIGHT:
-      s.refresh(RIGHT)
-      s.moved = True
+    if self.state != self.RIGHT:
+      self.refresh(self.RIGHT)
+      self.moved = True
 
-  def left(s):
+  def left(self):
     " set state to left "
-    if s.state != LEFT:
-      s.refresh(LEFT)
-      s.moved = True
+    if self.state != self.LEFT:
+      self.refresh(self.LEFT)
+      self.moved = True
 
-  def isright(s):
+  def isright(self):
     """
     test lightbar state for right position.
     @return: return True if state is right.
     """
-    return (s.state == RIGHT)
+    return (self.state == self.RIGHT)
 
-  def isleft(s):
+  def isleft(self):
     """
     test lightbar state for left position.
     @return: return True if state is left.
     """
-    return (s.state == LEFT)
+    return (self.state == self.LEFT)
 
-  def flip(s):
+  def flip(self):
     """
     Flip the lightbar state, changing to left when right,
     and right when left.
     """
-    if s.isright():
-      s.left()
+    if self.isright():
+      self.left()
     else:
-      s.right()
+      self.right()
 
-  def run(s, key=None, timeout=None):
+  def run(self, key=None, timeout=None):
     """ an action method for leftright bar:
         - set key as optional user keystroke
         - set interactive when passing keystroke
@@ -117,63 +118,65 @@ class LeftRightClass:
         - set timeout value to return None after that time elapsed
         - unset timeout to wait for user input indefinitly when not interactive
     """
-    s.moved = False
+    self.moved = False
     while True:
       # check for refresh
-      if s.laststate != s.state:
-        s.refresh ()
-      s.laststate = s.state
+      if self.laststate != self.state:
+        self.refresh ()
+      self.laststate = self.state
 
       # read from input
       if not key:
         key = getch(timeout)
-        s.lastkey = key
+        self.lastkey = key
         if key == None:
-          s.timeout = True
+          self.timeout = True
 
       # act on key
       if key in ['\t', ' ']:
-        s.flip()
-      elif key in ['h', curses.KEY_LEFT]:
-        s.left ()
-      elif key in ['l', curses.KEY_RIGHT]:
-        s.right ()
-      elif key in ['q', curses.KEY_EXIT]:
-        s.exit = True
-      elif key in [curses.KEY_ENTER, 'y', 'n']:
+        self.flip()
+      elif key in ['h', self.terminal.KEY_LEFT]:
+        self.left ()
+      elif key in ['l', self.terminal.KEY_RIGHT]:
+        self.right ()
+      elif key in ['q', self.terminal.KEY_EXIT]:
+        self.exit = True
+      elif key in [self.terminal.KEY_ENTER, 'y', 'n']:
         # yes is on left side
-        if key == 'y' or (key == curses.KEY_ENTER and s.isleft()):
-          s.left ()
+        if key == 'y' or (key == self.terminal.KEY_ENTER and self.isleft()):
+          self.left ()
         # and no on right
-        elif key == 'n' or (key == curses.KEY_ENTER and s.isright()):
-          s.right ()
-        return s.state # selection was made
-      if s.interactive or s.exit:
+        elif key == 'n' or (key == self.terminal.KEY_ENTER and self.isright()):
+          self.right ()
+        return self.state # selection was made
+      if self.interactive or self.exit:
         return None # no selection was made or user exit
       key = ''
 
-YES, NO     = 1, 2
 
 class YesNoClass(LeftRightClass):
+  YES, NO     = 1, 2
   " A horizontal yes/no lightbar "
   left_text   = '  Yes  '
   right_text  = '  No  '
-  def __init__(self, xypos, state=YES):
+  def __init__(self, xypos, state=None):
     """
     @param xypos: screen xy position as tuple
     @param state: state, default=YES
     """
-    LeftRightClass.__init__(self, xypos, state)
+    LeftRightClass.__init__(self, xypos,
+        state if state is not None else self.YES)
 
-PREV, NEXT  = 1, 2
 
 class PrevNextClass(LeftRightClass):
+  PREV, NEXT  = 1, 2
   " A horizontal previous/next lightbar "
   left_text  = '<- pREV  '
   right_text = ' nEXT ->'
-  def __init__(self, xypos, state=NEXT):
+  def __init__(self, xypos, state=None):
     """
     @param xypos: screen xy position as tuple
     @param state: state, default=NEXT
     """
-    LeftRightClass.__init__(self, xypos, state)
+    LeftRightClass.__init__(self, xypos,
+        state if state is not None else self.NEXT)
