f1157494714797aa3c8b7b19982e428fec83bdad
jquast <dingo@1984.ws>
2012-09-09 04:43:52 -0400

    add 'TAP' option to display actual keyboard input when debug is enabled. This is False by default, otherwise passwords could be displayed. add .idle() and .duration() methods add .origin() method and .source() property, optional source= parameter on init. should be socket addr,port make .handle property read-only more runscript() tightening ... handle 'ConnectionClosed' event by throwing same exception handle 'refresh-naws' event by updating terminal dimensions and passing a 'refresh' event through. also add ^L translation to refresh event; it is still recieved on input. more read_event() tighetening ...

--- bbs/session.py
+++ bbs/session.py
@@ -1,7 +1,6 @@
 import multiprocessing
 import traceback
 import itertools
-import StringIO
 import logging
 import time
 import sys
@@ -12,14 +11,15 @@ import exception
 import scripting
 
 logger = multiprocessing.get_logger()
-
 mySession = None
+TAP = False # if True, keystrokes logged at debug level
 
 def getsession():
+  """Return session, after a .run() method has been called on any 1 instance.
+  """
   assert mySession is not None, \
       'a Session() instance must be initialized with .run() method'
   return mySession
-
 
 class Session(object):
   """
@@ -36,23 +36,34 @@ class Session(object):
   returns a any any data waiting on the event queue within the specified
   timeout. if data is recieved on the IPC pipe that is not requested, it is
   buffered to _buffer.
+
+  _source is a tuple that indicates the origin of the terminal in some form,
+  such as (client.addr, client.port) for telnet connections.
   """
   _user = None
   _handle = None
   _activity = None
-  _encoding = None
   _cwd = None
-  _script_stack = None
-  _buffer = None
+  last_input_time = 0.0
+  connect_time = 0.0
 
-
-  def __init__ (self, terminal=None, pipe=None):
+  def __init__ (self, terminal=None, pipe=None, encoding=None,
+  source=('undef', None)):
     self.pipe = pipe
     self.terminal = terminal
     self._script_stack = list(((ini.cfg.get('matrix','script'),),))
-    self._encoding = ini.cfg.get('session', 'default_encoding')
+    self._encoding = encoding if encoding is not None \
+        else ini.cfg.get('session', 'default_encoding')
     self._buffer = dict()
+    self._source = source
+    self.last_input_time = \
+        self.connect_time = time.time()
 
+  def idle(self):
+    return time.time() -self.last_input_time
+
+  def duration(self):
+    return time.time() -self.connect_time
 
   @property
   def activity(self):
@@ -71,12 +82,16 @@ class Session(object):
     """User handle."""
     return self._handle
 
+  @handle.setter
+  def handle(self, value):
+    # _handle only set by @user.setter
+    raise TypeError, 'handle property read-only'
+
 
   @property
   def user(self):
     """User record object."""
     return self._user
-
 
   @user.setter
   def user(self, value):
@@ -86,6 +101,21 @@ class Session(object):
     self._user = value
     self._handle = value.handle
 
+  @property
+  def origin(self):
+    """A string describing the source of this connection."""
+    return '%r' % (self.souce,)
+
+  @property
+  def source(self):
+    """A tuple describing the session source, usually in
+       the form of (sock.addr, sock.port,). """
+    return self._source
+
+  @source.setter
+  def source(self, value):
+    assert type(value) is tuple
+    self._source = value
 
   @property
   def cwd(self):
@@ -132,51 +162,46 @@ class Session(object):
     assert mySession is None, 'run() cannot be called twice'
     mySession = self
     fallback_stack = copy.copy(self._script_stack)
-    #log_level = getattr(logging, ini.cfg.get('session','log_level').upper())
-    #logger.setLevel (log_level)
     while len(self._script_stack) > 0:
-      logger.debug ('%s/%s script_stack: %s',
-          self.pid, self.handle, self._script_stack)
+      logger.debug ('%s: script_stack is %s',
+          self.handle, self._script_stack)
       try:
         lastscript = self._script_stack[-1]
-        self.runscript (*self._script_stack.pop())
-
-        logger.warn ('crashloop: recovery <Session.runscript(%r)>' \
-            % (lastscript,))
+        value = self.runscript (*self._script_stack.pop())
         if not self._script_stack:
           logger.error ('_script_stack = <fallback_stack: %r>', \
               fallback_stack)
           self._script_stack = fallback_stack
+          continue
       except exception.Goto, e:
-        logger.info ('Goto: %s' % (e,))
+        logger.debug ('Goto: %s' % (e,))
         self._script_stack = [e[0] + tuple(e[1:])]
         continue
       except exception.Disconnect, e:
         logger.info ('User disconnected: %s' % (e,))
-        break
+        return
       except exception.ConnectionClosed, e:
         logger.info ('Connection Closed: %s' % (e,))
-        break
+        return
       except exception.SilentTermination, e:
         logger.info ('Silent Termination: %s' % (e,))
-        break
+        return
       except exception.ScriptError, e:
         logger.error ("ScriptError rasied: %s", e)
       except Exception, e:
-        # Pokemon exception
+        # Pokemon exception. question: can this python code
+        # be made to look like an ascii poke-ball?
         t, v, tb= sys.exc_info()
         map (logger.error, (l.rstrip() \
             for l in itertools.chain \
               (traceback.format_tb(tb), \
-               ('%s/%s %s' % (self.pid, self.handle, fe)
-                 for fe in traceback.format_exception_only(t, v)))))
+               (fe for fe in traceback.format_exception_only(t, v)))))
       if 0 != len(self._script_stack):
         # recover from a general exception or script error
         toss = self._script_stack.pop()
         logger.info ('%s after %s popped from script stack.',
           'continue' if 0 != len(self._script_stack) else 'stop',
           toss)
-    logger.info ('%s/%s end of session.', self.pid, self.handle)
 
 
   def write (self, data, encoding=None):
@@ -203,41 +228,51 @@ class Session(object):
 
   def _buffer_event (self, event, data):
     """
-      Push data into FIFO buffer keyed by event.
-      If event is a string named 'input', translate data as bytestrings
-      into items yielded from generator self.terminal.trans_input()
+       Push data into FIFO buffer keyed by event.  If event is a string named
+       'input', translate data as bytestrings into items yielded from generator
+       self.terminal.trans_input().
     """
-    if not self._buffer.has_key(event):
+    if event == 'ConnectionClosed':
+      raise exception.ConnectionClosed (data)
+    if event == 'refresh-naws':
+      # transliterate to 'refresh' event, but record new terminal dimensions
+      (self.terminal.columns, self.terminal.rows) = data
+      (event, data) = 'refresh', ('resize', data)
+    if not self._buffer.has_key(event) or event == 'refresh':
+      # create new buffer; only accept 1 most recent 'refresh' event
       self._buffer[event] = list()
-      logger.debug ('%s/%s new event buffer, %s.',
-          self.pid, self.handle, event,)
-    if event != 'input':
-      self._buffer[event].insert (0, data)
-      logger.debug ('%s/%s event buffered, (%s,%s).',
-          self.pid, self.handle, event, data,)
-      return
-    elif event == 'input':
+    if event == 'input':
       for keystroke in self.terminal.trans_input(data, self.encoding):
-        self._buffer[event].insert (0, keystroke)
-
+        if keystroke == chr(12):
+          # again; buffer only 1 most recent 'refresh' eventm
+          # this time, if <ctrl+L> is pressed; there exists a KEY_REFRESH ...
+          self._buffer['refresh'] = list((0, ('input', keystroke,),))
+        self._buffer['input'].insert (0, keystroke)
+      self._last_input_time = time.time()
+      logger.debug ('%s event buffered, %s.', self.handle,
+          (event, data if TAP else 'x' * len(data),))
+    else:
+      self._buffer[event].insert (0, data)
+      logger.info ('%s event buffered, (%s,%s).', self.handle, event, data,)
 
   def send_event (self, event, data):
     """
-      Send data to IPC pipe in form of (event, data).
-      engine.py of the main process translates the following events into
-      actions: 'output', send data to socket; 'global', duplicate data to all
-      other IPC processes; and 'db-<schema>', a IPC database communication
-      pipe used by dbproxy.py
+       Send data to IPC pipe in form of (event, data).
+       engine.py of the main process translates the following events into
+       actions: 'output', send data to socket; 'global', duplicate data to all
+       other IPC processes; and 'db-<schema>', a IPC database communication
+       pipe used by dbproxy.py
     """
     self.pipe.send ((event, data))
 
 
   def read_event (self, events, timeout=None):
     """
-      Poll for and return any buffered IPC data for events that have arrived
-      in the form of (event, data). Always available is the 'input' event for
-      keyboard input. A timeout of None waits indefinitely, otherwise
-      (None, None) is returned when timeout has exceeded.
+       Poll for and return any buffered IPC data for events that have arrived
+       in the form of (event, data). Always available is the 'input' event for
+       keyboard input. A timeout of None waits indefinitely, otherwise
+       (None, None) is returned when timeout has exceeded. when timeout of -1
+       is used, this call is non-blocking.
     """
     (event, data) = (None, None)
 
@@ -254,41 +289,28 @@ class Session(object):
     while waitfor > 0:
       if self.pipe.poll (None if waitfor == float('inf') else waitfor):
         event, data = self.pipe.recv()
-
         if event == 'exception':
           (t, v, tb) = data
           map (logger.error, (l.rstrip() for l in tb))
           logger.error ('local traceback follows')
           raise t, v
-
-        if event == 'connectionclosed':
-          raise exception.ConnectionClosed (data)
-
-        # buffer data received. data can be any size, such as a multibyte
-        # input sequence, or a chunk of output data. It must be pickable
         self._buffer_event (event, data)
-
-        # when an event has just been buffered that is of an event specified
-        # in the events=[] list, immediately pop the first column of data stored
-        # in the buffer and return. For instance, a single keystroke
         if event in events:
           return (event, self._event_pop(event))
-
-      # when the event recieved is not buffered and the event specified in
-      # the events=[] list has not returned, then return (None, None) to
-      # indicate that all buffers specified are flushed.
+        else:
+          logger.debug ('not seeking event %s; pass.', (event,))
+      # poll()
       if timeout == -1:
         return (None, None)
-
       waitfor = timeleft(t)
     return (None, None)
 
 
   def runscript(self, script, *args):
-    """Execute script module's .main() function with optional *args as arguments."""
+    """Execute script's .main() callable with optional *args."""
     import bbs
-    logger.info ('%s/%s runscript %r, args=%r.',
-        self.pid, self.handle, script, args)
+    logger.info ('%s/%s runscript %r.',
+        self.pid, self.handle, (script, args,))
 
     self._script_stack.append ((script,) + args)
     try:
@@ -299,14 +321,17 @@ class Session(object):
 
     current_path = os.path.dirname(self.script_filepath)
     if not current_path in sys.path:
-      logger.debug ('%s/%s append to sys.path: %s', self.pid, self.handle, current_path)
       sys.path.append (current_path)
+      logger.debug ('%s/%s append to sys.path: %s',
+          self.pid, self.handle, current_path)
 
     script = scripting.load(self.cwd, self.script_name)
     for idx in bbs.__all__:
       setattr(script, idx, getattr(bbs, idx))
-    assert hasattr(script, 'main'), "%s: main() not found." % (self.script_name,)
-    assert callable(script.main), "%s: main not callable." % (self.script_name,)
+    assert hasattr(script, 'main'), \
+        "%s: main() not found." % (self.script_name,)
+    assert callable(script.main), \
+        "%s: main not callable." % (self.script_name,)
     prev_path = self.cwd \
         if self.cwd is not None else current_path
     self.cwd = current_path
@@ -317,15 +342,3 @@ class Session(object):
     toss = self._script_stack.pop()
     logger.debug ('%s popped from script_stack, return value=%s', toss, value)
     return value
-
-  # XXX kill
-  def oflush (self):
-    import warnings
-    warnings.warn ('unncessary', DeprecationWarning)
-  # XXX kill
-  def getterminal(self):
-    import warnings
-    warnings.warn ('use terminal attribute instead', DeprecationWarning, 2)
-    return self.terminal
-
-
