9f132a15386eba67382da6093601247a5a01e3d4
jquast <dingo@1984.ws>
2012-09-09 05:59:02 -0400

    depricate and cleanup more things, but more importantly make password seamlessly encrypt when .password = value, and authuser(handle, try_pass) will also encrypt and compare.

--- bbs/userbase.py
+++ bbs/userbase.py
@@ -1,36 +1,11 @@
-from session import getsession, logger
 from dbproxy import DBProxy
+import bcrypt
 
 db = DBProxy('userbase')
 
-# XXX Begin Compatibility
-# XXX Rename / Cut
-def finduser(handle):
-  " Given handle, discover and return database key, matched case-insensitive "
-  for k in db.keys():
-    if k.lower() == handle.lower():
-      return k
-
-def userexist(handle):
-  " return True if user exists "
-  return db.has_key(handle)
-
-def authuser(handle, try_pass):
-  """
-    Return True if user by key of handle exists, has a password, the
-    password matches the try_pass argument, and password is not None.
-  """
-  h= finduser(handle)
-  if h is None:
-    return None
-  u= getuser(h)
-  if u is None:
-    return None
-  return u.password is not None and try_pass == u.password
-
-def getuser(handle):
-  " Return User object instance, retrieved by handle. "
-  return db[handle]
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def listusers():
   """
@@ -39,125 +14,139 @@ def listusers():
   """
   return db.values ()
 
-def listgroups():
-  " Return list of all groups on active accounts. "
-  groups = set()
-  for user in listusers():
-    map(groups.add, user.groups)
-  return groups
+def finduser(handle):
+  """Given handle, discover and return matching database key."""
+  for key in db.keys():
+    if key.lower() == handle.lower():
+      return key
 
-def groupmembers():
-  " return dict of groups containing list of members. "
-  return dict([(grp, [u for u in db.values() if grp in u.groups])
-    for grp in listgroups()])
+def getuser(handle):
+  """Return User object instance, retrieved by handle."""
+  return db[handle]
 
-def membersofgroup(group):
-  " return list of users found in group by name. "
-  return groupmembers()[group]
-# XXX Rename
-# XXX Begin Compatibility
+def userexist(handle):
+  """Return True if user exists."""
+  import warnings
+  warnings.warn(DeprecationWarning, 'userexist deprecated, use finduser()', 2)
+  return db.has_key(handle)
 
-class User(object):
+def authuser(handle, try_pass):
+  """Return True if try_pass is the correct password for a user."""
+  assert type(try_pass) is unicode
+  assert len(try_pass) > 0
+  (salt, hashed) = getuser(handle).password
+  return hashed == bcrypt.hashpw(try_pass, salt) \
+      if not None in (salt, hashed) \
+      else False
+
+class User(dict):
   _handle = None
   _calls = 0
   _lastcall = 0
-  _groups = []
-  _location = ''
-  _hint = ''
-  _saved = False
-  _postrefs = []
-  _password = None
+  _location = u''
+  _hint = u''
+  _password = (None, None)
   _plan = ''
-  def __init__(self, handle='anonymous', password=u'', location=u'', hint=u''):
+
+  def __init__(self, handle=u'anonymous', password=u'', location=u'', hint=u''):
     self.handle = handle
     self.password = password
     self.location = location
     self.hint = hint
+    dict.__init__ (self)
+    self.set('groups', set(),)
+
 
   @property
-  def calls(self):
-    return self._calls
-  @calls.setter
-  def calls(self, value):
-    assert type(value) is int
-    self._calls = value
+  def isSysop(self):
+    """Return True if 'sysop' in groups."""
+    return 'sysop' in self.get('groups')
 
   @property
   def handle(self):
+    """Nickname of caller and key of user in database."""
     return self._handle
+
   @handle.setter
   def handle(self, value):
     assert type(value) is unicode and len(value) > 0
     self._handle = value
+    db[self.handle] = self #save
 
-  @property
-  def lastcall(self):
-    return self._lastcall
-  @lastcall.setter
-  def lastcall(self, value):
-    assert type(value) is float
-    self._lastcall = value
-
-  @property
-  def groups(self):
-    return self._groups
-  @groups.setter
-  def groups(self, value):
-    assert type(value) is list
-    self._groups = value
-
-  @property
-  def location(self):
-    return self._location
-  @location.setter
-  def location(self, value):
-    assert type(value) is unicode
-    self._location = value
-
-  @property
-  def hint(self):
-    return self._hint
-  @hint.setter
-  def hint(self, value):
-    assert type(value) is unicode
-    self._hint = value
-
-  @property
-  def postrefs(self):
-    return self._postrefs
-  @postrefs.setter
-  def postrefs(self, value):
-    assert type(value) is list
-    self._postrefs = value
 
   @property
   def password(self):
     return self._password
+
   @password.setter
   def password(self, value):
     assert type(value) is unicode
-    self._password = value
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(value, salt)
+    self._password = (salt, hashed)
+
+  @property
+  def lastcall(self):
+    """Time last called in epoch seconds."""
+    return self._lastcall
+
+  @lastcall.setter
+  def lastcall(self, value):
+    assert type(value) in (int,float,)
+    self._lastcall = value
+    db[self.handle] = self #save
+
+
+  @property
+  def calls(self):
+    """Legacy, number of times user has 'called' this board."""
+    return self._calls
+
+  @calls.setter
+  def calls(self, value):
+    assert type(value) is int
+    self._calls = value
+    db[self.handle] = self #save
+
+
+  @property
+  def location(self):
+    """Legacy, used as a geographical location."""
+    return self._location
+
+  @location.setter
+  def location(self, value):
+    assert type(value) is unicode
+    self._location = value #save
+
+
+  @property
+  def hint(self):
+    """Legacy, used as an e-mail address."""
+    return self._hint
+
+  @hint.setter
+  def hint(self, value):
+    assert type(value) is unicode
+    self._hint = value
+    db[self.handle] = self #save
+
 
   @property
   def plan(self):
+    """Legacy, like unix .plan; blogosphere, lol."""
     return self._plan
+
   @plan.setter
   def plan(self, value):
     assert type(value) is unicode
     self._plan = value
 
-# XXX Compatibility
-# XXX Rename / Cut
   def set (self, key, value):
-    db[self.handle].__setattr__ (key, value)
+    self[key] = value
+    db[self.handle] = self # save
 
-  def get(self, key):
-    return db[self.handle].__getattr__(key)
-
-  def has_key(self, key):
-    return hasattr(db[self.handle], key)
-
-  def add(self):
+  def save(self):
     if 0 == len(listusers()) and not 'sysop' in self.groups:
       logger.warn ('first new user becomes sysop: %s', self.handle)
       self.groups = list(('sysop',))
@@ -165,21 +154,3 @@ class User(object):
 
   def delete(self):
     del db[self.handle]
-
-  def addgroup (self, group):
-    db[self.handle].groups = [g for g in db[self.handle].groups if g != group] \
-        + [group,]
-  def delgroup (self, group):
-    db[self.handle].groups = [g for g in db[self.handle].groups if g != group]
-
-  def posts(self):
-    return self._postrefs
-
-  def numPosts(self):
-    return len(self._postrefs)
-
-  def post(self, msg_index):
-    assert type(msg_index) is int
-    db[self.handle].postrefs = list((msg_index,) + db[self.handle].postrefs)
-# XXX End compatibility
-# XXX Rename / Cut
