b771b5f2fb6836c9c5923010f3064a9881e90b23
jquast <dingo@1984.ws>
2012-09-09 04:36:09 -0400

    not not export userbase as a module, intent is to use only limited methods jxport strutils.maxanswidth, session.logger, getpos 4->2 space

--- bbs/__init__.py
+++ bbs/__init__.py
@@ -1,18 +1,12 @@
-# begin XXX cut/transform XXX
-#from userbase import finduser, getuser, setuser
-
-import userbase
-import msgbase
-from ansi import pos
-# end XXX cut/transform XXX
+import msgbase # FIX, work on msging...
 import ini
-from strutils import chompn, asctime, ansilen, chkseq, seqc
+from strutils import chompn, asctime, ansilen, chkseq, seqc, maxanswidth
 from dbproxy import DBProxy
 from userbase import User, getuser, finduser, userexist, authuser, listusers
-from session import getsession
+from session import getsession, logger
 from fileutils import abspath, fopen, ropen
 from output import echo, oflush, delay
-from input import getch, readline, readlineevent
+from input import getch, getpos, readline, readlineevent
 from ansiwin import AnsiWindow
 from editor import HorizEditor
 from leftright import LeftRightClass, YesNoClass, PrevNextClass
@@ -21,6 +15,8 @@ from pager import ParaClass
 from sauce import SAUCE
 
 __all__ = [
+    'logger',
+    'maxanswidth',
     'chompn',
     'asctime',
     'ansilen',
@@ -53,6 +49,7 @@ __all__ = [
     'getterminal',
     'gethandle',
     'getch',
+    'getpos',
     'delay',
     'oflush',
     'echo',
@@ -67,72 +64,70 @@ __all__ = [
 ]
 
 def getterminal():
-    return getsession().terminal
+  return getsession().terminal
 
 
 def gethandle():
-    return getsession().handle
+  return getsession().handle
 
 
 def disconnect():
-    import exception as exception
-    raise exception.Disconnect('disconnect')
+  import exception as exception
+  raise exception.Disconnect('disconnect')
 
 
 def goto(*arg):
-    import exception
-    raise exception.Goto(arg)
+  import exception
+  raise exception.Goto(arg)
 
 
 def gosub(*arg):
-    return getsession().runscript(*(arg[0],) + arg[1:])
+  return getsession().runscript(*(arg[0],) + arg[1:])
 
 
 def sendevent(pid, event, data):
-    return getsession().send_event('event', (pid, event, data))
+  return getsession().send_event('event', (pid, event, data))
 
 
 def broadcastevent(event, data):
-    return getsession().send_event('global', (getsession().pid, event, data))
+  return getsession().send_event('global', (getsession().pid, event, data))
 
 
 def readevent(events = ['input'], timeout = None):
-    return getsession().read_event(events, timeout)
+  return getsession().read_event(events, timeout)
 
 
 def flushevent(event = 'input', timeout = -1):
-    return getsession().flush_event(event, timeout)
+  return getsession().flush_event(event, timeout)
 
 
 def flushevents(events = ['input'], timeout = -1):
-    return [flushevent(e, timeout) for e in events]
+  return [flushevent(e, timeout) for e in events]
 
 
 def loginuser(handle):
-    import time as time
-    u = userbase.getuser(handle)
-    u.calls += 1
-    u.lastcall = time.time()
+  import time as time
+  u = userbase.getuser(handle)
+  u.calls += 1
+  u.lastcall = time.time()
 
 
 def showfile(filename, bps=0, pause=0.1, cleansauce=True):
-    if '*' in filename or '?' in filename:
-        fobj = ropen(filename, 'rb')
-    else:
-        fobj = fopen(filename, 'rb')
-    if cleansauce:
-        data = str(SAUCE(fobj))
-    else:
-        data = chompn(fobj.read())
-    if not bps:
-        echo(chompn(data))
-    else:
-        cps = bps / 8
-        cpp = cps * pause
-        n = 0
-        for ch in data:
-            n += 1
-            if n == int(cpp):
-                getsession().read_event(events=['input'], timeout=pause)
-                n = 0
-            echo(ch)
+  # glob magic
+  fobj = ropen(filename, 'rb') \
+    if '*' in filename or '?' in filename \
+      else fopen(filename, 'rb')
+
+  data = chompn(str(SAUCE(fobj)) if cleansauce else fobj.read())
+
+  if 0 == bps:
+    echo (data)
+    echo (getterminal().normal)
+    return
+
+  # display at a timed speed, re-expereince the pace of 9600bps ...
+  cpp = (float(bps)/8) *pause
+  for n, ch in enumerate(data):
+    if 0 == (n % cpp):
+      getsession().read_event(events=['input'], timeout=pause)
+    echo (ch)
