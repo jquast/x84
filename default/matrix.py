d46d967136ceb0cbf3e993265218ff0c26895c7b
jquast <dingo@1984.ws>
2012-09-11 20:28:37 -0400

    a lot of silly shit.

--- default/matrix.py
+++ default/matrix.py
@@ -12,8 +12,9 @@ __url__ = 'https://github.com/jquast/x84/'
 TIMEOUT = 45
 CH_MASK_PASSWD = 'x'
 ALLOW_ANONYMOUS = True
-ALLOW_APPLY = True
 def main ():
+  echo ('\033(U') # switches to CP437 on some systems.
+  # you may or may not want this; the art on this bbs does!
   session = getsession()
   term = getterminal()
   handle=''
@@ -21,7 +22,21 @@ def main ():
   newcmds = ini.cfg.get('matrix', 'newcmds').split()
   APPLY_DENIED = '\r\n\r\nfiRSt, YOU MUSt AbANdON YOUR libERtIES.'
   apply_msg = '\r\n\r\n  --> Create new account? [ynq]   <--' + '\b'*5
-  encrypt_msg = term.cyan_reverse + '  encrypting ...'
+  prompt_user = '\r\n  user: '
+  badpass_msg = "\r\n  " + term.red_reverse + "'%s' login failed."
+  badanon_msg = "\r\n  " + term.bright_red + "'%s' login denied."
+  max_user = int(ini.cfg.get('nua', 'max_user'))
+  allow_apply = ini.cfg.get('nua', 'allow_apply') in ('yes',)
+  topscript = ini.cfg.get('matrix', 'topscript')
+  bbsname = ini.cfg.get('system','bbsname')
+  status_auth = ''.join((
+    term.move (0,0) + term.clear + term.cyan_reverse + '\033#8',
+    term.move (max(0,(term.height /2) -2), max(0,(term.width /2) -10),),' '*20,
+    term.move (max(0,(term.height /2) -1), max(0,(term.width /2) -10),),
+      'encrypting ...'.center (20),
+    term.move (max(0,(term.height /2)), max(0,(term.width /2) -10),),' '*20,
+    term.normal,))
+  status_dirties_screen = True
 
   def denied(msg):
     echo (msg)
@@ -29,8 +44,9 @@ def main ():
     getch (0.7)
 
   def refresh():
-    echo (term.move (0,0) + term.clear)
-    echo ('\r\n%s\r\n' % (ini.cfg.get('system','bbsname'),))
+    flushevent ('refresh')
+    echo (term.move (0,0) + term.clear + term.normal)
+    echo ('\r\n%s\r\n' % (bbsname,))
     echo ('see %s for source\r\n' % (__url__,))
     echo ('\r\n\r\n')
     showfile('art/1984.asc')
@@ -42,9 +58,7 @@ def main ():
   refresh ()
   while True:
     session.activity = 'logging in'
-    echo ('\r\n  user: ')
-    max_user = int(ini.cfg.get('nua', 'max_user'))
-
+    echo (prompt_user)
     handle, event, data = readlineevent \
         (width=max_user, value=handle,
             events=(('refresh','input',)), timeout=TIMEOUT)
@@ -55,13 +69,12 @@ def main ():
 
     if event == 'refresh':
       refresh ()
-      flushevent ('refresh')
 
     if handle == '':
       continue # re-prompt
 
     if handle.lower() in newcmds:
-      if ALLOW_APPLY:
+      if allow_apply:
         # 'new' in your language ..
         gosub ('nua', '')
         refresh()
@@ -71,47 +84,56 @@ def main ():
         denied (term.bright_red + APPLY_DENIED)
 
     elif handle in byecmds:
-      # 'so long,' in whatever your localization ...
       goto ('logoff')
 
     # this account name used to be about warez, not sql injections
     if handle.lower() == 'anonymous':
       if ALLOW_ANONYMOUS:
-        goto (ini.cfg.get('matrix', 'topscript'), 'anonymous')
-      else:
-        denied ("\r\n\r\n%s'%s' login denied." % (term.bright_red, handle,))
+        goto (topscript, 'anonymous')
+      denied (badanon_msg % (handle,))
 
-    # match handle, request to create new account
-    match = finduser(handle)
-    if match is None:
-      if not ALLOW_APPLY:
+    if not DBSessionProxy('userbase').has_key(handle):
+      #remain exactly where you are
+      #make no move, until you are ordered
+      #now we can see you. -i#
+      if allow_apply is False:
         # applications are denied
         denied (term.bright_red + APPLY_DENIED)
         getch (0.8)
         continue
+
+      # the photos of you and the girl will be recycled for prolitarian use
       echo (apply_msg)
       ynq = getch()
+      if str(ynq).lower() == 'q' or ynq == term.KEY_EXIT:
+        goto ('logoff')
       if str(ynq).lower() == 'y':
         goto ('nua', handle)
-      elif str(ynq).lower() == 'q' \
-      or ynq == term.KEY_EXIT:
-        goto ('logoff')
-      else: # 'n' is default
-        continue
-    handle = match
+      continue
 
     # request & authenticate password
     echo ('\r\n\r\n  pass: ')
+
+    chk = session._tap # save
+    session._tap = False
+
+    # get keyboard input
     password, event, data = readlineevent \
         (width=int(ini.cfg.get('nua', 'max_pass')),
             hidden=CH_MASK_PASSWD, timeout=TIMEOUT*2)
+
+    session._tap = chk # restore
+
     if (None, None) == (event, data):
       goto ('logoff')
-    echo (term.move_x(1) + term.clear_eol)
-    (x, y) = getpos(0.15)
-    echo (encrypt_msg)
+
+    if password == '':
+      continue
+
+    echo (status_auth)
     if authuser(handle, password):
       goto (ini.cfg.get('matrix', 'topscript'), handle)
-    if (None,None) != (x, y):
-      echo (term.move (x, y))
-    denied (term.red_reverse + "'%s' login failed." % (handle,))
+    if status_dirties_screen:
+      refresh()
+
+    denied (badpass_msg % (handle,))
