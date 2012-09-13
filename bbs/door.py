55e6a144caada48181a207c1936ca7258f66764d
jquast <dingo@1984.ws>
2012-09-12 23:22:05 -0400

    rewrite door.py. finally. 6 years now?

--- bbs/door.py
+++ bbs/door.py
@@ -1,83 +1,62 @@
-
 import pty
-import sys
+import re
 import os
 import select
+import tty
 
-import util
-import bbs
+from output import echo
+from input import getch, sendch
+from session import logger
 
-#def drop (type, node=0):
-def drop (type):
- if (type == 'door32') or (type == 'door32.sys'):
-  #fopen('drops/' + user['node'] + '/door32.sys', 'w')
-  f = open('./door32.sys', 'w')
-  # Line 1 : Comm type (0=local, 1=serial, 2=telnet)
-  f.write ('2' + '\n')
-  # Line 2 : Comm or socket handle
-  f.write ('0' + '\n')
-  # Line 3 : Baud rate
-  f.write ('115200' + '\n')
-  # Line 4 : BBSID (software name and version)
-  f.write ('lithium v.03' + '\n')
-  # Line 5 : User record position (1-based)
-  f.write ('0' + '\n')
-  # Line 6 : User's real name
-  f.write (str(bbs.cli['username']) + '\n')
-  # Line 7 : User's handle/alias
-  f.write (str(bbs.cli['username']) + '\n')
-  # Line 8 : User's security level
-  f.write ('0' + '\n')
-  # Line 9 : User's time left (in minutes)
-  f.write ('999' + '\n')
-  # Line 10: Emulation (0=ascii, 1=ansi, 2=avatar, 3=rip, 4=max graphics)
-  f.write ('2' + '\n')
-  # Line 11: Current node number
-  f.write ('0' + '\n')
-  f.close ()
+class Door(object):
+  POLL = 0.15
+  BLOCKSIZE = 1920
+  master_fd = None
+  pEIO = re.compile('[eE]rrno 5')
 
-def door (filename):
-  door2 (filename, [], util.echo)
+  def __init__(self, cmd='/bin/uname', args=()):
+    self.cmd = cmd
+    self.args = (self.cmd,) + args
 
-def fdoor (filename, args):
-  door2 (filename, args, util.telnetecho)
+  def run(self):
+    assert self.master_fd is None
+    pid, master_fd = pty.fork()
+    self.master_fd = master_fd
+    if pid == pty.CHILD:
+      os.execlp(self.cmd, *self.args)
+    try:
+      logger.info ('launched : %s %s' % (self.cmd, self.args,))
+      self._loop()
+    except IOError, e:
+      logger.error ('IOError: %s', e)
+    except OSError, e:
+      if self.pEIO.search (str(e)) != None:
+        logger.info ('stdout closed by child')
+        return
+      logger.error ('OSError: %s', e)
+    os.close(master_fd)
+    self.master_fd = None
 
-def door2(filename, args, sendfunc):
-  myfd = bbs.cli['socket'].fileno()
-  print (str(myfd) + '\r\n')
-  args.insert(0,filename)
+  def _loop(self):
+    while True:
+      # block up to self.POLL for screen output
+      try:
+        rfds, wfds, xfds = select.select([self.master_fd,],[],[], self.POLL)
+      except select.error, e:
+        if e[0] == 4:
+          print 'syscall?'
+          continue # log
 
-  pid,ptyfd = pty.fork()
-  if pid == 0:
-    os.execv (filename,args)
-    sys.exit(0)
-  elif pid == -1:
-    print 'Error trying pty.fork()'
+      if self.master_fd in rfds:
+        data = os.read(self.master_fd, self.BLOCKSIZE)
+        echo (data)
 
-  #print pid,fd
-
-  def loop (ptyfd, myfd):
-    while 1:
-      a,b,c = select.select ([ptyfd,myfd],[],[],4)
-      for f in a:
-        if (f == ptyfd):
-          try:
-            buffer = os.read (ptyfd,8192)
-            if (buffer == ''):
-              return
-            sendfunc (buffer)
-#            os.write (myfd,buffer)
-          except OSError:
-            return
-        if (f == myfd):
-          try:
-            buffer = os.read (myfd,8192)
-            # Client disconnected
-            if (buffer == ''):
-              return
-            os.write (ptyfd,buffer)
-          except OSError:
-            return
-  loop (ptyfd, myfd)
-  os.close (ptyfd)
-  os.wait()
+      # block up to self.POLL for keyboard input
+      data = getch(self.POLL)
+      if data is None:
+        continue
+      if type(data) is int:
+        data = sendch (data)
+      while 0 != len(data):
+        n = os.write(self.master_fd, data)
+        data = data[n:]
