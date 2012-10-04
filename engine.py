38975e06b7b513961559121892e805ef1158dae6
jquast <dingo@1984.ws>
2012-10-03 23:29:16 -0400

    specially handle 'cp437' encoding, converting utf-8 art blocks to native encoding then leaving as-is, as though it were iso8859-1. don't fake an ftp like a terminal anymore, don't accept -tap parameters from sys.argv add 'enabled' option for '[ftp]' section in default.ini shuffle about the poll routine for readability,   change refs to terminal.SESSION_CHANNELS to terminal.registry,   stop trying to catch strange thrown errors,   copy registry[:] so .remove doesnt need a eof_errors list,

--- engine.py
+++ engine.py
@@ -7,7 +7,7 @@ __maintainer__ = 'Jeff Quast'
 __email__ = 'dingo@1984.ws'
 __status__ = 'Alpha'
 __version__ = '1.0rc1'
-# iteration #1, pybbs? 2001 jojo, dingo contributing
+# iteration #1, pybbs? 2001 jojo, dingo contributing, public domain
 # iteration #2, The Progressive (PRSV), 2004 jojo, dingo & maze contributing
 #               twisted for networking, zodb for database, ssh support ...
 # Copyright (C) 2004 Johannes Lundberg
@@ -24,7 +24,7 @@ import sys
 import traceback
 import threading
 
-def main (logger, logHandler, cfgFile='default.ini', doTAP=False):
+def main (logger, logHandler, cfgFile='default.ini'):
   """
   x84 main entry point. The system begins and ends here.
   """
@@ -43,18 +43,8 @@ def main (logger, logHandler, cfgFile='default.ini', doTAP=False):
 
   # initialize scripting subsystem
   import bbs.scripting
+  import bbs.cp437
   bbs.scripting.init (bbs.ini.cfg.get('session', 'scriptpath'))
-
-  if doTAP:
-    bbs.ini.cfg.set('session', 'tap_input', 'yes')
-    bbs.ini.cfg.set('session', 'tap_output', 'yes')
-
-  # initialize ftp server
-  import ftp
-  ftp.logger.addHandler (logHandler)
-  # XXX just a hack to get it to poll ...
-  client, pipe, lock = None, ftp.init(), threading.Lock()
-  terminal.SESSION_CHANNELS.append ((client, pipe, lock,))
 
   # initialize telnet server
   import telnet
@@ -62,7 +52,7 @@ def main (logger, logHandler, cfgFile='default.ini', doTAP=False):
   telnet.logger.addHandler (logHandler)
   telnet_port = int(bbs.ini.cfg.get('telnet', 'port'))
   telnet_addr = bbs.ini.cfg.get('telnet', 'addr')
-  server = telnet.TelnetServer \
+  telnet_server = telnet.TelnetServer \
       (port=telnet_port, address=telnet_addr,
        on_connect=terminal.on_connect,
        on_disconnect=terminal.on_disconnect,
@@ -70,59 +60,92 @@ def main (logger, logHandler, cfgFile='default.ini', doTAP=False):
        timeout=0.01)
   logger.info ('[telnet:%s] listening tcp', telnet_port)
 
+  if bbs.ini.cfg.get('ftp', 'enabled', 'no'):
+    # initialize ftp server
+    import ftp
+    ftp.logger.addHandler (logHandler)
+    ftp_eventpipe = ftp.init()
+    #ftp_lock = threading.Lock()
+
   # main event loop
   eof_pipes = set()
   while True:
-    event = server.poll()
-    for client, pipe, lock in terminal.SESSION_CHANNELS:
-      # poll for keyboard input, send to session channel monitor
-      if client is not None and client.input_ready():
-        if lock.acquire(False):
-          inp = client.get_input()
-          lock.release()
-          pipe.send (('input', inp))
+    event, data = (None, None)
 
-      # poll for events received on child process pipe
-      if pipe.poll():
-        try:
-          if not lock.acquire(False):
-            # this client currently 'locked', by POSHandler, likely.
-            continue
-          lock.release ()
-          try:
-            event, data = pipe.recv()
-          except TypeError, e:
-            logger.error (e)
-            continue
-          if event == 'disconnect':
-            client.deactivate ()
-          elif event == 'output':
-            # data to send
-            if not lock.acquire (False):
-              # somebody slipped us a lock. ignore for this pass,
-              continue
-            client.send (*data)
-            lock.release ()
-          elif event == 'global':
-            pass
-#            # broadcast: repeat data as global to all other channels
-#            for (c,p,l) in terminal.SESSION_CHANNELS:
-#              if c != client:
-#                p.send ((event, data))
-          elif event == 'pos':
-            # query: what is the cursor position ?
-            t = terminal.POSHandler(pipe, client, lock, event, data)
-            t.start ()
-          elif event.startswith ('db-'):
-            # query: database dictionary method
-            t = db.DBHandler(pipe, event, data)
-            t.start ()
-          else:
-            assert 0, 'Unhandled event: %s' % ((event,data,),)
-        except EOFError:
-          eof_pipes.add ((client, pipe, lock))
-    while 0 != len(eof_pipes):
-      terminal.SESSION_CHANNELS.remove (eof_pipes.pop())
+    # process telnet server i/o, byte buffers ..
+    telnet_server.poll()
+
+    # process ftp server i/o
+    if bbs.ini.cfg.get('ftp', 'enabled', 'no') == 'yes':
+      if ftp_eventpipe.poll():
+        # ftp child process sent us a gift
+        event, data = ftp_eventpipe.recv()
+
+    for client, pipe, lock in terminal.registry[:]:
+      if not lock.acquire(False):
+        # this client currently 'locked', by POSHandler, likely.
+        continue
+      lock.release ()
+
+      # process telnet input as keypresses 'input' events,
+      if client.input_ready() and lock.acquire(False):
+        lock.release()
+        inp = client.get_input()
+        pipe.send (('input', inp))
+
+      # process bbs session i/o, requests from child processes,
+      if False == pipe.poll ():
+        # no session i/o to process, next client,
+        continue
+
+      try:
+        event, data = pipe.recv()
+
+      except EOFError:
+        logger.info ('eof,')
+        terminal.registry.remove ((client, pipe, lock))
+        continue
+
+      if event == 'disconnect':
+        client.deactivate ()
+
+      # 'output' data in form of (u'unicodestring', encoding=None)
+      elif event == 'output':
+        text, encoding = data
+        if 'cp437' != encoding:
+          # send output args (text, encoding) unmanipulated.
+          client.send_unicode (*data)
+        else:
+          # convert utf8'd cp437 art to real cp437 128-254 equivalents,
+          unibytes = u''.join([unichr(bbs.cp437.CP437.index(glyph))
+              if glyph in bbs.cp437.CP437 else glyph for glyph in text])
+          # and then we go and call it iso8859-1 -- iso8859-1 is the default
+          # encoding for bytes 128-254 in utf-8, leaving our cp437-mapped
+          # characters unmanipulated as possible ..
+          client.send_unicode (unibytes, encoding='iso8859-1')
+
+      # broadcast event to all other telnet clients,
+      elif event == 'global':
+        for (c,p,l) in terminal.registry:
+            if c != client:
+              p.send ((event, data,))
+
+      elif event == 'pos':
+        # query: what is the cursor position ? we answer
+        # with an equivalently named event as a callback mechanism.
+        t = terminal.POSHandler(pipe, client, lock, event, data)
+        t.start ()
+
+      elif event.startswith ('db-'):
+        # db query-> database dictionary method, callback
+        # with a matching ('db-*',) event. sqlite is used
+        # for now and is quick, but this prevents slow
+        # database queries from locking the i/o event loop.
+        t = db.DBHandler(pipe, event, data)
+        t.start ()
+
+      else:
+        assert 0, 'Unhandled event: %s' % ((event,data,),)
 
 
 if __name__ == '__main__':
@@ -135,7 +158,6 @@ if __name__ == '__main__':
   sys.stdout.write ('x/84 bbs ')
   log_level = logging.INFO
   cfgFile = 'default.ini'
-  doTAP=False
   if '-v' in sys.argv:
     sys.argv.remove('-v')
     log_level = logging.DEBUG
@@ -144,8 +166,6 @@ if __name__ == '__main__':
     cfgFile = sys.argv[sys.argv.index('-cfg')+1]
     sys.argv.remove(cfgFile)
     sys.argv.remove('-cfg')
-  if '-tap' in sys.argv:
-    doTAP = True
   logHandler = log.get_stderr(level=log_level)
   sys.stdout.flush()
-  main (logger, logHandler, cfgFile, doTAP)
+  main (logger, logHandler, cfgFile)
