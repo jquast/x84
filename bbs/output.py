519db2c06f9c0fc0cf85e1aee965299b61bc3f1e
jquast <dingo@1984.ws>
2012-10-04 23:06:17 -0400

    add warnings for folk not sending unicode ..

--- bbs/output.py
+++ bbs/output.py
@@ -1,11 +1,25 @@
+import warnings
+
 from session import getsession
 
 def delay(n):
     getsession().read_event([], seconds)
 
 def echo(data, encoding=None):
-    return getsession().write(data, encoding)
+  if data is None or 0 == len(data):
+    warnings.warn ('terminal capability not translated: %s%r' % \
+        (encoding if encoding is not None \
+        else '', data,), Warning, 2)
+  if type(data) is bytes:
+    warnings.warn('non-unicode: %s%r' % \
+        (encoding if encoding is not None \
+        else '', data,), UnicodeWarning, 2)
+    return getsession().write \
+        (data.decode(encoding if encoding is not None else 'iso8859-1'))
+
+  assert encoding is not None, 'just send unicode'
+  # thanks for using unicode !
+  return getsession().write (data)
 
 def oflush():
-  import warnings
   warnings.warn('oflush() is deprecated', DeprecationWarning, 2)
