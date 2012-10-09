df29d6406400a53e1288ac7369c8c58c1606a314
jquast <dingo@1984.ws>
2012-10-09 17:26:18 -0400

    unicode fixes

--- default/charset.py
+++ default/charset.py
@@ -10,10 +10,10 @@ def main():
   choice_2 = 'cp437'
   choice_1txt = choice_1 + " (YES!)"
   choice_2txt = choice_2 + " (OldSChOOliN' iT)"
-  enc_prompt = "Use %s encoding? Press left/right until artwork " \
-      "looks best. Adjust your terminal encoding or font if " \
-      "necessary. %s is preferred, otherwse only %s is supported. " \
-      "Press return to accept selection." % (choice_1txt, choice_1,
+  enc_prompt = u"Use %s encoding? Press left/right until artwork " \
+      u"looks best. Adjust your terminal encoding or font if " \
+      u"necessary. %s is preferred, otherwse only %s is supported. " \
+      u"Press return to accept selection." % (choice_1txt, choice_1,
           choice_2txt,)
 
   def refresh(enc):
@@ -23,21 +23,21 @@ def main():
     if enc == 'cp437':
       # ESC %@ goes back from UTF-8 to ISO 2022 in case UTF-8 had been entered
       # via ESC %G.
-      echo ('\033%@')
+      echo (u'\033%@')
       # ESC ) K or ESC ) U Sets character set G1 to codepage 437, for examble
       # linux vga console
-      echo ('\033)K')
-      echo ('\033)U')
+      echo (u'\033)K')
+      echo (u'\033)U')
     elif enc == 'utf8':
       # ESC %G activates UTF-8 with an unspecified implementation level from
       # ISO 2022 in a way that allows to go back to ISO 2022 again.
-      echo ('\033%G')
+      echo (u'\033%G')
 
     # clear & display art
     echo (term.move (0,0) + term.clear)
     showfile (artfile)
-    echo (term.normal + '\r\n\r\n')
-    echo ('\r\n'.join(textwrap.wrap(enc_prompt, term.width-3)) + '\r\n')
+    echo (term.normal + u'\r\n\r\n')
+    echo (u'\r\n'.join(textwrap.wrap(enc_prompt, term.width-3)) + u'\r\n')
 
     # create left/right encoding selector
     state = LeftRightClass.LEFT if enc == choice_1 else LeftRightClass.RIGHT
@@ -72,7 +72,7 @@ def main():
           lightbar = refresh (senc)
         user.set ('charset', senc)
         user.save ()
-        echo ("\r\n\r\n'%s' is now your preferred charset.\r\n" % \
+        echo (u"\r\n\r\n'%s' is now your preferred charset.\r\n" % \
             (user.get('charset'),))
         return
       elif lightbar.state != lightbar.laststate:
