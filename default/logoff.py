7e2494ef666b68401385983a8fd99122788ea5ad
jquast <jeff@quast.co.uk>
2012-10-08 23:16:34 -0400

    unicodify, also getpos() is causing the spurious __new__ to occur -- get on it ..

--- default/logoff.py
+++ default/logoff.py
@@ -13,21 +13,24 @@ def main():
       if session.user is not None else False
   is_sysop = session.user.is_sysop \
       if session.user is not None else False
-  session.activity = 'Logging Off!'
-  prompt_msg = '[spnG]: ' if expert is True \
-      else '%s:AY SOMEthiNG %s:REViOUS %s:EXt %s:Et thE fUCk Off !\b' \
+  session.activity = u'Logging Off!'
+  prompt_msg = u'[spnG]: ' if expert is True \
+      else u'%s:AY SOMEthiNG %s:REViOUS %s:EXt %s:Et thE fUCk Off !\b' \
       % (''.join((term.cyan_reverse, 's', term.normal, term.bold_black,)),
         ''.join((term.white_on_blue, 'p', term.normal, term.bold_black,)),
         ''.join((term.white_on_blue, 'n', term.normal, term.bold_black,)),
         ''.join((term.red_reverse, 'g', term.normal, term.bold_black,)),)
-  prompt_say = ''.join \
+  prompt_say = u''.join \
       ((term.bold_blue, session.handle \
-        if session.user is not None else 'PROlE', term.normal,
-        ' sAYs ', term.cyan_reverse, 'WhAt: ', term.normal,))
-  goodbye_msg = ''.join((term.black_bold, '\r\n\r\n',
-          'back to the mundane world...', '\r\n',))
-  commit_msg = '-- !  thANk YOU fOR YOUR CONtRibUtiON, bROthER  ! --'
-  newDb = ((time.time() -1984, 'B. b.', 'bEhAVE YOURSElVES ...'),)
+        if session.user is not None else u'PROlE', term.normal,
+        u' sAYs ', term.cyan_reverse, u'WhAt: ', term.normal,))
+  goodbye_msg = u''.join((term.black_bold, u'\r\n\r\n',
+          u'back to the mundane world...', u'\r\n',))
+  demerit_msg = u''.join((term.black_bold, u'\r\n\r\n',
+        u'tiMEOUt: 1 dEMERit.', '\r\n',))
+  commit_msg = u'-- !  thANk YOU fOR YOUR CONtRibUtiON, bROthER  ! --'
+  write_msg = u'bURNiNG tO ROM, PlEASE WAiT ...'
+  newDb = ((time.time() -1984, u'B. b.', u'bEhAVE YOURSElVES ...'),)
   chk_next = ('n', 'N', term.KEY_DOWN, term.KEY_NPAGE,)
   chk_prev = ('p', 'P', term.KEY_UP, term.KEY_PPAGE,)
   chk_say  = ('s', 'S',)
@@ -38,7 +41,7 @@ def main():
     """Refresh automsg prompt using string msg"""
     echo (term.move (max(14, (term.height -6)),
                      max(6, (term.width /2) -(AUTOMSG_LENGTH /2) -8)))
-    echo (''.join((term.normal, term.clear_eol)))
+    echo (u''.join((term.normal, term.clear_eol)))
     echo (msg)
 
   def refresh_automsg(idx):
@@ -49,8 +52,8 @@ def main():
     flushevent ('automsg')
     echo (term.move(max(12, (term.height/2)),
                     max(3, (term.width /2) -(AUTOMSG_LENGTH /2) -10)))
-    echo (''.join((term.normal, term.clear_eol, term.bold_black,)))
-    echo (' ... ') # loading
+    echo (u''.join((term.normal, term.clear_eol, term.bold_black,)))
+    echo (u' ... ') # loading
     automsgs = sorted(db.values()) if len(db) else newDb
     idx = len(automsgs) -1 if idx < 0 \
         else 0 if idx > len(automsgs) -1 \
@@ -62,7 +65,7 @@ def main():
       term.normal, ': ', term.cyan_reverse, msg \
         .ljust(AUTOMSG_LENGTH -10),))
     artago = ''.join((term.blue_reverse,
-      ('%s ago' % (timeago(time.time() -t),)) \
+      (u'%s ago' % (timeago(time.time() -t),)) \
           .ljust (AUTOMSG_LENGTH -ansilen(artmsg))))
     # output & return new index
     echo (term.normal.join((artnick, artmsg, ' ', artago,)))
@@ -85,15 +88,12 @@ def main():
     event, data = readevent(events=chk_events, timeout=TIMEOUT)
     if (event, data) == (None, None):
       # timeout
-      logger.info ('logoff/automsg timeout exceeded')
-      echo (''.join((term.black_bold, '\r\n\r\n',
-        'tiMEOUt: 1 dEMERit.', '\r\n',)))
-      disconnect ()
+      raise ConnectionTimeout, 'login prompt'
     if event == 'refresh':
       idx = refresh_all ()
     elif event == 'automsg':
       refresh_automsg (-1)
-      echo ('\a') # bel
+      echo (u'\a') # bel
     elif event == 'input':
       if data in chk_bye:
         echo (goodbye_msg)
@@ -105,17 +105,18 @@ def main():
       elif data in chk_say:
         # new prompt: say something !
         refresh_prompt (prompt_say)
-        (y, x) = getpos()
-        if (None,None) != (y, x):
-          echo (term.white_reverse)
-          echo (' ' * AUTOMSG_LENGTH)
+        (y, x) = getpos(None)
+        if (None, None) != (y, x):
+          # illegal ? white_reverse ?
+          echo (term.cyan_reverse)
+          echo (u' ' * AUTOMSG_LENGTH)
           echo (term.move (y, x))
         msg = readline(AUTOMSG_LENGTH)
         if len(msg.strip()):
           if (None,None) != (y, x):
             echo (term.move (y, x))
             echo (term.clear_bol)
-            echo ('bURNiNG tO ROM, PlEASE WAiT ...' .rjust(AUTOMSG_LENGTH))
+            echo (write_msg .rjust(AUTOMSG_LENGTH))
           idx = len(db)
           db[idx] = (time.time(), nick, msg.strip())
           broadcastevent ('automsg', (nick, msg.strip(),))
