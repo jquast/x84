6d459757149cef2343e016e2c2cf31cb98201a83
jquast <dingo@1984.ws>
2012-10-10 18:49:28 -0400

    PEP-8/pylint

--- bbs/exception.py
+++ bbs/exception.py
@@ -1,29 +1,27 @@
 """
-Custom exception classes for 'The Progressive' BBS.
-Copyright (C) 2005 Johannes Lundberg.
-$Id: exception.py,v 1.4 2008/06/08 22:17:15 dingo Exp $
+Custom exception classes for x/84 BBS https://github.com/jquast/x84
 """
 
 class Disconnect(Exception):
-  "Raised when the bbs closes a connection"
-  pass
+    """Raise this error to cause the socket to be disconnected."""
+    pass
+
 
 class ConnectionClosed (Exception):
-  "Raised when the socket closes connection"
-  pass
+    """This exception thrown when client closes connection."""
+    pass
+
 
 class ConnectionTimeout (Exception):
-  "Raised when client idle time exceeds threshold"
-  pass
+    """Raised this error to indicate idle time exceeded."""
+    pass
 
-class SilentTermination (Exception):
-  "Raised to silently terminate a session"
-  pass
 
 class Goto (Exception):
-  "Raised whenever a script wants to travel&exchange itself to another"
-  pass
+    """Raised whenever a script wants to travel&exchange itself to another."""
+    pass
+
 
 class ScriptError (Exception):
-  "Raised by runscript when there is an error running the script"
-  pass
+    """This exception thrown when runscript fails to locate script."""
+    pass
