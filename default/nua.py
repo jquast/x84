9c3fda11fe55ea0e79cb2f31fd0e122007dc4445
jquast <dingo@1984.ws>
2012-10-10 18:55:54 -0400

    indentation only

--- default/nua.py
+++ default/nua.py
@@ -17,151 +17,151 @@ loc_prompt = (23, 35)
 loc_yesno  = (62, 23) #23, 62)
 
 def main (handle):
-  import os
-  if handle.lower() in ('new',):
-    handle = u''
-  location, hint = '', ''
-  password, verify, = '', ''
-  session = getsession()
-  terminal = getterminal()
+    import os
+    if handle.lower() in ('new',):
+        handle = u''
+    location, hint = '', ''
+    password, verify, = '', ''
+    session = getsession()
+    terminal = getterminal()
 
-  def warning(msg):
-    " Display warning to user with a dynamic pause "
-    cpsec =  10.0
-    min_sec = 3
-    split_loc = 3
-    warning_msg = ''.join((
-        terminal.clear_eol,
-        terminal.normal, terminal.red, msg[:-split_loc],
-        terminal.normal, msg[-split_loc:],
-        terminal.bright_black, '!'))
-    echo (warning_msg)
-    inkey = getch(max(min_sec,float(len(msg))/cpsec))
-    echo (terminal.clear_bol)
-    return inkey
+    def warning(msg):
+        " Display warning to user with a dynamic pause "
+        cpsec =  10.0
+        min_sec = 3
+        split_loc = 3
+        warning_msg = ''.join((
+            terminal.clear_eol,
+            terminal.normal, terminal.red, msg[:-split_loc],
+            terminal.normal, msg[-split_loc:],
+            terminal.bright_black, '!'))
+        echo (warning_msg)
+        inkey = getch(max(min_sec,float(len(msg))/cpsec))
+        echo (terminal.clear_bol)
+        return inkey
 
-  session.activity = 'New User Application'
-  echo (terminal.clear + terminal.normal)
-  showfile ('art/newuser.asc')
-  echo ( 'New User Application'.center (terminal.width-1) + '\r\n')
+    session.activity = 'New User Application'
+    echo (terminal.clear + terminal.normal)
+    showfile ('art/newuser.asc')
+    echo ( 'New User Application'.center (terminal.width-1) + '\r\n')
 
-  while True:
-    user_ok = origin_ok = pass_ok = level = 0
-    while not (user_ok):
-      echo (terminal.move (*loc_user))
-      echo (terminal.clear_eol + terminal.normal)
-      echo ('username: ')
-      handle = readline (int(ini.cfg.get('nua', 'max_user')), handle)
-      echo (terminal.move (*loc_user))
-      if not handle:
-        inkey = warning('Enter an alias, Press Ctrl+X to cancel')
-        if inkey == chr(24):
-          return
-      elif finduser (handle):
-        warning ('User exists')
-      elif handle == '' or len(handle) < int(ini.cfg.get('nua', 'min_user')):
-        warning ('Too short! (%s)' % ini.cfg.get('nua', 'min_user'))
-      elif handle.lower() in ini.cfg.get('nua', 'invalid_handles').split():
-        warning ('Illegal username')
-      elif os.path.sep in handle:
-        # handle is often used for filenames, like tty recordings, so avoid
-        # allowing usernames with '/' inside the nickname
-        warning ('Illegal username')
-      elif ':' in handle:
-        # hrm,
-        warning ('Illegal username')
-      else:
-        user_ok = True
+    while True:
+        user_ok = origin_ok = pass_ok = level = 0
+        while not (user_ok):
+            echo (terminal.move (*loc_user))
+            echo (terminal.clear_eol + terminal.normal)
+            echo ('username: ')
+            handle = readline (int(ini.cfg.get('nua', 'max_user')), handle)
+            echo (terminal.move (*loc_user))
+            if not handle:
+                inkey = warning('Enter an alias, Press Ctrl+X to cancel')
+                if inkey == chr(24):
+                    return
+            elif finduser (handle):
+                warning ('User exists')
+            elif handle == '' or len(handle) < int(ini.cfg.get('nua', 'min_user')):
+                warning ('Too short! (%s)' % ini.cfg.get('nua', 'min_user'))
+            elif handle.lower() in ini.cfg.get('nua', 'invalid_handles').split():
+                warning ('Illegal username')
+            elif os.path.sep in handle:
+                # handle is often used for filenames, like tty recordings, so avoid
+                # allowing usernames with '/' inside the nickname
+                warning ('Illegal username')
+            elif ':' in handle:
+                # hrm,
+                warning ('Illegal username')
+            else:
+                user_ok = True
 
-    while not (origin_ok):
-      echo (terminal.move (*loc_origin))
-      echo (terminal.clear_eol + terminal.normal)
-      echo ('origin: ')
-      location = readline (int(ini.cfg.get('nua', 'max_origin')), location)
-      echo (terminal.move (*loc_origin))
-      if location == '':
-        inkey = warning('Enter a location, Press Ctrl+X to cancel')
-        if inkey == chr(24):
-          return
-        echo (terminal.clear_eol)
-      else:
-        origin_ok = True
+        while not (origin_ok):
+            echo (terminal.move (*loc_origin))
+            echo (terminal.clear_eol + terminal.normal)
+            echo ('origin: ')
+            location = readline (int(ini.cfg.get('nua', 'max_origin')), location)
+            echo (terminal.move (*loc_origin))
+            if location == '':
+                inkey = warning('Enter a location, Press Ctrl+X to cancel')
+                if inkey == chr(24):
+                    return
+                echo (terminal.clear_eol)
+            else:
+                origin_ok = True
 
-    while not (pass_ok):
-      echo (terminal.move(*loc_pass))
-      echo (terminal.clear_eol + terminal.normal)
-      echo ('password: ')
-      password = readline (int(ini.cfg.get('nua', 'max_pass')), hidden='x')
-      echo (terminal.move(*loc_pass))
-      if len(password) < 4:
-        # fail if password too short
-        echo (terminal.move (*loc_email))
-        inkey = warning('too short, Press Ctrl+X to cancel')
-        if inkey == chr(24):
-          return
-        echo (terminal.clear_eol)
-      else:
-        # verify
+        while not (pass_ok):
+            echo (terminal.move(*loc_pass))
+            echo (terminal.clear_eol + terminal.normal)
+            echo ('password: ')
+            password = readline (int(ini.cfg.get('nua', 'max_pass')), hidden='x')
+            echo (terminal.move(*loc_pass))
+            if len(password) < 4:
+                # fail if password too short
+                echo (terminal.move (*loc_email))
+                inkey = warning('too short, Press Ctrl+X to cancel')
+                if inkey == chr(24):
+                    return
+                echo (terminal.clear_eol)
+            else:
+                # verify
+                echo (terminal.clear_eol + terminal.normal)
+                echo ('   again: ')
+                verify = readline (int(ini.cfg.get('nua', 'max_pass')), hidden='z')
+                echo (terminal.move(*loc_pass))
+                if password != verify:
+                    inkey = warning ('verify must match, Press Ctrl+X to cancel')
+                    if inkey  == chr(24):
+                        return
+                    echo (terminal.clear_eol)
+                else:
+                    break
+
+        # this is a joke?
+        while (level < 2):
+            echo (terminal.move(*loc_email))
+            # email loop
+            echo (terminal.clear_eol + terminal.normal)
+            echo ('e-mail (optional): ')
+            hint= readline (int(ini.cfg.get('nua', 'max_email')))
+            echo (terminal.move(*loc_email))
+            # TODO regexp
+            if not len(hint):
+                level = 2
+                break # no e-mail
+            for ch in hint:
+                # must have @, level 1
+                if ch == '@':
+                    level = 1
+                # must have '.' following @, level 2
+                if level == 1 and ch == '.':
+                    level = 2
+                    break
+            if level == 2:
+                # email is valid, break out
+                break
+
+            # allow user to opt-out of e-mail
+            echo (terminal.move (*loc_state))
+            inkey = warning('invalid, Ctrl+O to Opt out')
+            echo (terminal.move (*loc_state))
+            if inkey == chr(15):
+                echo (terminal.clear_eol + terminal.normal)
+                echo ('make your statement, then: ')
+                hint = readline (int(ini.cfg.get('nua', 'max_email')))
+                if not hint:
+                    return
+                break
+
+        echo (terminal.move (*loc_prompt))
         echo (terminal.clear_eol + terminal.normal)
-        echo ('   again: ')
-        verify = readline (int(ini.cfg.get('nua', 'max_pass')), hidden='z')
-        echo (terminal.move(*loc_pass))
-        if password != verify:
-          inkey = warning ('verify must match, Press Ctrl+X to cancel')
-          if inkey  == chr(24):
-            return
-          echo (terminal.clear_eol)
-        else:
-          break
+        echo ('   Everything cool?')
 
-    # this is a joke?
-    while (level < 2):
-      echo (terminal.move(*loc_email))
-      # email loop
-      echo (terminal.clear_eol + terminal.normal)
-      echo ('e-mail (optional): ')
-      hint= readline (int(ini.cfg.get('nua', 'max_email')))
-      echo (terminal.move(*loc_email))
-      # TODO regexp
-      if not len(hint):
-        level = 2
-        break # no e-mail
-      for ch in hint:
-        # must have @, level 1
-        if ch == '@':
-          level = 1
-        # must have '.' following @, level 2
-        if level == 1 and ch == '.':
-          level = 2
-          break
-      if level == 2:
-        # email is valid, break out
-        break
-
-      # allow user to opt-out of e-mail
-      echo (terminal.move (*loc_state))
-      inkey = warning('invalid, Ctrl+O to Opt out')
-      echo (terminal.move (*loc_state))
-      if inkey == chr(15):
-        echo (terminal.clear_eol + terminal.normal)
-        echo ('make your statement, then: ')
-        hint = readline (int(ini.cfg.get('nua', 'max_email')))
-        if not hint:
-          return
-        break
-
-    echo (terminal.move (*loc_prompt))
-    echo (terminal.clear_eol + terminal.normal)
-    echo ('   Everything cool?')
-
-    lr = YesNoClass(loc_yesno)
-    lr.left ()
-    lr.run()
-    if lr.isleft():
-      # handle, password, location, hint
-      u = User (handle=handle.decode('utf-8'),
-          password=password.decode('utf-8'),
-          location=location.decode('utf-8'),
-          hint=hint.decode('utf-8'))
-      u.save ()
-      goto (ini.cfg.get('matrix', 'topscript'), u.handle)
+        lr = YesNoClass(loc_yesno)
+        lr.left ()
+        lr.run()
+        if lr.isleft():
+            # handle, password, location, hint
+            u = User (handle=handle.decode('utf-8'),
+                password=password.decode('utf-8'),
+                location=location.decode('utf-8'),
+                hint=hint.decode('utf-8'))
+            u.save ()
+            goto (ini.cfg.get('matrix', 'topscript'), u.handle)
