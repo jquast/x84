eb947191fb90e5e49344ec4f228b12e8f254b176
jquast <dingo@1984.ws>
2012-10-10 18:52:21 -0400

    PEP-8 indentation only

--- bbs/output.py
+++ bbs/output.py
@@ -6,20 +6,20 @@ def delay(n):
     getsession().read_event([], seconds)
 
 def echo(data, encoding=None):
-  if data is None or 0 == len(data):
-    warnings.warn ('terminal capability not translated? %s' % \
-        ('encoding=%s'%(encoding,) if encoding is not None else '',),
-        Warning, 2)
-  if type(data) is bytes:
-    warnings.warn('non-unicode: %s%r' % \
-        (encoding if encoding is not None \
-        else '', data,), UnicodeWarning, 2)
-    return getsession().write \
-        (data.decode(encoding if encoding is not None else 'iso8859-1'))
+    if data is None or 0 == len(data):
+        warnings.warn ('terminal capability not translated? %s' % \
+            ('encoding=%s'%(encoding,) if encoding is not None else '',),
+            Warning, 2)
+    if type(data) is bytes:
+        warnings.warn('non-unicode: %s%r' % \
+            (encoding if encoding is not None \
+            else '', data,), UnicodeWarning, 2)
+        return getsession().write \
+            (data.decode(encoding if encoding is not None else 'iso8859-1'))
 
-  assert encoding is None, 'just send unicode'
-  # thanks for using unicode !
-  return getsession().write (data)
+    assert encoding is None, 'just send unicode'
+    # thanks for using unicode !
+    return getsession().write (data)
 
 def oflush():
-  warnings.warn('oflush() is deprecated', DeprecationWarning, 2)
+    warnings.warn('oflush() is deprecated', DeprecationWarning, 2)
