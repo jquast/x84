00bcbac62c3768e9a184142a5e448ac8c0363a1a
jquast <dingo@1984.ws>
2012-10-10 18:44:01 -0400

    remove loginuser(), unused. otherwise pylint and PEP-8 adherance

--- bbs/__init__.py
+++ bbs/__init__.py
@@ -1,7 +1,17 @@
+"""
+bbs module for X/84 BBS https://github.com/jquast/x84
+everything in the bbs package is exported as a global to all bbs session
+scripts. It is as if the statement:
+
+    from bbs import *
+
+Is implied for all session scripts.
+"""
 import msgbase # FIX, work on msging...
 import ini
 from exception import Disconnect, Goto, ConnectionTimeout
-from strutils import chompn, asctime, timeago, ansilen, chkseq, seqp, seqc, maxanswidth
+from strutils import chompn, asctime, timeago, ansilen
+from strutils import chkseq, seqp, seqc, maxanswidth
 from cp437 import fromCP437
 from door import Door
 from dbproxy import DBSessionProxy
@@ -71,74 +81,106 @@ __all__ = [
     'SAUCE',]
 
 def getterminal():
-  return getsession().terminal
+    """
+    Return blessings terminal instace of this session.
+    """
+    return getsession().terminal
 
 
 def gethandle():
-  return getsession().handle
+    """
+    Return user handle of this session.
+    """
+    return getsession().handle
 
 
 def disconnect():
-  raise exception.Disconnect('disconnect')
+    """
+    Disconnect the session
+    """
+    raise Disconnect('disconnect')
 
 
-def goto(*arg):
-  raise exception.Goto(arg)
+def goto(*args):
+    """
+    Switch to another bbs script, with arguments. Do not return.
+    """
+    raise Goto(args)
 
 
 def gosub(*arg):
-  return getsession().runscript(*(arg[0],) + arg[1:])
+    """
+    Switch to another bbs script, with arguments. Returns script return value.
+    """
+    return getsession().runscript(*(arg[0],) + arg[1:])
 
 
 def sendevent(pid, event, data):
-  return getsession().send_event('event', (pid, event, data))
+    """
+    Send an event to the main bbs engine.
+    """
+    return getsession().send_event('event', (pid, event, data))
 
 
 def broadcastevent(event, data):
-  return getsession().send_event('global', (getsession().pid, event, data))
+    """
+    Broadcast an event to all other sessions.
+    """
+    return getsession().send_event('global', (getsession().pid, event, data))
 
 
 def readevent(events = ['input'], timeout = None):
-  return getsession().read_event(events, timeout)
+    """
+    Poll for and read an event from the main bbs engine.
+    """
+    return getsession().read_event(events, timeout)
 
 
 def flushevent(event = 'input', timeout = -1):
-  return getsession().flush_event(event, timeout)
+    """
+    Remove any data for specified event buffered from the main bbs engine.
+    """
+    return getsession().flush_event(event, timeout)
 
 
 def flushevents(events = ['input'], timeout = -1):
-  return [flushevent(e, timeout) for e in events]
+    """
+    Remove any data for specified events buffered from the main bbs engine.
+    """
+    return [flushevent(e, timeout) for e in events]
 
 
-def loginuser(handle):
-  import time as time
-  u = userbase.getuser(handle)
-  u.calls += 1
-  u.lastcall = time.time()
+def showfile (filename, bps=0, pause=0.1, cleansauce=True,
+        file_encoding='cp437'):
+    """
+    Display a file to the user. This is different from a echo(open().read()) in
+    several ways:
+        1. by default, it presumes the file is in CP437 encoding, and
+        translates to requivalent unicode bytes. CP437 encoding is used for
+        "ansi art".
+        2. it is possible to simulate a bits-per-second speed
+    """
+    # this is for ansi art, really. if you're not doing that, use .read() and
+    # such. this is overkill u kno?
+    # when unspecified, session interprets charset of file
+    # open a random file if '*' or '?' is used in filename (glob matching)
+    fobj = ropen(filename, 'rb') \
+      if '*' in filename or '?' in filename \
+        else fopen(filename, 'rb')
 
+    data = chompn(SAUCE(fobj).__str__() if cleansauce else fobj.read())
+    if file_encoding == 'cp437':
+        data = fromCP437(data)
+    else:
+        data = data.decode(file_encoding)
+    if 0 == bps:
+        echo (data)
+        echo (getterminal().normal)
+        return
 
-def showfile(filename, bps=0, pause=0.1, cleansauce=True, file_encoding='cp437'):
-  # this is for ansi art, really. if you're not doing that, use .read() and
-  # such. this is overkill u kno?
-  # when unspecified, session interprets charset of file
-  # open a random file if '*' or '?' is used in filename (glob matching)
-  fobj = ropen(filename, 'rb') \
-    if '*' in filename or '?' in filename \
-      else fopen(filename, 'rb')
-  session_encoding = getsession().encoding
-  data = chompn(SAUCE(fobj).__str__() if cleansauce else fobj.read())
-  if file_encoding == 'cp437':
-    data = fromCP437(data)
-  else:
-    data = data.decode(file_encoding)
-  if 0 == bps:
-    echo (data)
-    echo (getterminal().normal)
-    return
-
-  # display at a timed speed; re-expereince the pace of 9600bps ...
-  cpp = int((float(bps)/8) *pause)
-  for n, ch in enumerate(data):
-    if 0 == (n % cpp):
-      getsession().read_event(events=['input'], timeout=pause)
-    echo (ch)
+    # display at a timed speed; re-expereince the pace of 9600bps ...
+    cpp = int((float(bps)/8) *pause)
+    for num, char in enumerate(data):
+        if 0 == (num % cpp):
+            getsession().read_event(events=['input'], timeout=pause)
+        echo (char)
