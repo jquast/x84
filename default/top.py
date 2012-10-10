7e86d3865f6f28b707005e0124df6a3ee86becee
jquast <dingo@1984.ws>
2012-10-10 18:56:48 -0400

    indentation only

--- default/top.py
+++ default/top.py
@@ -3,45 +3,45 @@
 """
 
 def main(login_handle=None):
-  import time
-  user = User() if login_handle in (None, 'anonymous') \
-      else getuser(login_handle)
-  user.calls += 1
-  user.lastcall = time.time()
-  user.save ()
+    import time
+    user = User() if login_handle in (None, 'anonymous') \
+        else getuser(login_handle)
+    user.calls += 1
+    user.lastcall = time.time()
+    user.save ()
 
 
-  session = getsession()
-  session.activity = 'Intro screen'
-  session.user = user
-  term = session.terminal
+    session = getsession()
+    session.activity = 'Intro screen'
+    session.user = user
+    term = session.terminal
 
-  # rebuild last caller db
-  gosub('lc', True)
+    # rebuild last caller db
+    gosub('lc', True)
 
-  # figure out character set
-  gosub('charset')
+    # figure out character set
+    gosub('charset')
 
-  # check for new messages
-  gosub('chkmsgs')
+    # check for new messages
+    gosub('chkmsgs')
 
-  # ?quick login
-  echo ('\r\n\r\nQuick login? [yn] ')
-  while True:
-    k = getch()
-    if k in ('y', 'Y', 'q'):
-      goto ('main')
-    elif k in ('n', 'N'):
-      break
+    # ?quick login
+    echo ('\r\n\r\nQuick login? [yn] ')
+    while True:
+        k = getch()
+        if k in ('y', 'Y', 'q'):
+            goto ('main')
+        elif k in ('n', 'N'):
+            break
 
-  # long login
-  # ... last callers
-  gosub('lc')
+    # long login
+    # ... last callers
+    gosub('lc')
 
-  # ... news
-  gosub('news')
+    # ... news
+    gosub('news')
 
-  # ... one liners
-  gosub('ol')
+    # ... one liners
+    gosub('ol')
 
-  goto('main')
+    goto('main')
