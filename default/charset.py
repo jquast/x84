07842a306902dc99c2203c8232849d23fb1322c3
jquast <jeff@quast.co.uk>
2012-10-19 18:08:08 -0400

    simplify, document.

--- default/charset.py
+++ default/charset.py
@@ -1,74 +1,71 @@
 """
-This script helps the user select between cp437 and utf8 encoding.
+Session script for x/84, https://github.com/jquast/x84
+
+This script displays a CP437 artwork (block ansi), and prompts the user to
+chose 'utf8' or 'cp437' encoding. Other than the default, 'utf8', the
+Session.write() method takes special handling of a session.encoding value
+of 'cp437' for encoding translation.
 """
 def main():
     import textwrap
     session, term = getsession(), getterminal()
-    user = session.user
     artfile = 'art/plant.ans'
-    choice_1 = 'utf8' # default
-    choice_2 = 'cp437'
-    choice_1txt = choice_1 + " (YES!)"
-    choice_2txt = choice_2 + " (OldSChOOliN' iT)"
-    enc_prompt = u"Use %s encoding? Press left/right until artwork " \
-        u"looks best. Adjust your terminal encoding or font if " \
-        u"necessary. %s is preferred, otherwse only %s is supported. " \
-        u"Press return to accept selection." % (choice_1txt, choice_1,
-            choice_2txt,)
-    bar_width = max(8, (term.width/2) -10)
-    selector = Selector (yloc=term.height-1, xloc=5, width=bar_width)
-    selector.left = choice_1txt
-    selector.right = choice_2txt
-    selector.selection = choice_1txt
+    enc_prompt = u"Press left/right until artwork looks best. Clients " \
+            "should select utf8 encoding, older clients or clients with " \
+            "appropriate 8-bit fontsets can select cp437."
 
-    def refresh(enc):
-        getsession().encoding = enc
-        if enc == 'cp437':
-            # ESC %@ goes back from UTF-8 to ISO 2022 in case UTF-8 had been entered
-            # via ESC %G.
+    def get_selector(selection):
+        """
+        Instantiate a new selector, dynamicly for the window size.
+        """
+        width = max(30, (term.width/2) - 10)
+        xloc = min(0, (term.width/2)-bar_width)
+        selector = Selector (yloc=term.height-1, xloc=xloc, width=width,
+                left='utf8', right='cp437')
+        selector.selection = selection
+        return selector
+
+    def refresh(sel):
+        if sel.selection == 'utf8':
+            # ESC %G activates UTF-8 with an unspecified implementation
+            # level from ISO 2022 in a way that allows to go back to
+            # ISO 2022 again.
+            echo (u'\033%G')
+        elif sel.selection == 'cp437':
+            # ESC %@ returns to ISO 2022 in case UTF-8 had been entered.
+            # ESC ) K or ESC ) U Sets character set G1 to codepage 437.
             echo (u'\033%@')
-            # ESC ) K or ESC ) U Sets character set G1 to codepage 437, for examble
-            # linux vga console
             echo (u'\033)K')
             echo (u'\033)U')
-        elif enc == 'utf8':
-            # ESC %G activates UTF-8 with an unspecified implementation level from
-            # ISO 2022 in a way that allows to go back to ISO 2022 again.
-            echo (u'\033%G')
-
+        else:
+            assert False, "Only encodings 'utf8' and 'cp437' supported."
         # clear & display art
-        echo (term.move (0,0) + term.clear)
+        echo (term.move (0,0) + term.normal + term.clear)
         showfile (artfile)
         echo (term.normal + u'\r\n\r\n')
         echo (u'\r\n'.join(textwrap.wrap(enc_prompt, term.width-3)) + u'\r\n')
-        echo (selector.refresh ())
+        echo (sel.refresh ())
 
-    if user.get('charset', None) is None:
-        # user has no preferred charset, use session-detected/bbs-default
-        senc = session.encoding
-    else:
-        senc = user.get('charset')
-
-    refresh (choice_1) # default is 'utf8'
-
+    selector = get_selector ('utf8')
+    refresh ()
     while True:
-        (ev, data) = readevent(('input','refresh',),
-            int(ini.cfg.get('session','timeout')))
-        if (ev, data) == (None, None):
-            raise ConnectionTimeout ('timeout selecting character set')
-        if ev == 'input':
-            if data in (u'\r', term.KEY_ENTER):
-                # return was pressed
-                set_enc = choice_1 if selector.selection == selector.left else choice_2
-                user.set ('charset', set_enc)
-                user.save ()
-                echo (u"\r\n\r\n'%s' is now your preferred charset.\r\n" %
-                        (user.get('charset'),))
+        ch = getch ()
+        if ch == term.KEY_ENTER:
+            session.user.set ('charset', session.encoding)
+            session.user.save ()
+            echo (u"\r\n\r\n'%s' is now your preferred encoding.\r\n"
+                    % (session.encoding,))
+            return
+        else:
+            selector.process_keystroke (ch)
+            if selector.quit:
+                # 'escape' quits without save, though the encoding
+                # has been temporarily set for this session.
                 return
-            echo (selector.process_keystroke (data))
-            #if selector.quit:
-            #    return
-        if ev == 'refresh' or selector.moved:
-        # re-locate lightbar; re-display art & prompt
-            refresh(session.encoding)
-            #selector.state = selector.laststate
+            if selector.moved:
+                # set and refresh art in new encoding
+                session.encoding = selector.selection
+                refresh ()
+        if readevent('refresh', 0) is not None:
+            # instantiate a new selector in case the window size has changed.
+            selector = get_selector (session.encoding)
