d897be4e16be2ef9da7336483421df8b20754cd4
jquast <jeff@quast.co.uk>
2012-10-19 19:32:44 -0400

    move load order, cleanup,

--- default/top.py
+++ default/top.py
@@ -4,28 +4,23 @@
 
 def main(login_handle=None):
     import time
+    session, term = getsession(), getterminal()
+    session.activity = 'Intro screen'
+
     user = User() if login_handle in (None, 'anonymous') \
         else getuser(login_handle)
     user.calls += 1
     user.lastcall = time.time()
     user.save ()
-
-
-    session = getsession()
-    session.activity = 'Intro screen'
+    upref_enc = user.get('charset', None)
+    if upref_enc is not None:
+        session.encoding = upref_enc
     session.user = user
-    term = session.terminal
 
-    # rebuild last caller db
-    gosub('lc', True)
-
-    # figure out character set
-    gosub('charset')
-
-    # check for new messages
-    gosub('chkmsgs')
+    broadcastevent ('global', ('login', login_handle))
 
     # ?quick login
+    echo (term.move (0, 0) + term.normal + term.clear)
     echo ('\r\n\r\nQuick login? [yn] ')
     while True:
         k = getch()
@@ -33,6 +28,12 @@ def main(login_handle=None):
             goto ('main')
         elif k in ('n', 'N'):
             break
+
+    # check for new messages
+    #gosub('chkmsgs')
+
+    # figure out character set
+    gosub('charset')
 
     # long login
     # ... last callers
