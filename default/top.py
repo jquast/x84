dd163488db48fd445db5a834d302ad379c19d66d
jquast <jeff@quast.co.uk>
2012-10-21 15:44:10 -0400

    handle ref

--- default/top.py
+++ default/top.py
@@ -1,33 +1,40 @@
 """
 Post-login screen for X/84 (formerly 'The Progressive') BBS.
 
-When login_handle is None or u'', an in-memory account 'anonymous' is created
+When handle is None or u'', an in-memory account 'anonymous' is created
 and assigned to the session. Otherwise the handle passed is assigned.
 """
 
-def main(login_handle=None):
+def main(handle=None):
     import time
     session, term = getsession(), getterminal()
     session.activity = 'top'
-    if login_handle in (None, 'anonymous'):
+    if handle in (None, 'anonymous'):
         user = User(u'anonymous')
     else:
-        user = get_user(login_handle)
-    # assign user to session
+        user = get_user(handle)
+
+    # 1. assign session property, .user
     session.user = user
-    # update call records
+
+    # 2. update call records
     user.calls += 1
     user.lastcall = time.time()
     user.save ()
 
+    # 3. load preferred charset, if any
     upref_enc = user.get('charset', None)
     if upref_enc is not None:
         session.encoding = upref_enc
         echo ("\r\n\r\nYour user-preferred session, '%s' has been set.")
 
-#    broadcastevent ('global', ('login', login_handle))
-#
-#    # ?quick login
+    # 4. ?quick login
+    if term.number_of_colors > 0 or session.env.get('TERM') != 'unknown':
+        if term.width >= 79
+    else:
+        echo ('\r\nQuick login? [yn] ')
+    else:
+
 #    echo (term.move (0, 0) + term.normal + term.clear)
 #    echo (u'\r\nQuick login? [yn] ')
 #    while True:
