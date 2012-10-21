d310ee8c0d86f508c957121849bfa2cac0d45372
jquast <jeff@quast.co.uk>
2012-10-21 19:28:52 -0400

    try to fix linux blindly;

--- default/charset.py
+++ default/charset.py
@@ -6,6 +6,7 @@ chose 'utf8' or 'cp437' encoding. Other than the default, 'utf8', the
 Session.write() method takes special handling of a session.encoding value
 of 'cp437' for encoding translation.
 """
+
 def main():
     import textwrap
     session, term = getsession(), getterminal()
@@ -17,6 +18,8 @@ def main():
     enc_prompt = u"Press left/right until artwork looks best. Clients " \
             "should select utf8 encoding, older clients or clients with " \
             "appropriate 8-bit fontsets can select cp437."
+
+    save_msg = u"\r\n\r\n'%s' is now your preferred encoding.\r\n"
 
     def get_selector(selection):
         """
@@ -31,17 +34,17 @@ def main():
 
     def refresh(sel):
         flushevent ('refresh')
+        session.encoding = selector.selection
         if sel.selection == 'utf8':
             # ESC %G activates UTF-8 with an unspecified implementation
             # level from ISO 2022 in a way that allows to go back to
             # ISO 2022 again.
-            echo (u'\033%G')
+            echo (unichr(27) + u'%G')
         elif sel.selection == 'cp437':
             # ESC %@ returns to ISO 2022 in case UTF-8 had been entered.
-            # ESC ) K or ESC ) U Sets character set G1 to codepage 437.
-            echo (u'\033%@')
-            echo (u'\033)K')
-            echo (u'\033)U')
+            # ESC ) U Sets character set G1 to codepage 437 .. usually.
+            echo (unichr(27) + u'%@')
+            echo (unichr(27) + u')U')
         else:
             assert False, "Only encodings 'utf8' and 'cp437' supported."
         # clear & display art
@@ -57,9 +60,8 @@ def main():
         ch = getch ()
         if ch == term.KEY_ENTER:
             session.user['charset'] = session.encoding
-            echo (u"\r\n\r\n'%s' is now your preferred encoding.\r\n"
-                    % (session.encoding,))
-            getch (1.0)
+            echo (save_msg % (session.encoding,))
+            getch (0.5)
             return
         else:
             selector.process_keystroke (ch)
@@ -69,7 +71,6 @@ def main():
                 return
             if selector.moved:
                 # set and refresh art in new encoding
-                session.encoding = selector.selection
                 refresh (selector)
 
         if readevent('refresh', 0) is not None:
