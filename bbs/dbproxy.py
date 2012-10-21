40c34363f6e2dd6f87192ab6a8fc2f2a00d77b4f
jquast <jeff@quast.co.uk>
2012-10-21 01:43:45 -0400

    add locks
    add support for locking a database using named lock support added to
    engine.py

--- bbs/dbproxy.py
+++ bbs/dbproxy.py
@@ -1,45 +1,84 @@
 """
 Database proxy helper for X/84.
 """
+import logging
+import time
+
+import bbs.session
+
+#pylint: disable=C0103
+#        Invalid name "logger" for type constant (should match
+logger = logging.getLogger()
 
 class DBProxy(object):
     """
-    Provide dictionary-like object interface via IPC pipe data transfers.
+    Provide dictionary-like object interface to a database. a database call,
+    such as __len__() or keys() is issued as a command to the main engine,
+    which spawns a thread to acquire a lock on the database and return the
+    results via IPC pipe transfer.
     """
-    #pylint: disable=C0111
-    #        Missing docstring
 
     def __init__(self, schema):
-        """ @schema: database key
+        """
+        Arguments:
+            schema: database key, to become basename of .sqlite3 files.
         """
         self.schema = schema
 
     def __proxy_iter__(self, method, *args):
         """
-        Proxy a method that returns a data type supporting __iter__ by yielding
-        its IPC event data values until StopIteration is sent.
+        Iterable proxy for dictionary methods called over IPC pipe.
         """
-        from bbs.session import getsession
         event = 'db=%s' % (self.schema,)
-        getsession().send_event (event, (method, args))
-        event, data = getsession().read_event (events=(event,))
+        bbs.session.getsession().send_event (event, (method, args))
+        data = bbs.session.getsession().read_event (event)
         assert (None, 'StartIteration') == data
         while True:
-            event, data = getsession().read_event (events=(event,))
+            data = bbs.session.getsession().read_event (event)
             if data == (None, StopIteration):
                 raise StopIteration()
             yield data
 
     def __proxy__(self, method, *args):
         """
-        Proxy a method name and its arguments, then block
-        until a response is received.
+        Proxy for dictionary methods called over IPC pipe.
         """
-        from bbs.session import getsession
         event = 'db-%s' % (self.schema,)
-        getsession().send_event (event, (method, args))
-        event, data = getsession().read_event (events=(event,))
-        return data
+        bbs.session.getsession().send_event (event, (method, args))
+        return bbs.session.getsession().read_event (event)
+
+    def acquire(self, timeout=None):
+        """
+        Acquire bbs-global lock on database, possibly blocking until it can be
+        obtained. When timeout is not None, return False if lock cannot be
+        acquired after timeout has elapsed in seconds.
+        """
+        timeout = float('inf') if timeout is None else timeout
+        event = 'lock-%s' % (self.schema,)
+        stime = time.time ()
+        while time.time() - stime < timeout:
+            bbs.session.getsession().send_event (event, ('acquire', timeout))
+            # engine answers 'True' if lock is acquired, 'False' if not,
+            if bbs.session.getsession().read_event (event):
+                return True
+            time.sleep (0.60)
+            logger.warn ('failed to acquire lock, re-trying,')
+        return False
+
+    def release(self):
+        """
+        Release bbs-global lock on database.
+        """
+        event = 'lock-%s' % (self.schema,)
+        bbs.session.getsession().send_event (event, ('release', None))
+
+    def locked(self):
+        """
+        Tests wether the lock is currently locked
+        """
+        event = 'lock-%s' % (self.schema,)
+        bbs.session.getsession().send_event (event, ('locked', None))
+        return bbs.session.getsession().read_event (event)
 
     def __cmp__(self, *args):
         return self.__proxy__ ('__cmp__', *args)
@@ -61,6 +100,8 @@ class DBProxy(object):
         return self.__proxy__ ('__delitem__', *args)
     __delitem__.__doc__ = dict.__delitem__.__doc__
 
+    #pylint: disable=C0111
+    #        Missing docstring
     def get(self, *args):
         return self.__proxy__ ('get', *args)
     get.__doc__ = dict.get.__doc__
