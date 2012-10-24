e76f42e610305d4c40e024d1ba7735a1a7b55c76
jquast <jeff@quast.co.uk>
2012-10-24 18:39:39 -0400

    fix?

--- default/main.py
+++ default/main.py
@@ -4,27 +4,40 @@
 
 from bbs import *
 
-def main():
+def pak():
     term = getterminal()
-    def refresh():
-        " refresh main menu screen "
-        getsession().activity = 'Main Menu'
-        echo (u''.join((term.normal, term.normal_cursor, term.clear, '\r\n')))
-        echo (showcp437 ('default/art/main.asc'))
-        echo (u'\r\n\r\n > ')
-    def sorry():
-        echo (u'\r\n\r\n  ' + term.bright_red + u'Sorry')
-        getch (1)
-        refresh ()
-    def pak():
-        echo ('\r\n\r\n  ' + term.normal + u'Press any key...')
-        getch ()
-        refresh ()
+    echo ('\r\n\r\n  ' + term.normal + u'Press any key...')
+    getch ()
 
-    refresh ()
+def refresh():
+    " refresh main menu screen "
+    term = getterminal ()
+    echo (u''.join((term.normal, term.normal_cursor, term.clear, '\r\n')))
+    art = ([Ansi(from_cp437(line)) for line in open('default/art/main.asc')])
+    max_len = max([line.__len__() for line in art])
+    if max_len <= term.width:
+        for line in art:
+            echo (line.center(term.width - max_len).rstrip() + '\r\n')
+    def disp_entry(char, blurb):
+        return (term.bold_blue('(') + term.blue_reverse(char)
+                + term.bold_blue + ')' + term.bright_white (' '+blurb))
+    echo (term.move(len(art) + 3, max(1, (term.width/2) - 20)))
+    echo (disp_entry ('b', 'bbs lister'))
+    echo (disp_entry ('l', 'last calls'))
+    echo (disp_entry ('o', 'one liners'))
+    echo (term.move(len(art) + 4, max(1, (term.width/2) - 20)))
+    echo (disp_entry ('z', 'news'))
+    echo (disp_entry ('g', 'goodbye'))
+    echo (disp_entry ('c', 'charset'))
+    echo ('\r\n\r\n')
+
+def main():
+    session, term = getsession(), getterminal()
 
     while True:
-        choice = getch()
+        if pollevent('refresh'):
+            refresh ()
+        choice = getch(1)
         if choice == u'*':
             goto ('main')
         elif choice == u'b':
@@ -36,9 +49,10 @@ def main():
         elif choice == u'o':
             gosub ('ol')
             refresh ()
-        elif choice == u's':
-            gosub ('si')
-            refresh ()
         elif choice == u'z':
             gosub('news')
             refresh()
+        elif choice == u'g':
+            goto('logoff')
+        elif choice == u'c':
+            gosub('charset')
