e9f83ab7737e75d8eceb1401bb82abf984dfef6b
jquast <jeff@quast.co.uk>
2012-10-25 23:27:53 -0400

    docstring cleanup

--- bbs/exception.py
+++ bbs/exception.py
@@ -1,27 +1,31 @@
 """
-Custom exception classes for x/84 BBS https://github.com/jquast/x84
+Custom exception classes for x/84, https://github.com/jquast/x84
 """
 
 class Disconnect(Exception):
-    """Raise this error to cause the socket to be disconnected."""
+    """Throw to cause the socket to be disconnected."""
     pass
 
 
 class ConnectionClosed (Exception):
-    """This exception thrown when client closes connection."""
+    """Thrown when client closes connection."""
     pass
 
 
 class ConnectionTimeout (Exception):
-    """Raised this error to indicate idle time exceeded."""
+    """Thrown to indicate idle time exceeded."""
     pass
 
 
 class Goto (Exception):
-    """Raised whenever a script wants to travel&exchange itself to another."""
+    """Thrown when a script wants exec() another script"""
     pass
 
 
 class ScriptError (Exception):
-    """This exception thrown when runscript fails to locate script."""
+    """Thrown when runscript fails to locate script."""
+    pass
+
+class DatabaseError (Exception):
+    """Thrown when an error occured issuing a db proxy request."""
     pass
