f68f5a9cfd27201bdae90d2f2081f78bc1f736d8
jquast <dingo@1984.ws>
2012-11-03 17:36:11 -0400

    PEP8

--- x84/default/editor.py
+++ x84/default/editor.py
@@ -1,95 +1,89 @@
-import tempfile
+import logging
 import os
-#pylint: disable=W0614
-#        Unused import from wildcard import
-from x84.bbs import *
+from x84.bbs import getterminal, ScrollingEditor, Pager, getsession
+from x84.bbs import getch, echo
+
+logger = logging.getLogger()
+
 
 def banner():
     term = getterminal()
     return term.home + term.normal + term.clear
 
+
 def redraw(pager, le):
     rstr = u''
     rstr += pager.border()
-    rstr += pager.refresh ()
-    rstr += le.border ()
-    rstr += le.refresh ()
-    #rstr += le.fixate ()
+    rstr += pager.refresh()
+    rstr += le.border()
+    rstr += le.refresh()
     return rstr
+
 
 def get_ui(ucs=u'', ypos=None):
     term = getterminal()
     pager = Pager(term.height - 2, term.width, 1, 0)
     pager.colors['border'] = term.bold_blue
     pager.glyphs['left-vert'] = pager.glyphs['right-vert'] = u'|'
-    pager.update (ucs)
+    pager.update(ucs)
     yloc = pager.visible_bottom if ypos is None else ypos
     # actually, should - 1 for border ?!
     le = ScrollingEditor(term.width, yloc, 0)
     le.glyphs['bot-horiz'] = le.glyphs['top-horiz'] = u'-'
     le.colors['border'] = term.bold_green
-    print 'yloc', yloc
     return pager, le
 
+
 def prompt_commands(pager):
-    pager.footer ('q-uit, s-save')
+    pager.footer('q-uit, s-save')
     # todo: prompt
+
 
 def quit():
     # todo: prompt
     pass
+
 
 def main(uattr=u'draft'):
     """
     Retreive and store unicode bytes to user attribute keyed by 'uattr';
     """
     session, term = getsession(), getterminal()
-    #pager, le = get_ui(
-    #        DBProxy('userattr')[session.user.handle].get(uattr, u''), 0)
+
     test = open(os.path.join(os.path.dirname(__file__),
-        'art', 'news.txt')).read().decode('utf8').strip()
+                             'art', 'news.txt')).read().decode('utf8').strip()
     pager, le = get_ui(test)
-    xpos = Ansi(pager.content[-1]).__len__()
     dirty = True
     while True:
         if session.poll_event('refresh'):
             pager, le = get_ui(u'\n'.join(pager.content), le.yloc)
             dirty = True
         if dirty:
-            echo (banner())
-            echo (redraw(pager, le))
+            echo(banner())
+            echo(redraw(pager, le))
             dirty = False
         inp = getch()
-        res = le.process_keystroke (inp)
-        print repr(inp), repr(res)
-        echo (res)
-        if le.quit:
+        logger.info(repr(inp) + 'input')
+        res = le.process_keystroke(inp)
+        if 0 != len(res):
+            logger.info(repr(res) + 'echo')
+            echo(res)
+        elif le.quit:
+            logger.info('QUIT')
             break
-        if le.carriage_returned:
+        elif le.carriage_returned:
+            logger.info('RETURN')
             # woak
             break
+        else:
+            res = pager.process_keystroke(inp)
+            echo(res)
+            if pager.moved:
+                logger.info(repr(res) + 'echo')
+                echo(res)
 
-        #if ch == '/':
-            #pager.footer ('q-uit, s-save')
-            #ch = getch()
-            #if ch in (u'q', 'Q') and quit()
-            #    # quit w/o save
-            #    return False
-            #if process_keystroke(ch):
-            #    break
-#    editor = '/usr/local/bin/virus'
-#    fp, tmppath = tempfile.mkstemp ()
-#    nethackrc = session.user.get(sattr, '')
-#    length = len(nethackrc)
-#    if 0 != length:
-#        written = 0
-#        while written < length:
-#            written += os.write (fp, nethackrc[written:])
-#    os.close (fp)
-#    lastmod = os.stat(tmppath).st_mtime
-#    d = Door(editor, args=(tmppath,))
-#    d._TAP = True
-#    if 0 == d.run() and os.stat(tmppath).st_mtime > lastmod:
-#        # program exited normaly, file has been modified
-#        session.user[sattr] = open(tmppath, 'r').fp.read()
-#    os.unlink (tmppath)
+
+    #pager, le = get_ui(
+    #        DBProxy('userattr')[session.user.handle].get(uattr, u''), 0)
+    #xpos = Ansi(pager.content[-1]).__len__()
+    #xpos
