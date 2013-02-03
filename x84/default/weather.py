d9b6eff46408ff7acab3b570dba88b95eac6fa10
jquast <dingo@1984.ws>
2013-02-03 15:34:46 -0500

    nearly complete, today's conditions avail.

--- x84/default/weather.py
+++ x84/default/weather.py
@@ -10,21 +10,21 @@ weather_xmlurl = (u'http://apple.accuweather.com/'
                   + 'adcbin/apple/')  # ?zipcode=%s'
 location_xmlurl = (u'http://apple.accuweather.com/'
                    + 'adcbin/apple/Apple_find_city.asp')  # ?location=%s'
-days = ('Sunday', 'Monday',
-        'Tuesday', 'Wednesday',
-        'Thursday', 'Friday',
-        'Saturday',)
-current_keys = (
-    'City', 'State', 'Time', 'Temperature', 'RealFeel', 'Humidity',
-    'WeatherText', 'WeatherIcon', 'WindSpeed', 'WindDirection',
-    'Visibility', 'Precip', 'DayCode', 'Pressure', 'GmtDiff', 'Tree',
-    'Weed', 'Grass', 'Mold', 'UVIndex', 'Copyright', 'AirQuality',
-    'AirQualityType',)
-fcast_keys = (
-    'TXT_Short', 'TXT_Long', 'High_Temperature', 'Low_Temperature',
-    'Real_Feel_High', 'Real_Feel_Low', 'WindGust', 'MaxUV',
-    'Rain_Amount', 'Snow_Amount', 'Precip_Amount', 'TStorm_Prob',
-    'DayCode', 'WeatherIcon', 'WindSpeed', 'WindDirection', 'Max_UV',)
+#days = ('Sunday', 'Monday',
+#        'Tuesday', 'Wednesday',
+#        'Thursday', 'Friday',
+#        'Saturday',)
+#current_keys = (
+#    'City', 'State', 'Time', 'Temperature', 'RealFeel', 'Humidity',
+#    'WeatherText', 'WeatherIcon', 'WindSpeed', 'WindDirection',
+#    'Visibility', 'Precip', 'DayCode', 'Pressure', 'GmtDiff', 'Tree',
+#    'Weed', 'Grass', 'Mold', 'UVIndex', 'Copyright', 'AirQuality',
+#    'AirQualityType',)
+#fcast_keys = (
+#    'TXT_Short', 'TXT_Long', 'High_Temperature', 'Low_Temperature',
+#    'Real_Feel_High', 'Real_Feel_Low', 'WindGust', 'MaxUV',
+#    'Rain_Amount', 'Snow_Amount', 'Precip_Amount', 'TStorm_Prob',
+#    'DayCode', 'WeatherIcon', 'WindSpeed', 'WindDirection', 'Max_UV',)
 
 
 def disp_searching():
@@ -79,7 +79,7 @@ def get_zipsearch(zipcode=u''):
         u' ')))
     return LineEditor(width=min(30, term.width - 5), content=zipcode).read()
 
-def do_weather(postal):
+def do_fetch(postal):
     import StringIO
     resp = requests.get(u'http://apple.accuweather.com'
             + u'/adcbin/apple/Apple_Weather_Data.asp',
@@ -87,7 +87,8 @@ def do_weather(postal):
     weather = dict()
     if resp is None:
         disp_notfound()
-    elif resp.status_code != 200:
+        return None
+    if resp.status_code != 200:
         # todo: logger.error
         echo(u'\r\n\r\n')
         echo(term.bold_red(u'StAtUS COdE: %s' % (resp.status_code,)))
@@ -95,21 +96,27 @@ def do_weather(postal):
         echo(repr(resp.content))
         echo(u'\r\n\r\n' + 'PRESS ANY kEY')
         getch()
-    else:
-        xml_stream = StringIO.StringIO(resp.content)
-        tree = ET.parse(xml_stream)
-        root = tree.getroot()
-        for elem in root.find('CurrentConditions'):
-            if elem.tag in ('City', 'State', 'DaylightSavingsActive', 'UFDB'):
-                continue
-            weather[elem.tag] = elem.text.strip()
-        weather['Pressure-state'] = root.find('CurrentConditions'
-                ).find('Pressure').attrib.get('state')
-        for elem in root.find('Forecast'):
-            print elem.tag, elem.attrib
-            for subelem in elem:
-                print subelem.tag, subelem.attrib, repr(subelem.text)
+        return None
+    xml_stream = StringIO.StringIO(resp.content)
+    tree = ET.parse(xml_stream)
+    return tree.getroot()
+
+def parse_weather(root):
+    weather = dict()
+    # parse all current conditions from XML, value is cdata.
+    for elem in root.find('CurrentConditions'):
+        weather[elem.tag] = elem.text.strip() if elem.text is not None else u''
+        # store attribute values
+        if elem.tag in ('Pressure',):
+            for attr, val in elem.attrib.items():
+                weather['%s-%s' % (elem.tag, attr)] = val
     return weather
+
+def parse_forecast(root):
+    for elem in root.find('Forecast'):
+        print elem.tag, elem.attrib, elem.text
+        for subelem in elem:
+            print subelem.tag, subelem.attrib, repr(subelem.text)
 
 def do_search(search):
     import StringIO
@@ -138,80 +145,112 @@ def do_search(search):
     return locations
 
 
-def chose_location(locations):
-    from x84.bbs import getterminal, getsession, echo, Lightbar
-    session, term = getsession(), getterminal()
-    echo(u''.join((u'\r\n\r\n', term.clear_eol, term.normal,)))
-    echo(u' '.join((term.yellow(u'ChOSE A'),
-        term.bold_yellow('CitY'),
-        term.yellow_reverse(': '),)))
-    assert len(locations) > 0, (
-            u'Cannot chose from empty list')
-    if (session.user.get('expert', False) == True
-            or 0 == term.number_of_colors):
-        max_width = len('%d' % (len(locations),))
-        for num, loc in enumerate(locations):
-            echo(u' '.join((
-                term.blue('u['),
-                u'%*d' % (max_width, num),
-                term.blue(u']'),
-                term.bold(loc['city']), u',',
-                term.bold(loc['state']), u'\r\n')))
-            if 0 == num % (term.height - 1):
-                echo(term.reverse('--MORE--'))
-                if getch() is None:
-                    break
-                echo(u'\r\n')
-        echo(' '.join((u'\r\n\r\n',
-            term.bold_blue(u'['),
-            term.bold_black(u'%d-%d' % (0, len(locations),)),
-            term.bold_blue(u']'),
-            term.reverse_blue(':'),)))
-        idx = LineEditor.read(width=max_width)
-        if idx is None or len(idx) == 0:
-            return None
-    else:
-        fullheight = min(term.height - 8, len(locations))
-        fullwidth = max(int(term.width * .8),
-            min(term.width - 8, max([len(u'%(city)s, %(state)s' % loc)
-                for loc in locations])))
-        echo(u'\r\n' * fullheight)
-        lightbar = Lightbar(height=fullheight,
-                            width=fullwidth,
-                            yloc=term.height - fullheight,
-                            xloc=int((term.width / 2) - (fullwidth / 2)))
-        lookup = dict([(loc['postal'], loc) for loc in locations])
-        lightbar.update([(loc['postal'], (u'%(city)s, %(state)s' % loc))
-            for loc in locations])
-        lightbar.colors['border'] = term.yellow
-        echo(lightbar.border())
-        echo(lightbar.title(u''.join((
-            term.yellow(u' -'), term.bold_white(u'-[ '),
-            term.bold_yellow('CitY'),
-            term.bold_white(u', '),
-            term.bold_yellow('StAtE'),
-            term.bold_white(u' ]-'), term.yellow(u'- '),))))
-        echo(lightbar.footer(u''.join((
-            term.bold_yellow(u' ( '),
-            term.yellow_underline('Escape'),
+def chose_location_dummy():
+    from x84.bbs import getterminal, echo
+    msg_enteridx = (
+            term.bold_yellow(u'('),
+            term.underline_yellow(u'0'),
+            term.yellow(u'-'),
+            term.underline_yellow(u'%d' % (len(locations) - 1,)),
+            term.yellow(u','),
+            term.underline_yellow('Escape'),
             term.bold_white(u':'),
             term.yellow('EXit'),
-            term.bold_yellow(u' ) '),))))
-        lightbar.colors['highlight'] = term.yellow_reverse
-        choice = lightbar.read()
-        echo(lightbar.erase())
-        return ((loc for loc in locations if choice == loc['postal']
-            ).next() if choice is not None else choice)
+            term.bold_yellow(u')'), u' ',
+            term.reverse_yellow(':'),)
+    max_nwidth = len('%d' % (len(locations - 1),))
+    def disp_entry(num, loc):
+        return u''.join((
+            term.bold_yellow('u['),
+            u'%*d' % (max_nwidth, num),
+            term.bold_yellow(u']'), u' ',
+            term.yellow(loc['city']), u', ',
+            term.yellow(loc['state']), u'\r\n',))
+    for num, loc in enumerate(locations):
+        echo(disp_entry(num, loc))
+        if 0 == num % (term.height - 1):
+            echo(term.reverse('--MORE--'))
+            if getch() is None:
+                break
+            echo(u'\r\n')
+    idx = u''
+    while True:
+        echo(u''.join(msg_enteridx))
+        idx = LineEditor.read(width=max_width, content=idx)
+        if idx is None or len(idx) == 0:
+            return None
+        try:
+            idx = int(idx)
+        except ValueError as err:
+            echo(term.bold_red(u'\r\n%s' % (err,)))
+            continue
+        if idx < 0 or idx > len(locations) - 1:
+            echo(term.bold_red(u'\r\nValue out of range'))
+            continue
+        return locations[idx]
+
+
+def chose_location_lightbar(locations):
+    from x84.bbs import getterminal, echo, Lightbar
+    term = getterminal()
+    fmt = u'%(city)s, %(state)s'
+    lookup = dict([(loc['postal'], loc) for loc in locations])
+    fullheight = min(term.height - 8, len(locations) + 2)
+    fullwidth = min(75, int(term.width * .8))
+    # shrink window to minimum width
+    maxwidth = max([len(fmt % val) for val in lookup.values()]) + 2
+    if maxwidth < fullwidth:
+        fullwidth = maxwidth
+    echo(u'\r\n' * fullheight)
+    lightbar = Lightbar(height=fullheight,
+                        width=fullwidth,
+                        yloc=term.height - fullheight,
+                        xloc=int((term.width / 2) - (fullwidth / 2)))
+    lightbar.update([(key, fmt % val) for key, val in lookup.items()])
+    lightbar.update([(key, fmt % val) for key, val in lookup.items()])
+    lightbar.colors['border'] = term.yellow
+    echo(lightbar.border())
+    echo(lightbar.title(u''.join((
+        term.yellow(u'-'), term.bold_white(u'[ '),
+        term.bold_yellow('CitY'),
+        term.bold_white(u', '),
+        term.bold_yellow('StAtE'),
+        term.bold_white(u' ]'), term.yellow(u'-'),))))
+    echo(lightbar.footer(u''.join((
+        term.yellow(u'-'), term.bold_black(u'( '),
+        term.yellow_underline('Escape'), u':',
+        term.yellow('EXit'),
+        term.bold_black(u' )'), term.yellow(u'-'),))))
+    lightbar.colors['highlight'] = term.yellow_reverse
+    choice = lightbar.read()
+    echo(lightbar.erase())
+    return ((loc for loc in locations if choice == loc['postal']
+        ).next() if choice is not None else choice)
+
+
+def chose_location(locations):
+    from x84.bbs import getterminal, getsession, echo
+    session, term = getsession(), getterminal()
+    assert len(locations) > 0, (
+            u'Cannot chose from empty list')
+    msg_chosecity = (
+            term.yellow(u'ChOSE A'),
+            term.bold_yellow('CitY'),
+            term.yellow_reverse(':'), u' ',)
+    echo(u''.join((u'\r\n\r\n', term.clear_eol, term.normal,)))
+    echo(u' '.join(msg_chosecity))
+    if (session.user.get('expert', False) or 0 == term.number_of_colors):
+        return chose_location_dummy(locations)
+    return chose_location_lightbar(locations)
 
 
 def prompt_wfc(location):
     from x84.bbs import getterminal, echo, getch
     term = getterminal()
     echo(u''.join((u'\r\n\r\n', term.clear_eol,
-        term.bold_yellow(u'diSPlAY'),
-        term.yellow(u' WEAthER fORECASt fOR '),
-        term.yellow_reverse('%(city)s, %(state)s' % location),
-        term.bold_white(' ? '),
+        term.yellow(u'diSPlAY WEAthER fORECASt fOR '),
+        term.bold('%(city)s, %(state)s' % location),
+        term.yellow(' ? '),
         term.bold_yellow(u'['),
         term.underline_yellow(u'yn'),
         term.bold_yellow(u']'),
@@ -245,10 +284,36 @@ def main():
 
     if False == prompt_wfc(location):
         return
-    forecast = do_weather(location.get('postal'))
+    root = do_fetch(location.get('postal'))
+    if root is None:
+        return
+    weather = parse_weather(root)
+    disp_weather(weather)
+
+def disp_weather(weather):
+    from x84.bbs import getterminal, echo, Ansi, getch
+    term = getterminal()
+    width = min(60, term.width * .8)
+    echo(u''.join((u'\r\n\r\n',
+        term.clear_eol,
+        term.yellow(u'CURRENt WEAthER CONditiONS fOR '),
+        term.bold_white(weather.get('City', u'?')),
+        term.bold_yellow(', %s' % (weather['State'],))
+        if 'State' in weather and 0 != len(weather['State']) else u'', ' ',
+        term.yellow(u' AS Of '),
+        term.bold_white(weather.get('Time')), u' ',
+        term.yellow_reverse_bold(u'...'),
+        u'\r\n',)))
+    for key, value in weather.items():
+        if 0 == len(value):
+            continue
+        echo(u'\r\n%s %s %s.' % (
+            term.bold_yellow(key),
+            term.bold_black('is'),
+            term.bold_white(value),))
+    getch()
     return
-#    echo(term.move(4, 16) + term.clear_eol)
-#
+
 #    # convert forecast data into list, ordered sunday through monday
 #    for key in forecast.keys():
 #        fc[days.index(forecast[key]['DayCode'])] = forecast[key]
