cc7d11290818c62b27f3287d776d519ebe0e0fec
jquast <dingo@1984.ws>
2013-02-08 14:10:56 -0500

    much improved polling/dirty/update

--- x84/default/online.py
+++ x84/default/online.py
@@ -1,6 +1,10 @@
 """ Who's online script for X/84, https://github.com/jquast/x84 """
 import time
 SELF_ID = -1
+POLL_KEY = 0.1  # blocking ;; how often to poll keyboard
+POLL_INF = 1    # seconds elapsed until re-ask clients for more details
+POLL_AYT = 2    # seconds elapsed until global 'are you there?' is checked,
+POLL_OUT = 1    # seconds elapsed before screen updates
 # slen: returns terminal width of ascii representation of #sessions
 slen = lambda sessions: len(u'%d' % (len(sessions),))
 
@@ -23,87 +27,6 @@ def banner():
         term.bold_green(" whO'S ONliNE"),))
 
 
-def main(login_handle=None):
-    from x84.bbs import getsession, getterminal, getch, echo
-    session, term = getsession(), getterminal()
-    session.activity = u"Who's Online"
-    poll_ayt = 5
-    fresh = 1
-    ayt_lastfresh = 0
-    def broadcast_AYT(last_update):
-        # broadcast are-you-there
-        if time.time() - last_update > poll_ayt:
-            session.send_event('global', 'AYT')
-            last_update = time.time()
-        return last_update
-
-    sessions = dict()
-    dirty = True
-    cur_row = 0
-    while True:
-        ayt_lastfresh = broadcast_AYT(ayt_lastfresh)
-        inp = getch(0.5)
-        if session.poll_event('refresh') or (
-                inp in (u' ', term.KEY_REFRESH, unichr(12))):
-            dirty = True
-        if inp in (u'q', 'Q', term.KEY_EXIT, unichr(27)):
-            return
-
-        # add sessions that respond to AYT
-        data = session.poll_event('ACK')
-        if data is not None:
-            sid, handle = data
-            if sid in sessions:
-                sessions[sid]['handle'] = handle
-            else:
-                sessions[sid] = dict((
-                    ('handle', handle),
-                    ('lastfresh', time.time()),))
-                dirty = True
-
-        # update sessions that respond to info-req
-        data = session.poll_event('info-ack')
-        if data is not None:
-            sid, attrs = data
-            if sessions.get(sid, dict()).get('activity') != attrs['activity']:
-                # and refresh screen if activity changes
-                dirty = True
-            sessions[sid] = attrs
-            sessions[sid]['lastfresh'] = time.time()
-
-        # update our own session
-        sessions[SELF_ID] = session.info()
-        sessions[SELF_ID]['lastfresh'] = time.time()
-
-        # request that all sessions update if more stale than poll_ayt,
-        # or is missing session info (only AYT replied so far!)
-        request_info(set([key for key, attr in sessions.items()
-            if time.time() - attr['lastfresh'] > poll_ayt
-            or attr.get('idle', -1) == -1]) ^ set([-1]))
-
-        # prune users who haven't responded to AYT
-        for sid, attrs in sessions.items()[:]:
-            if time.time() - attrs['lastfresh'] > (poll_ayt * 2):
-                del sessions[sid]
-                dirty = True
-
-        if dirty:
-            otxt = update(sessions)
-            olen = len(otxt.splitlines())
-            if 0 == cur_row or (cur_row + olen) >= term.height:
-                otxt_b = banner()
-                otxt_h = heading(sessions)
-                cur_row = len(otxt_b.splitlines()) + len(otxt_h.splitlines())
-                echo(u'\r\n'.join((u'\r\n\r\n', otxt_b, otxt_h, otxt)))
-            else:
-                echo(u''.join((
-                    u'\r\n',
-                    '-'.center(term.width).rstrip(),
-                    u'\r\n')))
-                echo(otxt)
-            cur_row += olen
-            dirty = False
-
 def update(sessions):
     from x84.bbs import getsession, getterminal, ini
     session, term = getsession(), getterminal()
@@ -112,13 +35,12 @@ def update(sessions):
             u'%*d' % (4 + slen(sessions), node),
             u'%4is' % (attrs.get('idle', 0),), u' ',
             u'%-*s' % (max_user, (
-                attrs.get('handle', u'** Connecting')
-                if attrs.get('handle') != session.user.handle
-                else session.user.handle),),
+                u'** diSCONNECtEd' if 'delete' in attrs
+                else attrs.get('handle', u'** CONNECtiNG')),),
             term.green(u' - '),
             term.bold_green((attrs.get('activity', u''))
-                if attrs.get('handle') != session.user.handle else
-                session.activity),
+                if attrs.get('sid') != session.sid else
+                term.bold_black(session.activity)),
             )) for node, (sid, attrs) in get_nodes(sessions)]))
 
 
@@ -161,3 +83,89 @@ def prompt():
         decorate('Escape/q', 'Uit'),
         decorate('Spacebar', 'REfRESh'),
         ))).wrap(int(term.width * .7))
+
+def main(login_handle=None):
+    from x84.bbs import getsession, getterminal, getch, echo
+    session, term = getsession(), getterminal()
+    session.activity = u"Who's Online"
+    ayt_lastfresh = 0
+    def broadcast_AYT(last_update):
+        # broadcast are-you-there
+        if time.time() - last_update > POLL_AYT:
+            session.send_event('global', 'AYT')
+            last_update = time.time()
+        return last_update
+
+    sessions = dict()
+    dirty = time.time()
+    cur_row = 0
+    while True:
+        ayt_lastfresh = broadcast_AYT(ayt_lastfresh)
+        inp = getch(POLL_KEY)
+        if session.poll_event('refresh') or (
+                inp in (u' ', term.KEY_REFRESH, unichr(12))):
+            dirty = time.time()
+        if inp in (u'q', 'Q', term.KEY_EXIT, unichr(27)):
+            return
+
+        # add sessions that respond to AYT
+        data = session.poll_event('ACK')
+        if data is not None:
+            sid, handle = data
+            if sid in sessions:
+                sessions[sid]['handle'] = handle
+            else:
+                sessions[sid] = dict((
+                    ('handle', handle),
+                    ('lastfresh', time.time()),))
+                dirty = time.time()
+
+        # update sessions that respond to info-req
+        data = session.poll_event('info-ack')
+        if data is not None:
+            sid, attrs = data
+            if sessions.get(sid, dict()).get('activity') != attrs['activity']:
+                # and refresh screen if activity changes
+                dirty = time.time()
+            sessions[sid] = attrs
+            sessions[sid]['lastfresh'] = time.time()
+
+        # update our own session
+        sessions[SELF_ID] = session.info()
+        sessions[SELF_ID]['lastfresh'] = time.time()
+
+        # request that all sessions update if more stale than POLL_INF,
+        # or is missing session info (only AYT replied so far!),
+        # or has been displayed as 'Disconnected' (marked for deletion)
+        request_info(set([key for key, attr in sessions.items()
+            if time.time() - attr['lastfresh'] > POLL_INF
+            or attr.get('idle', -1) == -1
+            or attr.get('delete', 0) == 1]) ^ set([-1]))
+
+        # prune users who haven't responded to AYT
+        for sid, attrs in sessions.items()[:]:
+            if time.time() - attrs['lastfresh'] > (POLL_AYT * 2):
+                sessions[sid]['delete'] = 1
+                dirty = time.time()
+
+        if dirty is not None and time.time() - dirty > POLL_OUT:
+            otxt = update(sessions)
+            olen = len(otxt.splitlines())
+            if 0 == cur_row or (cur_row + olen) >= term.height:
+                otxt_b = banner()
+                otxt_h = heading(sessions)
+                cur_row = len(otxt_b.splitlines()) + len(otxt_h.splitlines())
+                echo(u'\r\n'.join((u'\r\n\r\n', otxt_b, otxt_h, otxt)))
+            else:
+                echo(u''.join((
+                    u'\r\n',
+                    '-'.center(term.width).rstrip(),
+                    u'\r\n')))
+                echo(otxt)
+            cur_row += olen
+            dirty = None
+
+        # delete disconnected sessions
+        for sid, attrs in sessions.items()[:]:
+            if attrs.get('delete', 0) == 1:
+                del sessions[sid]['delete']
