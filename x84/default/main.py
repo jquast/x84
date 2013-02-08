cb4da0b8c670309c236e65194da76696435752a8
jquast <dingo@1984.ws>
2013-02-07 22:17:39 -0500

    new main menu look; briefer, easier to customize

--- x84/default/main.py
+++ x84/default/main.py
@@ -4,97 +4,80 @@
 
 
 def refresh():
-    from x84.bbs import getterminal, echo, Ansi, from_cp437
+    from x84.bbs import getterminal, echo, Ansi, showcp437
     import os
-    " refresh main menu screen "
     term = getterminal()
-    echo(u''.join((term.normal, term.clear, term.normal_cursor)))
-    art = ([Ansi(from_cp437(line)) for line in open(os.path.join(
-        os.path.dirname(__file__), 'art', 'main.asc'))])
-
-    def disp_art():
-        for line in art:
-            echo((line.rstrip().center(term.width).rstrip())[:term.width - 1]
-                 + u'\r\n')
-
-    def disp_entry(char, blurb):
-        return Ansi(term.bold_blue('(') + term.blue_reverse(char)
-                    + term.bold_blue + ')' + term.bright_white(blurb) + u' ')
-
-    def pos_row(n):
-        art_row = 12
-        art_middl = 8
-        art_width = 30
-        return term.move(min(term.height - (art_row - n),
-                             max(len(art) - art_row,
-                                 art_middl + n)),
-                         max(1, (term.width / 2) - art_width)
-                         ) or '\r\n'
-
-    disp_art()
-    # group 1,
-    echo(pos_row(0))
-    echo(disp_entry('b', 'bS liStER'))
-    echo(disp_entry('l', 'ASt CAllS'))
-    echo(disp_entry('o', 'NE liNERS'))
-    echo(disp_entry('$', "WhO'S ONliNE"))
-    # group 2,
-    echo(pos_row(2))
-    echo(disp_entry('n', 'NEWS'))
-    echo(disp_entry('w', 'EAthER fORECASt'))
-    echo(disp_entry('p', 'ROfilE EditOR'))
-    # group 3,
-    echo(pos_row(4))
-    echo(disp_entry('!', 'POSt A MSG'))
-    echo(disp_entry('r', 'EAd All MSGS'))
-    echo(disp_entry('g', 'OOdbYE/lOGOff'))
+    artfile = os.path.join(os.path.dirname(__file__), 'art', 'main.asc')
+    echo(u''.join((
+        u'\r\n\r\n',
+        term.blue(u'/'.rjust(term.width / 2)), term.bold_black(u'/ '),
+        term.bold('x'), term.bold_blue('/'), term.bold('84'), u' ',
+        'MAiN MENU',
+        u'\r\n')))
+    for line in showcp437(artfile):
+        echo(line)
     echo(u'\r\n\r\n')
-    echo(term.move(term.height - 1))
+    entries = (
+            ('b', 'bS liStER'),
+            ('l', 'ASt CAllS'),
+            ('o', 'NE liNERS'),
+            ('w', "hO'S ONliNE"),
+            ('n', 'EWS'),
+            ('$', 'EAthER fORECASt'),
+            ('e', 'Edit PROfilE'),
+            ('!', 'POSt A MSG'),
+            ('r', 'EAd All MSGS'),
+            ('g', 'OOdbYE /lOGOff'),)
+    buf_str = u''
+    for key, name in entries:
+        out_str = u''.join((
+            term.bold(u'('),
+            term.bold_blue_underline(key),
+            term.bold(u')'),
+            term.bold_blue(name.split()[0]),
+            u' ', u' '.join(name.split()[1:]),
+            u'  '))
+        ansilen = len(Ansi(buf_str + out_str))
+        if ansilen >= (term.width * .8):
+            echo(Ansi(buf_str).center(term.width) + u'\r\n\r\n')
+            buf_str = out_str
+        else:
+            buf_str += out_str
+    echo(Ansi(buf_str).center(term.width) + u'\r\n\r\n')
+    echo(u' [%s]:' % (
+        term.blue_underline(''.join([key for key, name in entries]))))
 
 
 def main():
     from x84.bbs import getsession, getterminal, getch, goto, gosub
     session, term = getsession(), getterminal()
 
-    dirty = True
+    choice = -1
     while True:
-        if dirty or session.poll_event('refresh'):
+        if choice is not None:
             refresh()
-            dirty = False
         choice = getch(1)
         if choice == u'*':
-            # reload main menu using hidden option '*'
-            # for making changes to main.py
-            goto('main')
-        # group 1,
+            goto('main')  # reload main menu using hidden option '*'
         elif choice == u'b':
             gosub('bbslist')
-            dirty = True
         elif choice == u'l':
             gosub('lc')
-            dirty = True
         elif choice == u'o':
             gosub('ol')
-            dirty = True
-        elif choice == u'$':
+        elif choice == u'w':
             gosub('online')
-            dirty = True
-        # group 2,
         elif choice == u'n':
             gosub('news')
-            dirty = True
-        elif choice == u'w':
+        elif choice == u'$':
             gosub('weather')
-            dirty = True
         elif choice == u'p':
             gosub('profile')
-            dirty = True
-        # group 3,
+        elif choice == u't':
+            gosub('tetris')  # currently hidden (jojo's)
         elif choice == u'!':
             gosub('writemsg')
-            dirty = True
         elif choice == u'r':
             gosub('readmsgs')
-            dirty = True
         elif choice == u'g':
             goto('logoff')
