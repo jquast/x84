1a86797ad66b3f7587e97ca485e4688f18147c29
jquast <dingo@1984.ws>
2013-02-14 17:22:54 -0500

    easier playback interface

--- x84/default/ttyplay.py
+++ x84/default/ttyplay.py
@@ -2,46 +2,34 @@
 
 def main(ttyfile=u'', peek=False):
     from x84.bbs import getsession, getterminal, echo, getch
-    from x84.bbs import Door, ScrollingEditor, Ansi, ini
+    from x84.bbs import Door, ini, Lightbar
     import os, re
     TTYPLAY = ini.CFG.get('ttyplay', 'exe')
     if not os.path.exists(TTYPLAY):
-        echo(u'\r\n%s not installed.\r\n' % (TTYPLAY,))
+        echo(u'\r\n%s NOt iNStAllEd.\r\n' % (TTYPLAY,))
         getch()
         return
+
     session, term = getsession(), getterminal()
-    if 'sysop' in session.user.groups:
+    if 'sysop' in session.user.groups and ttyfile == u'':
         folder = os.path.dirname(ttyfile) or session._ttyrec_folder
-        se = ScrollingEditor(term.width - 18, term.height - 3, 9)
-        se.content = os.path.basename(ttyfile)
-        se.enable_scrolling = True
-        se.colors['highlight'] = term.blue_reverse
-        files = Ansi(u', '.join([fn
-            for fn in os.listdir(session._ttyrec_folder)
-            if fn.endswith('.ttyrec')])).wrap(term.width)
-        echo(u'\r\n\r\n')
-        echo(files)
-        echo(u'\r\n\r\n')
-        echo(term.move(term.height, 1))
-        while True:
-            echo(term.bold_blue(u'ttyplay: '))
-            echo(u'\r\n')
-            ttyfile = se.read()
-            if (ttyfile is None
-                    or 0 == len(ttyfile.strip())
-                    or os.path.sep in ttyfile):
-                print 'x'
-                return
-            if not (os.path.exists(os.path.join(folder, ttyfile))
-                    and os.path.isfile(os.path.join(folder, ttyfile))):
-                echo(term.bold_red('\r\npath not found.\r\n\r\n'))
-                continue
-            ttyfile = os.path.join(folder, ttyfile)
-            break
+        files = sorted([fn for fn in os.listdir(session._ttyrec_folder)
+                if fn.endswith('.ttyrec')])
+        echo(u'\r\n' * term.height)
+        sel = Lightbar(term.height -1, term.width -1, 0, 0)
+        sel.colors['border'] = term.bold_green
+        echo(sel.border() + sel.title('-  SElECt A RECORdiNG  -'))
+        sel.update([(fname, fname) for fname in files])
+        x_ttyfile = sel.read()
+        if x_ttyfile is None or sel.quit:
+            return
+        ttyfile = os.path.join(folder, x_ttyfile)
+
     if not (os.path.exists(ttyfile) and os.path.isfile(ttyfile)):
-        echo(term.bold_red('\r\npath not found: %s\r\n' % (ttyfile,)))
+        echo(term.bold_red('\r\n\r\nPAth NOt fOUNd: %s\r\n' % (ttyfile,)))
         getch()
         return
+
     # .. we could look for and write various information headers..
     # .. esp. at EOF, about session.user.handle & connect_time & etc.
     data = open(ttyfile, 'rb').read(64)
