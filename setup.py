15cfbd3e78fd45cbac6409ecb10eae841a56365e
jquast <dingo@1984.ws>
2013-02-14 22:08:32 -0500

    updated descriptions, keywords

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ Distribution file for x/84
 from distutils.core import setup
 
 setup(name='x84',
-      version='1.0.1',
-      description='UTF-8 Telnet BBS',
+      version='1.0.2',
+      description='UTF-8 telnet server with default "scene board"',
       author='Jeff Quast',
-      author_email='dingo@1984.ws',
+      author_email='contact@jeffquast.com',
       url='http://github.com/jquast/x84/',
       keywords=[
           'telnet', 'terminal', 'server', 'ansi', 'bbs', 'mud',
-          'curses', 'multiprocessing', 'ttyrec', 'session', 'ttyplay',
-          'sqlite', 'utf8', 'cp437', 'rfc856', 'rfc857', 'rfc858',
-          'rfc859', 'rfc1073', 'rfc1572', 'naws'],
+          'curses', 'multiprocessing', 'ttyrec', 'ttyplay',
+          'sqlite', 'utf8', 'cp437', 'rfc854', 'rfc856', 'rfc857',
+          'rfc858', 'rfc859', 'rfc1073', 'rfc1572', 'naws',
+          'xterm', 'unix', 'posix', 'doors'],
       license='ISC',
       packages=['x84', 'x84.default', 'x84.default.art', 'x84.bbs'],
       package_data={
@@ -22,7 +23,7 @@ setup(name='x84',
           'x84.default': ['*.ans',],
           'x84.default.art': ['*.asc', '*.ans', '*.txt'],
       },
-      requires=['requests', 'sauce', 'sqlitedict', 'xmodem'],
+      requires=['requests', 'sauce', 'sqlitedict'],
       scripts=['bin/x84'],
       classifiers=[
           'Development Status :: 4 - Beta',
@@ -40,5 +41,5 @@ setup(name='x84',
           'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Topic :: Communications :: BBS',
-          'Topic :: Terminals :: Telnet', ]
+          'Topic :: Terminals :: Telnet',]
       )
