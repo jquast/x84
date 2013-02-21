fa33c1c17080a13941a2916f4f272e886c0cd77e
jquast <dingo@1984.ws>
2013-02-21 02:28:19 -0500

    add editor.txt to distribution

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='x84',
       packages=['x84', 'x84.default', 'x84.default.art', 'x84.bbs'],
       package_data={
           '': ['README.rst'],
-          'x84.default': ['*.ans',],
+          'x84.default': ['*.ans', '*.txt',],
           'x84.default.art': ['*.asc', '*.ans', '*.txt'],
       },
       install_requires=[
