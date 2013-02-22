e7e75162d4d1ab5e338578b62b2cd47ea91bac52
jquast <dingo@1984.ws>
2013-02-21 20:35:13 -0500

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 import os
 
 setup(name='x84',
-      version='1.0.3',
+      version='1.0.4',
       description="utf-8 and cp437 telnet BBS",
       long_description=open(os.path.join(os.path.dirname(__file__),
                                          'README.txt')).read(),
