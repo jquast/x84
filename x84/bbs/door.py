85e654264977cb65f6223d369f712a7bf4661c0a
jquast <contact@jeffquast.com>
2013-05-14 20:04:14 -0400

    try this rowhack

--- x84/bbs/door.py
+++ x84/bbs/door.py
@@ -460,6 +460,7 @@ class DOSDoor(Door):
     """
     RE_REPWITH_CLEAR = r'\033\[(1;80H.*\033\[1;1H|H\033\[2J)' # dos CLS? or lord?
     RE_REPWITH_NONE = r'(\033\[(6n|\?1049[lh]|\d+;\d+r)|dosemu\.org\.\033\[1;1H)'
+    RE_ROWONE_FIX = (r'\033\[1;(\d+)H')
     #RE_TRIMOUT = r'\033\[(6n|\?1049[lh]|1;80H\033\[1K |\d+;\d+r)'
     START_BLOCK_INP = 2.0
 
@@ -470,6 +471,7 @@ class DOSDoor(Door):
         self.check_winsize()
         self._stime = time.time()
         self._re_trim_clear = re.compile(self.RE_REPWITH_CLEAR, flags=re.DOTALL)
+        self._re_rowone_fix = re.compile(self.RE_ROWONE_FIX)
         self._re_trim_none = re.compile(self.RE_REPWITH_NONE, flags=re.DOTALL)
         self._replace_clear = ''.join((
                 self._term.move(self._term.height, 0),
@@ -477,9 +479,19 @@ class DOSDoor(Door):
                 self._term.home))
 
     def output_filter(self, data):
-        return re.sub(pattern=self._re_trim_none, repl=u'',
-                string=re.sub(pattern=self._re_trim_clear, repl=(self._replace_clear),
-                    string=Door.output_filter(self, data)))
+        data = Door.output_filter(self, data)
+        data = re.sub(pattern=self._re_trim_clear, repl=(self._replace_clear),
+                string=data)
+        data = re.sub(pattern=self._re_trim_none, repl=u'',
+                string=data)
+        rowhack = self._re_rowone_fix.search(data)
+        if rowhack is not None:
+            data = u''.join((
+                    data[:rowhack.start()],
+                    self._replace_clear,
+                    self._term.move(0, int(rowhack.group(1)) - 1),
+                    data[rowhack.end():]))
+        return data
 
     def input_filter(self, data):
         return data if time.time() - self._stime > self.START_BLOCK_INP else ''
