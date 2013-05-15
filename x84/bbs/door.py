0537dab4003842ec214805acc424fbcfd2dd8c7b
jquast <contact@jeffquast.com>
2013-05-15 00:31:31 -0400

    don't scroll

--- x84/bbs/door.py
+++ x84/bbs/door.py
@@ -485,7 +485,7 @@ class DOSDoor(Door):
         self._stime = time.time()
         self._re_trim_clear = re.compile(self.RE_REPWITH_CLEAR, flags=re.DOTALL)
         self._re_trim_none = re.compile(self.RE_REPWITH_NONE, flags=re.DOTALL)
-        self._replace_clear = (u'\r\n' * 25)
+        self._replace_clear = (u'\r\n')
 
     def output_filter(self, data):
         data = Door.output_filter(self, data)
