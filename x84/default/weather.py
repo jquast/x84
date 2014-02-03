d4bbb57b31cdcc277720b540f0874f3b2679e369
jquast <contact@jeffquast.com>
2014-02-02 20:00:45 -0800

    new tv panel-like weather icons, thanks to xzip

--- x84/default/weather.py
+++ x84/default/weather.py
@@ -2,29 +2,71 @@
 Weather retriever for x/84 https://github.com/jquast/x84
 """
 from xml.etree import cElementTree as ET
+import itertools
+import textwrap
 import requests
+import time
+import os
 
-# sorry this isn't doing celcius, etc., its just how the data comes,
-# conversions were done in previous versions, but removed when rewritten
-# for brevity.
+weather_icons = os.path.join(os.path.dirname(__file__), 'art', 'weather')
+panel_width = 16
+panel_height = 8
+top_margin = 3
+next_margin = 3
+
+
+def temp_conv(val, centigrade):
+    """
+    Convert temperature ``val`` to C or F, returning both the integer
+    value and brief descriptor as tuple, fe. (33, u'F',).
+    """
+    try:
+        val = int(val)
+    except:
+        return '', ''
+    if not centigrade:
+        return val, u'F'
+    val = int((val - 32) * (float(5) / 9))
+    return val, u'C'
+
+
+def speed_conv(val, centigrade):
+    """
+    Convert windspeed ``val`` to MPH or KPH, returning both the integer
+    value and brief descriptor as tuple, fe. (10, u'MPH',). We re-use
+    the session boolean 'centigrade' as weather or not to use MPH or KPH,
+    (centigrade is metric, otherwise imperial). This isn't 100% accurate,
+    but close enough for our needs ..
+    """
+    # we simply use the 'centigrade' measurement as imperial vs. metric
+    try:
+        val = int(val)
+    except:
+        return '', ''
+    if not centigrade:
+        return val, u'MPH'
+    else:
+        return int(float(val) / 0.62137), 'KPH'
+
 
 def disp_msg(msg):
     """ Display unicode string ``msg`` in yellow. """
     from x84.bbs import getterminal, echo
     term = getterminal()
-    echo(u''.join((u'\r\n\r\n',
-                   term.bold_yellow('%s ' % (msg,),),
-                   term.yellow_reverse_bold(u'...'),)))
+    msg = term.bold_yellow(msg)
+    dotdot = term.yellow_reverse_bold(u'...')
+    echo(u'\r\n\r\n{msg} {dotdot}'.format(msg=msg, dotdot=dotdot))
 
 
 def disp_notfound():
     """ Display 'bad request -/- not found in red. """
     from x84.bbs import getsession, getterminal, echo, getch
     term = getterminal()
-    echo(u''.join((u'\r\n\r\n',
-                   term.bold(u'bAd REQUESt'),
-                   term.bold_red(' -/- '),
-                   term.bold('NOt fOUNd.',),)))
+    bad_req = term.bold(u'bAd REQUESt')
+    decorator = term.bold_red(u'-/-')
+    not_found = term.bold('NOt fOUNd.')
+    echo('\r\n\r\n{bad_req} {decorator} {not_found}'.format(
+        bad_req=bad_req, decorator=decorator, not_found=not_found))
     if not getsession().user.get('expert', False):
         getch(1.7)
 
@@ -33,37 +75,40 @@ def disp_found(num):
     """ Display 'N locations discovered' in yellow/white. """
     from x84.bbs import getterminal, echo
     term = getterminal()
-    echo(u''.join((u'\r',
-                   term.bold_white(u'%d' % (num,)),
-                   term.yellow(u' lOCAtiON%s diSCOVEREd ' %
-                               (u's' if num > 1 else u'')),
-                   term.bold_black(u'...'),)))
+    disp_n = term.bold_white('{}'.format(num))
+    locations = term.yellow(u'lOCAtiON{s} diSCOVEREd'.format(
+        s=u's' if num > 1 else u'',))
+    dotdot = term.bold_black(u'...')
+    echo('\r{disp_n} {locations} {dotdot}'.format(
+        disp_n=disp_n, locations=locations, dotdot=dotdot))
 
 
 def disp_search_help():
     """ Display searchbar usage. """
-    from x84.bbs import getterminal, echo
+    from x84.bbs import getterminal, echo, Ansi
     term = getterminal()
-    msg_enterzip = (
-        term.yellow(u'ENtER US '),
-        term.bold_yellow(u'POStAl COdE'),
-        term.yellow(u', OR NEARESt '),
-        term.bold_yellow(u'iNtERNAtiONAl CitY. '),
-        term.bold_yellow(u'('),
-        term.underline_yellow('Escape'),
-        term.bold_white(u':'),
-        term.yellow('EXit'),
-        term.bold_yellow(u')'),)
-    echo(u''.join((u'\r\n\r\n', term.normal,) + msg_enterzip))
+
+    enter = term.yellow(u'ENtER US')
+    postal = term.bold_yellow(u'POStAl COdE')
+    or_nearest = term.yellow(u', OR NEARESt')
+    int_city = term.bold_yellow(u'iNtERNAtiONAl CitY.')
+    keyhelp = (u'{t.bold_yellow}({t.normal}'
+               u'{t.underline_yellow}ESCAPE{t.normal}'
+               u'{t.bold_white}:{t.normal}'
+               u'{t.yellow}EXit{t.normal}'
+               u'{t.bold_yellow}){t.normal}'.format(t=term))
+
+    echo(u'\r\n\r\n' + term.normal)
+    echo(Ansi(u'{enter} {postal}{or_nearest} {int_city} {keyhelp}'.format(
+        enter=enter, postal=postal, or_nearest=or_nearest,
+        int_city=int_city, keyhelp=keyhelp)).wrap(term.width))
 
 
 def do_fetch(postal):
     """
     Given postal code, fetch and return xml root node of weather results.
     """
-    from x84.bbs import echo, getch, getterminal
     import StringIO
-    term = getterminal()
     disp_msg('fEtChiNG')
     resp = requests.get(u'http://apple.accuweather.com'
                         + u'/adcbin/apple/Apple_Weather_Data.asp',
@@ -72,15 +117,8 @@ def do_fetch(postal):
         disp_notfound()
         return None
     if resp.status_code != 200:
-        # todo: logger.error
-        echo(u'\r\n')
-        echo(term.bold_red(u'StAtUS COdE: %s' % (resp.status_code,)))
-        echo(u'\r\n\r\n')
-        echo(repr(resp.content))
-        echo(u'\r\n\r\n' + 'PRESS ANY kEY')
-        getch()
-        return None
-    print resp.content
+        raise RuntimeError('Status code: {}, content={!r}'.format(
+            resp.status_code, resp.content))
     xml_stream = StringIO.StringIO(resp.content)
     tree = ET.parse(xml_stream)
     return tree.getroot()
@@ -106,7 +144,7 @@ def do_search(search):
         echo(u'\r\n\r\n' + 'PRESS ANY kEY')
         getch()
     else:
-        print resp.content
+        #print resp.content
         xml_stream = StringIO.StringIO(resp.content)
         locations = list([dict(elem.attrib.items())
                           for _event, elem in ET.iterparse(xml_stream)
@@ -118,7 +156,7 @@ def do_search(search):
     return locations
 
 
-def parse_weather(root):
+def parse_todays_weather(root):
     """
     Parse and return dictionary describing today's weather
     from weather xml root node.
@@ -141,11 +179,59 @@ def parse_forecast(root):
     forecast = dict()
     for elem in root.find('Forecast'):
         if elem.tag == 'day':
-            key = elem.attrib.get('number')
+            key = int(elem.attrib.get('number'))
             forecast[key] = dict()
             for subelem in elem:
                 forecast[key][subelem.tag] = subelem.text.strip()
-    return forecast
+    return [value for key, value in sorted(forecast.items())]
+
+
+def get_centigrade():
+    from x84.bbs import getterminal, getsession, echo, getch
+    term = getterminal()
+    session = getsession()
+    echo(u'\r\n\r\n')
+    echo(term.yellow(u'CElCiUS'))
+    echo(term.bold_yellow(u'('))
+    echo(term.bold_yellow_reverse(u'C'))
+    echo(term.bold_yellow(u')'))
+    echo(u' or ')
+    echo(term.yellow(u'fAhRENhEit'))
+    echo(term.bold_yellow(u'('))
+    echo(term.bold_yellow_reverse(u'F'))
+    echo(term.bold_yellow(u')'))
+    echo('? ')
+    while True:
+        inp = getch()
+        print(repr(inp))
+        if inp in (u'c', u'C'):
+            session.user['centigrade'] = True
+            session.user.save()
+            break
+        elif inp in (u'f', u'F'):
+            session.user['centigrade'] = False
+            session.user.save()
+            break
+        elif inp in (u'q', u'Q', term.KEY_EXIT):
+            break
+
+
+def chk_centigrade():
+    from x84.bbs import getterminal, getsession, echo, getch
+    term = getterminal()
+    session = getsession()
+    echo(u'\r\n\r\n')
+    echo(u'USiNG ')
+    if session.user.get('centigrade', None):
+        echo(term.yellow(u'CElCiUS'))
+    else:
+        echo(term.yellow(u'fAhRENhEit'))
+    echo(term.bold_black('...'))
+    echo(u' PRESS ')
+    echo(term.bold_yellow_reverse(u'!'))
+    echo(u' tO ChANGE.')
+    if getch(timeout=1) == u'!':
+        get_centigrade()
 
 
 def get_zipsearch(zipcode=u''):
@@ -292,103 +378,130 @@ def location_prompt(location, msg='WEAthER'):
                    u': '),))
     while True:
         inp = getch()
-        if inp is None or inp in (u'n', u'N', 'q', 'Q', term.KEY_EXIT):
+        if inp is None or inp in (u'n', u'N', u'q', u'Q', term.KEY_EXIT):
             return False
         if inp in (u'y', u'Y', u' ', term.KEY_ENTER):
             return True
 
 
-def disp_forecast(forecast):
-    """ Display weather forecast.  """
-    from x84.bbs import getterminal, echo, Ansi, getch
+def get_icon(weather):
+    from x84.bbs import from_cp437
+
+    # attribute 'WeatherIcon' is mapped to one of the {}.ans files
+    icon = int(weather['WeatherIcon'])
+    artfile = os.path.join(weather_icons, '{}.ans'.format(icon))
+    if not os.path.exists(artfile):
+        art = u'[ .{:>2}. ]'.format(icon)
+    else:
+        art = [from_cp437(line.rstrip())
+               for line in open(artfile, 'r').readlines()]
+    return art
+
+
+def display_panel(weather, column, centigrade):
+    from x84.bbs import getterminal, echo, from_cp437
     term = getterminal()
-    lno = 1
-    echo(u'\r\n')
-    for key in sorted(forecast.keys()):
-        fcast = forecast[key]
-        rstr = u''.join((
-            term.bold_yellow_underline(fcast['DayCode']),
-            u', ',
-            u'/'.join(fcast['ObsDate'].split('/', 3)[0:2]),
-            term.bold_underline(u':'), u' ',
-            u'%s. ' % (
-                term.bold(
-                    fcast.get('TXT_Long', fcast.get('TXT_Short', u''))),),
-            u'hiGH Of %sF, lOW Of %sF. ' % (
-                term.bold(fcast.get('High_Temperature')),
-                term.bold(fcast.get('Low_Temperature')),),))
-        if 0.0 != float(fcast.get('Snow_Amount', '0.0')):
-            rstr += u'SNOW AMOUNt %s iN., ' % (
-                    term.yellow(fcast.get('Snow_Amount')),)
-        if 0.0 != float(fcast.get('Rain_Amount', '0.0')):
-            rstr += u'RAiN AMOUNt %s iN., ' % (
-                    term.yellow(fcast.get('Snow_Amount')),)
-        if 0.0 != float(fcast.get('Precip_Amount', '0.0')):
-            rstr += u'PRECiPiTAtiON %s iN., ' % (
-                    term.yellow(fcast.get('Precip_Amount')),)
-        if 0 != int(fcast.get('TStorm_Prob', '0')):
-            rstr += u'thUNdERStORM PRObAbilitY: %s, ' % (
-                    term.yellow(fcast.get('TStorm_Prob')),)
 
-        if 0 != len(fcast.get('WindDirection', u'')):
-            rstr += u'WiNdS %s ' % (
-                    term.yellow(fcast.get('WindDirection')),)
-        if 0 != len(fcast.get('WindSpeed', u'')):
-            rstr += u'At %sMPh, ' % (
-                    term.yellow(fcast.get('WindSpeed')),)
-        if 0 != len(fcast.get('WindGust', u'')):
-            rstr += u'GUStS Of %sMPh. ' % (
-                    term.yellow(fcast.get('WindGust')),)
-            if 0 != len(fcast.get('Real_Feel_High', u'')):
-                rstr += u'PROdUCiNG '
-        if 0 != len(fcast.get('Real_Feel_High', u'')):
-            rstr += u'A WiNdCHill HiGh Of %sF, lOW %sF. ' % (
-                    term.yellow(
-                        fcast.get('Real_Feel_High')),
-                    term.yellow(
-                        fcast.get('Real_Feel_Low', u'?')),)
-        echo(u'\r\n')
-        lno += 1
-        lines = Ansi(rstr).wrap(min(60, int(term.width * .8))).splitlines()
-        for line in lines:
-            lno += 1
-            echo(line + u'\r\n')
-            if 0 == lno % (term.height - 1):
-                echo(term.yellow_reverse('--MORE--'))
-                if getch() is None:
-                    return False
-                echo(u'\r\n')
+    # display day of week,
+    day_txt = term.bold(weather.get('DayCode', u'').center(panel_width))
+    echo(term.move(top_margin, column))
+    echo(day_txt)
+
+    # display WeatherIcon ansi art,
+    for row_idx, art_row in enumerate(get_icon(weather)):
+        echo(term.move(row_idx + top_margin + 2, column))
+        echo(art_row)
+    echo(term.normal)
+
+    degree = from_cp437(''.join([chr(248)]))
+    # display days' high,
+    echo(term.move(panel_height + top_margin + 2, column))
+    high = weather.get('High_Temperature', None)
+    high, conv = temp_conv(high, centigrade)
+    echo(u'High: {high:>2}{degree}{conv}'.format(
+        high=high, degree=degree, conv=conv).rjust(panel_width - 3))
+
+    # display days' low,
+    echo(term.move(panel_height + top_margin + 3, column))
+    low = weather.get('Low_Temperature', None)
+    low, conv = temp_conv(low, centigrade)
+    echo(u'Low: {low:>2}{degree}{conv}'.format(
+        low=low, degree=degree, conv=conv).rjust(panel_width - 3))
+
+    # display short txt,
+    weather_txt = unicode(weather.get('TXT_Short', ''))
+    txt_wrapped = textwrap.wrap(weather_txt, (panel_width - 2))
+
+    row_loc = panel_height + top_margin + 5
+    for row_idx, txt_row in enumerate(txt_wrapped):
+        row_loc = panel_height + top_margin + row_idx + 5
+        echo(term.move(row_loc, column + 1))
+        echo(txt_row.center(panel_width - 2))
+    return row_loc
 
 
-def disp_weather(weather):
-    """ Display today's weather. """
-    from x84.bbs import getterminal, echo, Ansi
+def display_weather(todays, weather, centigrade):
+    """
+    Display weather as vertical panels.
+
+    Thanks to xzip, we now have a sortof tv-weather channel art :-)
+    """
+    from x84.bbs import getterminal, echo, from_cp437
     term = getterminal()
-    rstr = u''.join((u'At ',
-                     term.bold(u'%s%s' % (weather.get('City'),
-                         u', %s' % (weather['State'],) if ('State' in weather
-                             and 0 != len(weather['State'])) else u'',)),
-                     term.underline(u':'), u' ',
-                     u'%s. ' % (
-                     term.bold_yellow(weather.get('WeatherText')),),
-                     u'tEMPERAtURE Of %sF, ' % (
-                     term.bold(weather.get('Temperature')),),
-                     u'RElAtiVE hUMiditY %s. ' % (
-                     term.yellow(weather.get('Humidity')),),
-                     u'WiNdS %s At %s MPh' % (
-                     term.yellow(weather.get('WindDirection')),
-                     term.yellow(weather.get('WindSpeed'))),
-                     u', PROdUCiNG A WiNdCHill Of %sF. ' % (
-                     term.yellow(weather.get('RealFeel')),)
-                     if (weather.get('RealFeel', weather.get('Temperature'))
-                         != weather.get('Temperature'))
-                     else u'. ',
-                     u'ThE PRESSURE WAS %s iNChES ANd %s.' % (
-                     term.yellow(weather.get('Pressure')),
-                     term.yellow(weather.get('Pressure-state')),
-                     ),))
-    echo(u'\r\n\r\n')
-    echo(Ansi(rstr).wrap(min(60, int(term.width * .8))))
+
+    echo(term.height * '\r\n')
+    echo(term.move(1, 1))
+    at = term.yellow_bold('At')
+    city = term.bold(todays.get('City', u''))
+    state = todays.get('State', u'')
+    if state:
+        state = u', {}'.format(term.bold_yellow_reverse(state))
+    dotdot = term.bold_black('...')
+    echo(u'{at} {city}{state} {dotdot}'.format(
+        at=at, city=city, state=state, dotdot=dotdot))
+    bottom = 2
+    for column in range(0, (term.width - panel_width), panel_width):
+        try:
+            day = weather.pop(0)
+        except IndexError:
+            break
+        bottom = max(display_panel(day, column, centigrade), bottom)
+
+    timenow = time.strftime('%I:%M%p', time.strptime(todays['Time'], '%H:%M'))
+    temp, deg_conv = temp_conv(todays.get('Temperature', ''), centigrade)
+    real_temp, deg_conv = temp_conv(todays.get('RealFeel', ''), centigrade)
+    speed, spd_conv = speed_conv(todays.get('WindSpeed', ''), centigrade)
+    degree = from_cp437(''.join([chr(248)]))
+
+    current_0 = u'Current conditions at {timenow}'.format(timenow=timenow)
+    current_1 = u'{w[WeatherText]}'.format(w=todays)
+    current_2 = u'Temperature is {temp}{degree}{deg_conv}'.format(
+        temp=temp, degree=degree, deg_conv=deg_conv)
+    current_3 = u'' if real_temp == temp else (
+        u'(feels like {real_temp}{degree}{deg_conv})'.format(
+            real_temp=real_temp, degree=degree, deg_conv=deg_conv))
+    current_4 = u'Winds {speed}{spd_conv} {w[WindDirection]}'.format(
+        speed=speed, w=todays, spd_conv=spd_conv)
+    current_5 = u'Humidity of {w[Humidity]}'.format(w=todays)
+
+    wrapped = textwrap.wrap(
+        u'{0}: {1}. {2} {3}, {4}, {5}.'.format(
+            current_0, current_1, current_2, current_3,
+            current_4, current_5), min(term.width - panel_width - 5, 40))
+    row_num = 0
+
+    art = get_icon(todays)
+    for row_num, (row_txt, art_txt) in enumerate(
+            itertools.izip_longest(wrapped, art)):
+        echo(term.move(bottom + next_margin + row_num, 1))
+        echo(art_txt)
+        if row_txt:
+            echo(term.move(bottom + next_margin + row_num, panel_width + 5))
+            echo(term.normal)
+            echo(row_txt + u'\r\n')
+        else:
+            echo(u'\r\n')
+    echo(term.move(bottom + next_margin + row_num))
 
 
 def main():
@@ -412,15 +525,15 @@ def main():
         root = do_fetch(location.get('postal'))
         if root is None:
             return
-        weather = parse_weather(root)
-        #if False == location_prompt(location, 'WEAthER'):
-        #    break
-        disp_weather(weather)
-        if False == location_prompt(location, 'fORECASt'):
-            break
+        todays = parse_todays_weather(root)
         forecast = parse_forecast(root)
-        disp_forecast(forecast)
-        echo(u'\r\n')
+        if session.user.get('centigrade', None) is None:
+            get_centigrade()
+        else:
+            chk_centigrade()
+        centigrade = session.user.get('centigrade', False)
+        display_weather(todays, forecast, centigrade)
+        echo(u'\r\n\r\n')
         echo(term.yellow_reverse('--ENd Of tRANSMiSSiON--'))
         getch()
         break
@@ -433,10 +546,10 @@ def main():
                        term.bold_black(u'PRiVAtE'),
                        term.bold_yellow(') '),
                        term.yellow('? '),
-            term.bold_yellow(u'['),
-            term.underline_yellow(u'yn'),
-            term.bold_yellow(u']'),
-            u': '),))
+                       term.bold_yellow(u'['),
+                       term.underline_yellow(u'yn'),
+                       term.bold_yellow(u']'),
+                       u': '),))
         while True:
             inp = getch()
             if inp is None or inp in (u'n', u'N', 'q', 'Q', term.KEY_EXIT):
