ee211103d0c4d170b31c9d08806f389d34617f81
jquast <contact@jeffquast.com>
2014-04-14 05:53:52 -0700

    more improving

--- x84/default/automsg.py
+++ x84/default/automsg.py
@@ -1,77 +1,84 @@
-""" Automsg script 1.0 by Hellbeard for x84 """
+""" Automsg script for x/84 bbs, https://github.com/jquast/x84 """
 from x84.bbs import getsession, getterminal, echo, LineEditor, showcp437
 import os
 import codecs
+__author__ = 'Hellbeard'
+__version__ = 1.0
 
 
-def get_datafile():
+def get_datafile_path():
     from x84.bbs import ini
-    folder = os.path.join(ini.CFG.get('system', 'datapath'), os.path.pardir, 'data')
+    folder = os.path.join(ini.CFG.get('system', 'datapath'))
     return os.path.join(folder, 'automsg.txt')
 
 
-# -----------------------------------------------------------------------------
+def ask(msg):
+    echo(term.move(18, 0))
+    echo(term.magenta(msg))
+    echo(term.green(u'(') + term.cyan(u'yes/no') + term.green(u')'))
+
+
 def banner():
-  session, term = getsession(), getterminal()
-  art_file = os.path.join(os.path.dirname(__file__), 'art', 'automsg.ans')
-  echo(term.clear)
-  for line in showcp437(art_file):
-    echo (line)
+    session, term = getsession(), getterminal()
+    art_file = os.path.join(os.path.dirname(__file__), 'art', 'automsg.ans')
+    echo(term.clear)
+    for line in showcp437(art_file):
+        echo(line)
 
-  with open(get_datafile()) as fo:
-    handle = fo.readline().strip()
-    echo(term.move(9, 30) + term.blue_on_green(handle))
-    for row in range(1, 4):
-      echo(term.move(10 + row, 9) + term.yellow(fo.readline().strip()))
+    if not os.path.exists(get_datafile_path()):
+        with codecs.open(get_datafile_path(), 'w', 'utf-8') as fo:
+            fo.write('big brother\n')
+            fo.write('behave yourselves.\n')
+            fo.write('\n\n')
 
-  echo(term.move(9, 9) + term.white(u'Public message from:'))
-  echo(term.move(18, 0))
-  echo(term.magenta(u'do you want to write a new public message? '))
-  echo(term.green(u'(') + term.cyan(u'yes/no') + term.green(u')'))
+    echo(term.move(9, 9) + term.white(u'Public message from:'))
 
-# -----------------------------------------------------------------------------
+    with codecs.open(get_datafile_path(), 'r', 'utf-8') as fo:
+        handle = fo.readline().strip()
+        echo(term.move(9, 30) + term.blue_on_green(handle))
+        for row in range(1, 4):
+            echo(term.move(10 + row, 9) + term.yellow(fo.readline().strip()))
+
+    ask(u'do you want to write a new public message? ')
+
 
 def main():
-  term = getterminal()
-  session = getsession()
-  session.activity = 'automsg'
-  banner()
+    term = getterminal()
+    session = getsession()
+    session.activity = 'automsg'
+    banner()
 
+    while True:
+        inp = term.inkey()
+        if inp.lower() in (u'n', 'q') or inp.code == term.KEY_ENTER:
+            return
 
+        if inp.lower() == u'y':
+            echo(term.move(9, 30))
+            echo(term.blue_on_green(session.user.handle))
+            echo((u' ' * 7))
 
-  while True:
-    keypressed = term.inkey()
-    if keypressed.lower() in (u'n', 'q') or keypressed.code == term.KEY_ENTER:
-      break
+            echo(term.move(18, 0) + term.clear_eol)
+            for row in range (1, 4):
+                echo(term.move(10 + row, 9))
+                echo(u' ' * 57)
 
-    if keypressed.lower() == u'y':
-      echo(term.move(9, 30))
-      echo(term.blue_on_green(session.user.handle))
-      echo((u' ' * 7))
+            msg = []
+            for row in range (1, 4):
+                echo(term.move(10 + row, 9))
+                le = LineEditor(56)
+                le.colors['highlight'] = term.yellow
+                msg.append(le.read())
 
-      echo(term.move(18, 0) + term.clear_eol)
-      for row in range (1, 4):
-        echo(term.move(10 + row, 9))
-        echo(u' ' * 57)
+            ask(u'submit your text as a public message? ')
 
-      msg = []
-      for row in range (1, 4):
-        echo(term.move(10 + row, 9))
-        le = LineEditor(56)
-        le.colors['highlight'] = term.yellow
-        msg.append(le.read())
-
-      echo (term.move(18, 0))
-      echo (term.magenta(u'submit your text as a public message? '))
-      echo (term.green(u'(') + term.cyan(u'yes/no') + term.green(u')'))
-
-      while 1:
-        keypressed = term.inkey()
-        if keypressed.lower() == u'n':
-          return
-        if keypressed == 'y' or keypressed == 'Y':
-          echo(term.move(18, 0) + term.clear_eol)
-          with codecs.open(get_datafile(), 'w', 'utf-8') as fo:
-            fo.write('\n'.join([session.user.handle] + msg))
-            fo.close()
-          return
+            while 1:
+                inp = term.inkey()
+                if inp.lower() == u'n':
+                    return
+                if inp == 'y' or inp == 'Y':
+                    echo(term.move(18, 0) + term.clear_eol)
+                    with codecs.open(get_datafile_path(), 'w', 'utf-8') as fo:
+                        fo.write('\n'.join([session.user.handle] + msg))
+                        fo.close()
+                    return
