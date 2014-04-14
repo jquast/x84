126137b2b280f6f462cc056f0683115c9c9c593f
jquast <contact@jeffquast.com>
2014-04-14 05:43:13 -0700

    cleanup and use ini `datapath' for automsg.txt location

--- x84/default/automsg.py
+++ x84/default/automsg.py
@@ -1,30 +1,33 @@
 """ Automsg script 1.0 by Hellbeard for x84 """
-from x84.bbs import getsession, getterminal, echo, getch, LineEditor, showcp437
+from x84.bbs import getsession, getterminal, echo, LineEditor, showcp437
 import os
 import codecs
 
+
+def get_datafile():
+    from x84.bbs import ini
+    folder = os.path.join(ini.CFG.get('datapath'), os.path.pardir, 'data')
+    return os.path.join(folder, 'automsg.txt')
+
+
 # -----------------------------------------------------------------------------
 def banner():
-  term = getterminal()
-  session = getsession()
+  session, term = getsession(), getterminal()
+  art_file = os.path.join(os.path.dirname(__file__), 'art', 'automsg.ans')
   echo(term.clear)
-  term = getterminal()
-  banner = ''
-  for line in showcp437(os.path.dirname(__file__)+ '/art/'+'automsg.ans'):
-    banner = banner + line
-  echo (banner)
+  for line in showcp437(art_file):
+    echo (line)
 
-  msg = ''
-  fo = open(os.path.dirname(__file__)+ '/art/automsg.txt', 'r')
+  with open(get_datafile()) as fo:
+    handle = fo.readline().strip()
+    echo(term.move(9, 30) + term.blue_on_green(handle))
+    for row in range(1, 4):
+      echo(term.move(10 + row, 9) + term.yellow(fo.readline().strip()))
 
-  echo(term.move(9,30)+term.blue_on_green+str(fo.readline())+term.normal)
-
-  for i in range (1,4):
-    echo(term.move(10+i,9)+term.yellow)
-    echo(u''+str(fo.readline()))
-  fo.close()
-  echo(term.move(9,9)+term.white+'Public message from:')
-  echo(term.move(18,0)+term.magenta+'do you want to write a new public message? '+term.green+'('+term.cyan+'yes/no'+term.green+')')
+  echo(term.move(9, 9) + term.white(u'Public message from:'))
+  echo(term.move(18, 0))
+  echo(term.magenta(u'do you want to write a new public message? '))
+  echo(term.green(u'(') + term.cyan(u'yes/no') + term.green(u')'))
 
 # -----------------------------------------------------------------------------
 
@@ -34,41 +37,41 @@ def main():
   session.activity = 'automsg'
   banner()
 
-  folder = os.path.join(os.path.dirname(__file__), os.path.pardir, 'data')
-  if not os.path.exists(folder):
-    os.makedirs(folder)
-  automsg_txt = os.path.join(folder, 'automsg.txt')
 
 
-  keypressed = ''
-  while 1:
-    keypressed = getch()
-    if keypressed == 'n' or keypressed == 'N' or keypressed == 'Q' or keypressed == 'q' or keypressed == term.KEY_ENTER:
-      return
+  while True:
+    keypressed = term.inkey()
+    if keypressed.lower() in (u'n', 'q') or keypressed.code == term.KEY_ENTER:
+      break
 
-    if keypressed == 'y' or keypressed == 'Y':
-      echo(term.move(9,30)+term.blue_on_green+session.user.handle+term.normal+'       ')
-      echo(term.move(18,0)+term.clear_eol)
-      msg = {}
-      for i in range (1,4):
-        echo(term.move(10+i,9)+'                                                         ')
-      for i in range (1,4):
-        echo(term.move(10+i,9))
+    if keypressed.lower() == u'y':
+      echo(term.move(9, 30))
+      echo(term.blue_on_green(session.user.handle))
+      echo((u' ' * 7))
+
+      echo(term.move(18, 0) + term.clear_eol)
+      for row in range (1, 4):
+        echo(term.move(10 + row, 9))
+        echo(u' ' * 57)
+
+      msg = []
+      for row in range (1, 4):
+        echo(term.move(10 + row, 9))
         le = LineEditor(56)
         le.colors['highlight'] = term.yellow
-        msg[i] = le.read()
+        msg.append(le.read())
 
-      echo(term.move(18,0)+term.magenta+'submit your text as a public message? '+term.green+'('+term.cyan+'yes/no'+term.green+')')
-      keypressed = ''
+      echo (term.move(18, 0))
+      echo (term.magenta(u'submit your text as a public message? '))
+      echo (term.green(u'(') + term.cyan(u'yes/no') + term.green(u')'))
+
       while 1:
-        keypressed = getch()
-        if keypressed == 'n' or keypressed == 'N':
+        keypressed = term.inkey()
+        if keypressed.lower() == u'n':
           return
         if keypressed == 'y' or keypressed == 'Y':
-          echo(term.move(18,0)+term.clear_eol)
-          fo = codecs.open(automsg_txt, 'w', 'utf-8')
-          fo.write(session.user.handle+'\n')
-          for i in range (1,4):
-            fo.write(msg[i]+'\n')
-          fo.close()
+          echo(term.move(18, 0) + term.clear_eol)
+          with codecs.open(get_datafile(), 'w', 'utf-8') as fo:
+            fo.write('\n'.join([session.user.handle] + msg))
+            fo.close()
           return
