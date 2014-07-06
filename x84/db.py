1a029b2e0fa8a39ab70c43fee46455c2f00c4b33
jquast <contact@jeffquast.com>
2014-07-05 18:05:44 -0700

    allow db access outside of session
    although DATALOCK contains multiprocessing.Lock() instances,
    these aren't shared across processes... I'm not sure if locking
    a database is actually effective here... we'll just see how it
    goes ..

--- x84/db.py
+++ x84/db.py
@@ -1,10 +1,94 @@
 """
 Database request handler for x/84 http://github.com/jquast/x84
 """
+# std
+import multiprocessing
 import threading
+import logging
 import os
 
-FILELOCK = threading.Lock()
+# 3rd-party
+import sqlitedict
+
+FILELOCK = multiprocessing.Lock()
+DATALOCK = {}
+
+
+def get_database(filepath, table):
+    global FILELOCK
+    with FILELOCK:
+        # if the bbs is run as root, file ownerships become read-only
+        # and db transactions will throw 'read-only database' errors,
+        # exit earlier if we know that file permissions are to blame
+        check_db(filepath)
+        dictdb = sqlitedict.SqliteDict(filename=filepath,
+                                       tablename=table,
+                                       autocommit=True)
+    return dictdb
+
+
+def check_db(filepath):
+    db_folder = os.path.dirname(filepath)
+    if not os.path.exists(db_folder):
+        os.makedirs(db_folder)
+    assert os.access(db_folder, os.F_OK | os.R_OK), (
+        'Must have rw access to db_folder:', db_folder)
+    if os.path.exists(filepath):
+        assert os.access(filepath, os.F_OK | os.R_OK | os.W_OK), (
+            'Must have r+w access to fb file:', filepath)
+
+
+def get_db_filepath(schema):
+    import x84.bbs.ini
+    folder = x84.bbs.ini.CFG.get('system', 'datapath')
+    return os.path.join(folder, '{0}.sqlite3'.format(schema))
+
+
+def get_db_lock(schema, table):
+    log = logging.getLogger(__name__)
+    key = (schema, table)
+    global DATALOCK, FILELOCK
+    with FILELOCK:
+        if key not in DATALOCK:
+            DATALOCK[key] = multiprocessing.Lock()
+    log.debug("get_db_lock({!r})".format(key))
+    return DATALOCK[key]
+
+
+def get_db_func(dictdb, cmd):
+    assert hasattr(dictdb, cmd), (
+        "{cmd!r} not a valid method of {db_type!r}"
+        .format(cmd=cmd, db_type=type(dictdb)))
+    func = getattr(dictdb, cmd)
+    assert callable(func), (
+        "{cmd!r} not a callable method of {db_type!r}"
+        .format(cmd=cmd, db_type=type(dictdb)))
+    return func
+
+
+def parse_dbevent(event):
+    assert event[2] in ('-', '='), ('event name must match db[-=]event')
+    iterable = event[2] == '='
+    schema = event[3:]
+    assert schema.isalnum() and os.path.sep not in schema, (
+        'database schema {!r} must be alpha-numeric and not contain {!r}'
+        .format(schema, os.path.sep))
+
+    return iterable, schema
+
+
+def should_tapdb():
+    import x84.bbs.ini
+    return x84.bbs.ini.CFG.getboolean('session', 'tap_db')
+
+
+def log_db_cmd(log, schema, cmd, args):
+    s_args = '()'
+    if len(args):
+        s_args = '(*{0})'.format(len(args))
+    log.debug('{schema}/{cmd}{args}'.format(schema=schema,
+                                            cmd=cmd,
+                                            args=s_args))
 
 
 class DBHandler(threading.Thread):
@@ -14,8 +98,6 @@ class DBHandler(threading.Thread):
     queue with the same 'event' name.
     """
 
-    # pylint: disable=R0902
-    #        Too many instance attributes (8/7)
     def __init__(self, queue, event, data):
         """ Arguments:
               queue: parent input end of multiprocessing.Queue()
@@ -24,94 +106,44 @@ class DBHandler(threading.Thread):
                   single transfer. When '=', an iterable is yielded and the
                   data is transfered via the IPC Queue as a stream.
         """
-        import logging
-        import x84.bbs.ini
-        self.queue = queue
-        self.event = event
         self.log = logging.getLogger(__name__)
-        assert event[2] in ('-', '='), ('event name must match db[-=]event')
-        self.iterable = event[2] == '='
-        self.schema = event[3:]
-        assert self.schema.isalnum() and os.path.sep not in self.schema, (
-            'database schema "%s" must be alpha-numeric and not contain %s' % (
-                self.schema, os.path.sep,))
-        self.table = data[0]
-        self.cmd = data[1]
-        self.args = data[2]
-        folder = x84.bbs.ini.CFG.get('system', 'datapath')
-        self._tap_db = x84.bbs.ini.CFG.getboolean('session', 'tap_db')
-        self.filepath = os.path.join(folder, '%s.sqlite3' % (self.schema,),)
+        self.queue, self.event = queue, event
+        self.table, self.cmd, self.args = data
+
+        self.iterable, self.schema = parse_dbevent(event)
+        self.filepath = get_db_filepath(self.schema)
+        self._tap_db = should_tapdb()
+
         threading.Thread.__init__(self)
 
     def run(self):
         """
         Execute database command and return results to session queue.
         """
-        import sqlitedict
-        db_folder = os.path.dirname(self.filepath)
-        with FILELOCK.acquire():
-            # if the bbs is run as root, file ownerships become read-only
-            # and db transactions will throw 'read-only database' errors,
-            # exit earlier if we know that file permissions are to blame
-            if not os.path.exists(db_folder):
-                os.makedirs(db_folder)
-            assert os.access(db_folder, os.F_OK | os.R_OK), (
-                'Must have rw access to db_folder:', db_folder)
-            if os.path.exists(self.filepath):
-                assert os.access(self.filepath, os.F_OK | os.R_OK | os.W_OK), (
-                    'Must have r+w access to fb file:', self.filepath)
-            dictdb = sqlitedict.SqliteDict(filename=self.filepath,
-                                           tablename=self.table,
-                                           autocommit=True)
-        assert hasattr(dictdb, self.cmd), (
-            "'%(cmd)s' not a valid method of <type 'dict'>" % self)
-        func = getattr(dictdb, self.cmd)
-        assert callable(func), (
-            "'%(cmd)s' not a valid method of <type 'dict'>" % self)
+        dictdb = get_database(self.filepath, self.table)
+        func = get_db_func(dictdb, self.cmd)
         if self._tap_db:
-            args = '()'
-            if len(args):
-                args = '(*%d)' % (len(self.args))
-            self.log.debug('{self.schema}/{self.cmd}{args}'
-                           .format(self=self, args=args))
+            log_db_cmd(self.log, self.schema, self.cmd, self.args)
 
-        # single value result,
-        if not self.iterable:
-            try:
-                if 0 == len(self.args):
-                    result = func()
-                else:
-                    result = func(*self.args)
-            # pylint: disable=W0703
-            #         Catching too general exception
-            except Exception as err:
-                # Pokemon exception; package & raise from session process,
-                self.queue.send(('exception', err,))
-                dictdb.close()
-                self.log.exception(err)
-                return
-            self.queue.send((self.event, result))
-            dictdb.close()
-            return
-
-        # iterable value result,
-        self.queue.send((self.event, (None, 'StartIteration'),))
         try:
-            if 0 == len(self.args):
-                for item in func():
-                    self.queue.send((self.event, item,))
+            # single value result,
+            if not self.iterable:
+                result = func(*self.args)
+                self.queue.send((self.event, result))
+
+            # iterable value result,
             else:
+                self.queue.send((self.event, (None, 'StartIteration'),))
                 for item in func(*self.args):
                     self.queue.send((self.event, item,))
+                self.queue.send((self.event, (None, StopIteration,),))
+
         # pylint: disable=W0703
         #         Catching too general exception
         except Exception as err:
             # Pokemon exception; package & raise from session process,
             self.queue.send(('exception', err,))
-            dictdb.close()
             self.log.exception(err)
-            return
-
-        self.queue.send((self.event, (None, StopIteration,),))
-        dictdb.close()
+        finally:
+            dictdb.close()
         return
