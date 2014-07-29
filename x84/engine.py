faecce3d6b729e38fd6481ae6db72b63a5d4de5a
Todd Boyd <haliphax@gmail.com>
2014-07-29 13:25:29 -0500

    ip whitelisting feature for fail2ban mechanism

--- x84/engine.py
+++ x84/engine.py
@@ -163,6 +163,7 @@ def accept(log, server):
         when = int(time.time())
         # default options
         ip_blacklist = set([])
+        ip_whitelist = set([])
         max_attempted_logins = 3
         max_attempted_logins_window = 30
         initial_ban_length = 360
@@ -172,6 +173,12 @@ def accept(log, server):
         try:
             ip_blacklist = set(map(str.strip,
                CFG.get('fail2ban', 'ip_blacklist', '').split(' ')))
+        except ConfigParser.NoOptionError:
+            pass
+        
+        try:
+            ip_whitelist = set(map(str.strip,
+                CFG.get('fail2ban', 'ip_whitelist', '').split(' ')))
         except ConfigParser.NoOptionError:
             pass
 
@@ -246,7 +253,7 @@ def accept(log, server):
                 ATTEMPTED_LOGINS[ip] = record
                 log.debug('Window extended')
         # log attempted login
-        elif ip != '127.0.0.1':
+        elif ip not in ip_whitelist:
             log.debug('First attempted login for this window')
             ATTEMPTED_LOGINS[ip] = {
                 'attempts': 1,
