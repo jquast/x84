c8afc0a11e3f3632de7e5525b2fe98a32c891608
Jeff Quast <jquast@io.com>
2014-12-29 22:11:07 -0800

    Bugfix extras/automsg.py -- does not compile (!!)

--- x84/default/extras/automsg.py
+++ x84/default/extras/automsg.py
@@ -1,4 +1,4 @@
-""" Automsg script for x/84 bbs, https://github.com/jquast/x84 """
+""" Automsg script for x/84 bbs, https://github.com/jquast/x84
 
  Classic /X style script that enables the user to leave a message/statement
  for the next users logging in.
