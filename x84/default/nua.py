3b125f3df40f12c4e607dbf6e32bef8ba273a4d3
Jeff Quast <jquast@io.com>
2015-01-03 06:29:57 -0800

    refactor nua, more ini-configuration, docstrings

--- x84/default/nua.py
+++ x84/default/nua.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 New user account script for x/84, https://github.com/jquast/x84
+
+This script is closely coupled with, and dependend on by profile.py.
 """
 # std imports
 from __future__ import division
@@ -18,71 +20,66 @@ log = logging.getLogger(__name__)
 here = os.path.dirname(__file__)
 
 #: banner art displayed in main()
-art_file = os.path.join(here, 'art', 'nua*.ans')
+art_file = get_ini(
+    section='nua', key='art_file'
+) or os.path.join(here, 'art', 'nua*.ans')
 
 #: preferred fontset for SyncTerm emulator
-syncterm_font = 'topaz'
+syncterm_font = get_ini(
+    section='nua', key='syncterm_font'
+) or 'topaz'
 
 #: which script to execute after registration
-top_script = get_ini(section='matrix',
-                     key='topscript'
-                     ) or 'top'
+top_script = get_ini(
+    section='matrix', key='topscript'
+) or 'top'
 
 #: which login names trigger new user account script
-new_usernames = get_ini(section='matrix',
-                        key='newcmds',
-                        split=True
-                        ) or ['new']
+new_usernames = get_ini(
+    section='matrix', key='newcmds', split=True
+) or ['new']
 
 #: maximum length of user handles
-username_max_length = get_ini(section='nua',
-                              key='max_user',
-                              getter='getint'
-                              ) or 10
+username_max_length = get_ini(
+    section='nua', key='max_user', getter='getint'
+) or 10
 
 #: minimum length of user handles
-username_min_length = get_ini(section='nua',
-                              key='min_user',
-                              getter='getint',
-                              ) or 2
+username_min_length = get_ini(
+    section='nua', key='min_user', getter='getint',
+) or 2
 
 #: which login names trigger new user account script
-invalid_usernames = get_ini(section='nua',
-                            key='invalid_handles',
-                            split=True
-                            ) or ['new', 'apply',
-                                  'exit', 'logoff', 'bye', 'quit',
-                                  'sysop', 'anonymous', ]
+invalid_usernames = get_ini(
+    section='nua', key='invalid_handles', split=True
+) or ['new', 'apply', 'exit', 'logoff', 'bye', 'quit', 'sysop', 'anonymous'
+      ] + new_usernames
 
 #: login name validation as a regular expression
-username_re_validator = get_ini(section='nua',
-                                key='handle_validation',
-                                ) or ['^[A-Za-z0-9]{3,11}$']
+username_re_validator = get_ini(
+    section='nua', key='handle_validation'
+) or ['^[A-Za-z0-9]{3,11}$']
 
 
 #: maximum length of user 'location' field
-location_max_length = get_ini(section='nua',
-                              key='max_location',
-                              getter='getint'
-                              ) or 25
+location_max_length = get_ini(
+    section='nua', key='max_location', getter='getint'
+) or 25
 
 #: maximum length of email address
-email_max_length = get_ini(section='nua',
-                           key='max_email',
-                           getter='getint'
-                           ) or 30
+email_max_length = get_ini(
+    section='nua', key='max_email', getter='getint'
+) or 30
 
 #: maximum length of password
-password_max_length = get_ini(section='nua',
-                              key='max_pass',
-                              getter='getint'
-                              ) or 15
+password_max_length = get_ini(
+    section='nua', key='max_pass', getter='getint'
+) or 15
 
 #: minimum length of password
-password_min_length = get_ini(section='nua',
-                              key='min_pass',
-                              getter='getint'
-                              ) or 4
+password_min_length = get_ini(
+    section='nua', key='min_pass', getter='getint'
+) or 4
 
 #: primary color (highlight)
 color_primary = get_ini(
@@ -98,6 +95,26 @@ color_secondary = get_ini(
 hidden_char = get_ini(
     section='nua', key='hidden_char'
 ) or u'\u00f7'
+
+#: structure for prompting input/validation
+vfield = collections.namedtuple('input_validation', [
+    # field name of user record
+    'name',
+    # field query displayed to user
+    'prompt_key',
+    # dictionary of arguments passed to LineEditor
+    'kwargs',
+    # validation function, None if no validation is required.
+    # function receives (user, new_value) and returns tuple
+    # of (errmsg, index modifier).
+    'validation_function',
+    # when resuming input, how to retrieve field value, may
+    # return None (for example, the password field)
+    'getter',
+    # describe the necessity or optionality of the field and
+    # its purpose to the user
+    'description',
+])
 
 
 def validate_handle(user, handle):
@@ -146,70 +163,57 @@ def validate_password_again(user, password):
     # returns -1 as modifier, to return to password prompt.
     return errmsg, -1
 
-#: structure for prompting input/validation
-vfield = collections.namedtuple('input_validation', [
-    # field name of user record
-    'name',
-    # field query displayed to user
-    'prompt_key',
-    # dictionary of arguments passed to LineEditor
-    'kwargs',
-    # validation function, None if no validation is required.
-    # function receives (user, new_value) and returns tuple
-    # of (errmsg, index modifier).
-    'validation_function',
-    # when resuming input, how to retrieve field value, may
-    # return None (for example, the password field)
-    'getter',
-    # describe the necessity or optionality of the field and
-    # its purpose to the user
-    'description',
-])
 
-#: user record fields prompted by do_nua() loop
-validation_fields = (
-    vfield(name='handle',
-           prompt_key='Username',
-           kwargs={'width': username_max_length},
-           validation_function=validate_handle,
-           getter=lambda: getattr(user, 'handle', None),
-           description=u'Handle or Alias that you will be known '
-           u'by on this board.'),
-    vfield(name='location',
-           prompt_key='Origin (optional)',
-           kwargs={'width': location_max_length},
-           validation_function=None,
-           getter=lambda: getattr(user, 'location', None),
-           description=u'Group of affiliation, geographic location, '
-           u'or other moniker which other members will '
-           u'know as your place or origin.'),
-    vfield(name='email',
-           prompt_key='E-mail (optional)',
-           kwargs={'width': email_max_length},
-           validation_function=None,
-           getter=lambda: getattr(user, 'email', None),
-           description=u'E-mail address is both private and optional, '
-           u'allowing the ability to reset your password '
-           u'should it ever be forgotten.'),
-    vfield(name='password',
-           prompt_key='Password',
-           kwargs={'width': password_max_length,
-                   'hidden': hidden_char},
-           validation_function=validate_password,
-           getter=lambda: None,
-           description=None),
-    vfield(name=None,
-           prompt_key='Again',
-           kwargs={'width': password_max_length,
-                   'hidden': hidden_char},
-           validation_function=validate_password_again,
-           getter=lambda: None,
-           description=None),
-)
+def get_validation_fields(user):
+    """ Return field validation definitions prompted by do_nua() loop. """
+    fields = collections.OrderedDict()
+    fields['handle'] = vfield(
+        name='handle',
+        prompt_key='Username',
+        kwargs={'width': username_max_length},
+        validation_function=validate_handle,
+        getter=lambda: getattr(user, 'handle', None),
+        description=(u'Handle or Alias you will be known '
+                     u'by on this board.'))
+    fields['location'] = vfield(
+        name='location',
+        prompt_key='Origin (optional)',
+        kwargs={'width': location_max_length},
+        validation_function=None,
+        getter=lambda: getattr(user, 'location', None),
+        description=(u'Group of affiliation, geographic location, '
+                     u'or other moniker which other members will '
+                     u'know as your place or origin.'))
+    fields['email'] = vfield(
+        name='email',
+        prompt_key='E-mail (optional)',
+        kwargs={'width': email_max_length},
+        validation_function=None,
+        getter=lambda: getattr(user, 'email', None),
+        description=(u'E-mail address is both private and optional, '
+                     u'allowing the ability to reset your password '
+                     u'should it ever be forgotten.'))
+    fields['password'] = vfield(
+        name='password',
+        prompt_key='Password',
+        kwargs={'width': password_max_length,
+                'hidden': hidden_char},
+        validation_function=validate_password,
+        getter=lambda: None,
+        description=None)
+    fields['password_again'] = vfield(
+        name=None,
+        prompt_key='Again',
+        kwargs={'width': password_max_length,
+                'hidden': hidden_char},
+        validation_function=validate_password_again,
+        getter=lambda: None,
+        description=None)
+    return fields
 
 
-#: positions next prompt 40 pixels minus center of screen
 def fixate_next(term, newlines=2):
+    """ positions next prompt 40 pixels minus center of screen. """
     return u''.join((
         u'\r\n\r\n' * newlines,
         term.move_x(max(0, (term.width // 2) - 40))))
@@ -239,6 +243,71 @@ def prompt_yesno(question):
             return yn.lower() == u'y'
         echo(term.move_x(0) + term.clear_eol)
         echo(fixate_next(term, newlines=0))
+
+
+def show_validation_error(errmsg):
+    """ Display validation error message. """
+    term = getterminal()
+    sep_bad = getattr(term, color_secondary)(u'**')
+    echo(fixate_next(term, newlines=1))
+    for txt in term.wrap(errmsg, width=max(0, min(80, term.width) - 3)):
+        echo(fixate_next(term, newlines=0))
+        echo(u'{sep} {txt}'.format(sep=sep_bad, txt=txt))
+        echo(u'\r\n')
+
+
+def show_description(description):
+    """ Display field description message. """
+    term = getterminal()
+    for txt in term.wrap(description, width=min(80, term.width)):
+        echo(fixate_next(term, newlines=0))
+        echo(getattr(term, color_secondary)(txt.rstrip()))
+        echo(u'\r\n')
+
+
+def do_nua(user):
+    """ Perform new user account field setting and validation. """
+    session, term = getsession(), getterminal()
+    session.activity = u'Applying for an account'
+
+    idx = 0
+    validation_fields = get_validation_fields(user)
+    while idx != len(validation_fields):
+        field = validation_fields.values()[idx]
+        print(field)
+        echo(fixate_next(term, newlines=1))
+        if field.description:
+            show_description(field.description)
+            echo(fixate_next(term, newlines=0))
+        value = prompt_input(term=term,
+                             key=field.prompt_key,
+                             content=field.getter(),
+                             **field.kwargs)
+
+        # user pressed escape, prompt for cancellation
+        if value is None:
+            if prompt_yesno('Cancel'):
+                return None
+            # re-prompt for current field
+            continue
+
+        value = value.strip()
+
+        # validate using function, `field.validation_function()'
+        if field.validation_function:
+            errmsg, mod = field.validation_function(user, value)
+            if errmsg:
+                show_validation_error(errmsg)
+                # re-prompt for current field
+                idx += mod
+                continue
+
+        # set local variable, increment index to next field
+        if field.name:
+            setattr(user, field.name, value)
+        idx += 1
+
+    return user
 
 
 def main(handle=u''):
@@ -277,65 +346,3 @@ def main(handle=u''):
                 'Security race condition: account already exists')
             user.save()
             goto(top_script, user.handle)
-
-
-def show_validation_error(errmsg):
-    """ Display validation error message. """
-    term = getterminal()
-    sep_bad = getattr(term, color_secondary)(u'**')
-    echo(fixate_next(term, newlines=1))
-    for txt in term.wrap(errmsg, width=max(0, min(80, term.width) - 3)):
-        echo(fixate_next(term, newlines=0))
-        echo(u'{sep} {txt}'.format(sep=sep_bad, txt=txt))
-        echo(u'\r\n')
-
-
-def show_description(description):
-    term = getterminal()
-    for txt in term.wrap(description, width=min(80, term.width)):
-        echo(fixate_next(term, newlines=0))
-        echo(getattr(term, color_secondary)(txt.rstrip()))
-        echo(u'\r\n')
-
-
-def do_nua(user):
-    session, term = getsession(), getterminal()
-    session.activity = u'Applying for an account'
-
-    idx = 0
-    while idx != len(validation_fields):
-        field = validation_fields[idx]
-        echo(fixate_next(term, newlines=1))
-        if field.description:
-            show_description(field.description)
-            echo(fixate_next(term, newlines=0))
-        value = prompt_input(term=term,
-                             key=field.prompt_key,
-                             content=field.getter(),
-                             **field.kwargs)
-
-        # user pressed escape, prompt for cancellation
-        if value is None:
-            if prompt_yesno('Cancel'):
-                return None
-            # re-prompt for current field
-            continue
-
-        value = value.strip()
-
-        # validate using function, `field.validation_function()'
-        if field.validation_function:
-            errmsg, mod = field.validation_function(user, value)
-            if errmsg:
-                show_validation_error(errmsg)
-                # re-prompt for current field
-                idx += mod
-                continue
-
-        # set local variable, increment index to next field
-        if field.name:
-            setattr(user, field.name, value)
-        idx += 1
-
-    return user
-
