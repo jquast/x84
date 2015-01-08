60938ed3b415e7b142fe6753dba9b91bf17d6b8b
eric <eric@lagomera.lan>
2015-01-08 11:26:17 +0100

    modified:   lc.py 	modified:   logoff.py 	modified:   matrix.py 	modified:   news.py 	modified:   online.py 	modified:   userlist.py 	modified:   vote.py

--- x84/default/userlist.py
+++ x84/default/userlist.py
@@ -21,7 +21,7 @@ here = os.path.dirname(__file__)
 art_file = os.path.join(here, 'art', 'userlist.ans')
 
 #: encoding used to display artfile
-art_encoding = 'topaz'
+art_encoding = 'cp437'
 
 #: fontset for SyncTerm emulator
 syncterm_font = 'topaz'
