f4a112c7deaf0f05e6aa4461fe71998be1ae6dc9
Jeff Quast <contact@jeffquast.com>
2015-02-14 16:05:40 -0800

    2.0.4 release

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ class BuildDocs(Command):
 
 
 setup(name='x84',
-      version='2.0.3',
+      version='2.0.4',
       description=("Framework for Telnet and SSH BBS or MUD server "
                    "development with example default bbs board"),
       long_description=open(os.path.join(HERE, README)).read(),
